0018191dc632eb2538c4090b814c3ff13f59e6a7
Arthur Juliani <awjuliani@gmail.com>
2017-09-23 12:26:41 -0700

    Add discrete state support to PPO and provide discrete state example environment

--- python/ppo/models.py
+++ python/ppo/models.py
@@ -7,7 +7,7 @@ from unityagents import UnityEnvironmentException
 
 def create_agent_model(env, lr=1e-4, h_size=128, epsilon=0.2, beta=1e-3, max_step=5e6):
     """
-    Takes a Unity environment and model-specific hyperparameters and returns the
+    Takes a Unity environment and model-specific hyper-parameters and returns the
     appropriate PPO agent model for the environment.
     :param env: a Unity environment.
     :param lr: Learning rate.
@@ -15,26 +15,18 @@ def create_agent_model(env, lr=1e-4, h_size=128, epsilon=0.2, beta=1e-3, max_ste
     :param epsilon: Value for policy-divergence threshold.
     :param beta: Strength of entropy regularization.
     :return: a sub-class of PPOAgent tailored to the environment.
+    :param max_step: Total number of training steps.
     """
     brain_name = env.brain_names[0]
-    if env.brains[brain_name].action_space_type == "continuous":
-        if env.brains[brain_name].number_observations == 0:
-            return ContinuousControlModel(lr, env.brains[brain_name].state_space_size,
-                                          env.brains[brain_name].action_space_size, h_size, epsilon, beta, max_step)
+    brain = env.brains[brain_name]
+    if brain.action_space_type == "continuous":
+        if brain.number_observations == 0:
+            return ContinuousControlModel(lr, brain, h_size, epsilon, max_step)
         else:
             raise UnityEnvironmentException("There is currently no PPO model which supports both a continuous "
                                             "action space and camera observations.")
-    if env.brains[brain_name].action_space_type == "discrete":
-        if env.brains[brain_name].number_observations == 0:
-            return DiscreteControlModel(lr, env.brains[brain_name].state_space_size,
-                                        env.brains[brain_name].action_space_size, h_size, epsilon, beta, max_step)
-        else:
-            brain = env.brains[brain_name]
-            if env.brains[brain_name].state_space_size > 0:
-                print("This brain contains agents with both observations and states. There is currently no PPO model"
-                      "which supports this. Defaulting to Vision-based PPO model.")
-            h, w = brain.camera_resolutions[0]['height'], brain.camera_resolutions[0]['height']
-            return VisualDiscreteControlModel(lr, h, w, env.brains[brain_name].action_space_size, h_size, epsilon, beta, max_step)
+    if brain.action_space_type == "discrete":
+        return DiscreteControlModel(lr, brain, h_size, epsilon, beta, max_step)
 
 
 def save_model(sess, saver, model_path="./", steps=0):
@@ -69,7 +61,30 @@ def export_graph(model_path, env_name="env", target_nodes="action"):
 
 
 class PPOModel(object):
-    def __init__(self, probs, old_probs, value, entropy, beta, epsilon, lr, max_step):
+    def create_visual_encoder(self, o_size_h, o_size_w, h_size):
+        self.observation_in = tf.placeholder(shape=[None, o_size_h, o_size_w, 1], dtype=tf.float32,
+                                             name='observation_0')
+        self.conv1 = tf.layers.conv2d(self.observation_in, 32, kernel_size=[3, 3], strides=[2, 2],
+                                      use_bias=False, activation=tf.nn.elu)
+        self.conv2 = tf.layers.conv2d(self.conv1, 64, kernel_size=[3, 3], strides=[2, 2],
+                                      use_bias=False, activation=tf.nn.elu)
+        hidden = tf.layers.dense(c_layers.flatten(self.conv2), h_size, use_bias=False, activation=tf.nn.elu)
+        return hidden
+
+    def create_continuous_state_encoder(self, s_size, h_size):
+        self.state_in = tf.placeholder(shape=[None, s_size], dtype=tf.float32, name='state')
+        hidden_1 = tf.layers.dense(self.state_in, h_size, use_bias=False, activation=tf.nn.elu)
+        hidden_2 = tf.layers.dense(hidden_1, h_size, use_bias=False, activation=tf.nn.elu)
+        return hidden_2
+
+    def create_discrete_state_encoder(self, s_size, h_size):
+        self.state_in = tf.placeholder(shape=[None, 1], dtype=tf.int32, name='state')
+        state_in = tf.reshape(self.state_in, [-1])
+        state_onehot = c_layers.one_hot_encoding(state_in, s_size)
+        hidden = tf.layers.dense(state_onehot, h_size, activation=tf.nn.elu)
+        return hidden
+
+    def create_ppo_optimizer(self, probs, old_probs, value, entropy, beta, epsilon, lr, max_step):
         """
         Creates training-specific Tensorflow ops for PPO models.
         :param probs: Current policy probabilities
@@ -79,6 +94,7 @@ class PPOModel(object):
         :param entropy: Current policy entropy
         :param epsilon: Value for policy-divergence threshold
         :param lr: Learning rate
+        :param max_step: Total number of training steps.
         """
         self.returns_holder = tf.placeholder(shape=[None], dtype=tf.float32, name='discounted_rewards')
         self.advantage = tf.placeholder(shape=[None, 1], dtype=tf.float32, name='advantages')
@@ -104,13 +120,16 @@ class PPOModel(object):
 
 
 class ContinuousControlModel(PPOModel):
-    def __init__(self, lr, s_size, a_size, h_size, epsilon, beta, max_step):
+    def __init__(self, lr, brain, h_size, epsilon, max_step):
         """
         Creates Continuous Control Actor-Critic model.
         :param s_size: State-space size
         :param a_size: Action-space size
         :param h_size: Hidden layer size
         """
+        s_size = brain.state_space_size
+        a_size = brain.action_space_size
+
         self.state_in = tf.placeholder(shape=[None, s_size], dtype=tf.float32, name='state')
         self.batch_size = tf.placeholder(shape=None, dtype=tf.int32, name='batch_size')
         hidden_policy = tf.layers.dense(self.state_in, h_size, use_bias=False, activation=tf.nn.tanh)
@@ -138,57 +157,40 @@ class ContinuousControlModel(PPOModel):
 
         self.old_probs = tf.placeholder(shape=[None, a_size], dtype=tf.float32, name='old_probabilities')
 
-        PPOModel.__init__(self, self.probs, self.old_probs, self.value, self.entropy, 0.0, epsilon, lr, max_step)
+        self.create_ppo_optimizer(self.probs, self.old_probs, self.value, self.entropy, 0.0, epsilon, lr, max_step)
 
 
 class DiscreteControlModel(PPOModel):
-    def __init__(self, lr, s_size, a_size, h_size, epsilon, beta, max_step):
+    def __init__(self, lr, brain, h_size, epsilon, beta, max_step):
         """
         Creates Discrete Control Actor-Critic model.
-        :param s_size: State-space size
-        :param a_size: Action-space size
+        :param brain: State-space size
         :param h_size: Hidden layer size
         """
-        self.state_in = tf.placeholder(shape=[None, s_size], dtype=tf.float32, name='state')
+        hidden_state, hidden_visual, hidden = None, None, None
+        if brain.number_observations > 0:
+            h_size, w_size = brain.camera_resolutions[0]['height'], brain.camera_resolutions[0]['height']
+            hidden_visual = self.create_visual_encoder(h_size, w_size, h_size)
+        if brain.state_space_size > 0:
+            s_size = brain.state_space_size
+            if brain.state_space_type == "continuous":
+                hidden_state = self.create_continuous_state_encoder(s_size, h_size)
+            else:
+                hidden_state = self.create_discrete_state_encoder(s_size, h_size)
+
+        if hidden_visual is None and hidden_state is None:
+            raise Exception("No valid network configuration possible. "
+                            "There are no states or observations in this brain")
+        elif hidden_visual is not None and hidden_state is None:
+            hidden = hidden_visual
+        elif hidden_visual is None and hidden_state is not None:
+            hidden = hidden_state
+        elif hidden_visual is not None and hidden_state is not None:
+            hidden = tf.concat([hidden_visual, hidden_state], axis=1)
+
+        a_size = brain.action_space_size
+
         self.batch_size = tf.placeholder(shape=None, dtype=tf.int32, name='batch_size')
-        hidden_1 = tf.layers.dense(self.state_in, h_size, use_bias=False, activation=tf.nn.elu)
-        hidden_2 = tf.layers.dense(hidden_1, h_size, use_bias=False, activation=tf.nn.elu)
-        self.policy = tf.layers.dense(hidden_2, a_size, activation=None, use_bias=False,
-                                      kernel_initializer=c_layers.variance_scaling_initializer(factor=0.1))
-        self.probs = tf.nn.softmax(self.policy)
-        self.action = tf.multinomial(self.policy, 1)
-        self.output = tf.identity(self.action, name='action')
-        self.value = tf.layers.dense(hidden_2, 1, activation=None, use_bias=False)
-
-        self.entropy = -tf.reduce_sum(self.probs * tf.log(self.probs + 1e-10), axis=1)
-
-        self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32)
-        self.selected_actions = c_layers.one_hot_encoding(self.action_holder, a_size)
-        self.old_probs = tf.placeholder(shape=[None, a_size], dtype=tf.float32, name='old_probabilities')
-        self.responsible_probs = tf.reduce_sum(self.probs * self.selected_actions, axis=1)
-        self.old_responsible_probs = tf.reduce_sum(self.old_probs * self.selected_actions, axis=1)
-
-        PPOModel.__init__(self, self.responsible_probs, self.old_responsible_probs,
-                          self.value, self.entropy, beta, epsilon, lr, max_step)
-
-
-class VisualDiscreteControlModel(PPOModel):
-    def __init__(self, lr, o_size_h, o_size_w, a_size, h_size, epsilon, beta, max_step):
-        """
-        Creates Discrete Control Actor-Critic model for use with visual observations (images).
-        :param o_size_h: Observation height.
-        :param o_size_w: Observation width.
-        :param a_size: Action-space size.
-        :param h_size: Hidden layer size.
-        """
-        self.observation_in = tf.placeholder(shape=[None, o_size_h, o_size_w, 1], dtype=tf.float32,
-                                             name='observation_0')
-        self.conv1 = tf.layers.conv2d(self.observation_in, 32, kernel_size=[3, 3], strides=[2, 2],
-                                      use_bias=False, activation=tf.nn.elu)
-        self.conv2 = tf.layers.conv2d(self.conv1, 64, kernel_size=[3, 3], strides=[2, 2],
-                                      use_bias=False, activation=tf.nn.elu)
-        self.batch_size = tf.placeholder(shape=None, dtype=tf.int32)
-        hidden = tf.layers.dense(c_layers.flatten(self.conv2), h_size, use_bias=False, activation=tf.nn.elu)
         self.policy = tf.layers.dense(hidden, a_size, activation=None, use_bias=False,
                                       kernel_initializer=c_layers.variance_scaling_initializer(factor=0.1))
         self.probs = tf.nn.softmax(self.policy)
@@ -204,5 +206,5 @@ class VisualDiscreteControlModel(PPOModel):
         self.responsible_probs = tf.reduce_sum(self.probs * self.selected_actions, axis=1)
         self.old_responsible_probs = tf.reduce_sum(self.old_probs * self.selected_actions, axis=1)
 
-        PPOModel.__init__(self, self.responsible_probs, self.old_responsible_probs,
-                          self.value, self.entropy, beta, epsilon, lr, max_step)
+        self.create_ppo_optimizer(self.responsible_probs, self.old_responsible_probs,
+                                  self.value, self.entropy, beta, epsilon, lr, max_step)
---
+++ unity-environment/Assets/ML-Agents/Examples/Basic/Scripts/BasicDecision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDecision : MonoBehaviour, Decision {
+
+	public float[] Decide (List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+	{
+		return default(float[]);
+
+	}
+
+	public float[] MakeMemory (List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+	{
+		return default(float[]);
+		
+	}
+}
