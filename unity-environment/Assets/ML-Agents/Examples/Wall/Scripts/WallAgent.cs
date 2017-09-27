9bdc10e8fcc92d823183cf98ee5ab1b958cdd7b1
Arthur Juliani <awjuliani@gmail.com>
2017-09-27 11:41:33 -0700

    Add additional state variables

--- unity-environment/Assets/ML-Agents/Examples/Wall/Scripts/WallAgent.cs
+++ unity-environment/Assets/ML-Agents/Examples/Wall/Scripts/WallAgent.cs
@@ -7,16 +7,27 @@ public class WallAgent : Agent
 	public GameObject goalHolder;
     public GameObject block;
     public GameObject area;
+    public GameObject wall;
 
 	public override List<float> CollectState()
 	{
 		List<float> state = new List<float>();
         Vector3 velocity = GetComponent<Rigidbody>().velocity;
 		state.Add(transform.position.x - area.transform.position.x);
-		state.Add(transform.position.y - area.transform.position.x);
-		state.Add(transform.position.z + 4 - area.transform.position.x);
+		state.Add(transform.position.y - area.transform.position.y);
+		state.Add(transform.position.z + 5 - area.transform.position.z);
 
-        state.Add(velocity.x);
+        state.Add(goalHolder.transform.position.x -area.transform.position.x);
+		state.Add(goalHolder.transform.position.y -area.transform.position.y);
+		state.Add(goalHolder.transform.position.z + 5 -area.transform.position.z);
+
+		state.Add(block.transform.position.x - area.transform.position.x);
+        state.Add(block.transform.position.y - area.transform.position.y);
+		state.Add(block.transform.position.z + 5 - area.transform.position.z);
+
+		state.Add(wall.transform.localScale.y);
+
+		state.Add(velocity.x);
 		state.Add(velocity.y);
 		state.Add(velocity.z);
 
@@ -37,15 +48,15 @@ public class WallAgent : Agent
 		if (movement == 4) { directionY = 1; }
 
         Vector3 fwd = transform.TransformDirection(Vector3.down);
-        if (!Physics.Raycast(transform.position, fwd, 0.6f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.4f, 0f, 0f), fwd, 0.6f) && 
-            !Physics.Raycast(transform.position + new Vector3(-0.4f, 0f, 0f), fwd, 0.6f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, 0.4f), fwd, 0.6f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, -0.4f), fwd, 0.6f))
+        if (!Physics.Raycast(transform.position, fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(0.45f, 0f, 0f), fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(-0.45f, 0f, 0f), fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, 0.45f), fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, -0.45f), fwd, 0.55f))
         { 
             directionY = 0f;
-            directionX = directionX / 2f;
-            directionZ = directionZ / 2f;
+            directionX = directionX / 5f;
+            directionZ = directionZ / 5f;
         }
 
 		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(directionX * 40f, directionY * 300f, directionZ * 40f));
@@ -54,7 +65,7 @@ public class WallAgent : Agent
             GetComponent<Rigidbody>().velocity *= 0.95f;
         }
 
-        if (gameObject.transform.position.y < 0.0f || Mathf.Abs(gameObject.transform.position.x - area.transform.position.x) > 7f || Mathf.Abs(gameObject.transform.position.z + 4 - area.transform.position.z) > 7)
+        if (gameObject.transform.position.y < 0.0f || Mathf.Abs(gameObject.transform.position.x - area.transform.position.x) > 8f || Mathf.Abs(gameObject.transform.position.z + 5 - area.transform.position.z) > 8)
 		{
 			done = true;
 			reward = -1f;
@@ -63,10 +74,11 @@ public class WallAgent : Agent
 
 	public override void AgentReset()
 	{
-        transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1.1f, -8f) + area.transform.position;
+        transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1.1f, -9f) + area.transform.position;
 		GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-        goalHolder.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 0.1f, -1f) + area.transform.position;
-        block.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1f, Random.Range(-7f, -5f)) + area.transform.position;
+        goalHolder.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 0.1f, 0f) + area.transform.position;
+        wall.transform.localScale = new Vector3(12f, Random.Range(1f, 3f), 1f);
+        block.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1f, Random.Range(-4f, -8f)) + area.transform.position;
 	}
 
 	public override void AgentOnDone()
