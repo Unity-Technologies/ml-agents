abc5178b26b294d66d981fd31f292a433bccbc8c
Arthur Juliani <awjuliani@gmail.com>
2017-09-28 18:13:27 -0700

    Scale state for WallJump

--- unity-environment/Assets/ML-Agents/Examples/Wall/Scripts/WallAgent.cs
+++ unity-environment/Assets/ML-Agents/Examples/Wall/Scripts/WallAgent.cs
@@ -12,20 +12,20 @@ public class WallAgent : Agent
 	public override List<float> CollectState()
 	{
 		List<float> state = new List<float>();
-        Vector3 velocity = GetComponent<Rigidbody>().velocity;
-		state.Add(transform.position.x - area.transform.position.x);
-		state.Add(transform.position.y - area.transform.position.y);
-		state.Add(transform.position.z + 5 - area.transform.position.z);
+        Vector3 velocity = GetComponent<Rigidbody>().velocity / 20f;
+        state.Add((transform.position.x - area.transform.position.x) / 10f);
+        state.Add((transform.position.y - area.transform.position.y) / 10f);
+        state.Add((transform.position.z + 5 - area.transform.position.z) / 10f);
 
-        state.Add(goalHolder.transform.position.x -area.transform.position.x);
-		state.Add(goalHolder.transform.position.y -area.transform.position.y);
-		state.Add(goalHolder.transform.position.z + 5 -area.transform.position.z);
+        state.Add((goalHolder.transform.position.x - area.transform.position.x) / 10f);
+        state.Add((goalHolder.transform.position.y - area.transform.position.y) / 10f);
+        state.Add((goalHolder.transform.position.z + 5 - area.transform.position.z) / 10f);
 
-		state.Add(block.transform.position.x - area.transform.position.x);
-        state.Add(block.transform.position.y - area.transform.position.y);
-		state.Add(block.transform.position.z + 5 - area.transform.position.z);
+        state.Add((block.transform.position.x - area.transform.position.x) / 10f);
+        state.Add((block.transform.position.y - area.transform.position.y) / 10f);
+        state.Add((block.transform.position.z + 5 - area.transform.position.z) / 10f);
 
-		state.Add(wall.transform.localScale.y);
+		state.Add(wall.transform.localScale.y / 5f);
 
 		state.Add(velocity.x);
 		state.Add(velocity.y);
@@ -41,18 +41,22 @@ public class WallAgent : Agent
 		float directionX = 0;
 		float directionZ = 0;
 		float directionY = 0;
-		if (movement == 0) { directionX = -1; }
-		if (movement == 1) { directionX = 1; }
-		if (movement == 2) { directionZ = -1; }
-		if (movement == 3) { directionZ = 1; }
-		if (movement == 4) { directionY = 1; }
+		if (movement == 1) { directionX = -1; }
+		if (movement == 2) { directionX = 1; }
+		if (movement == 3) { directionZ = -1; }
+		if (movement == 4) { directionZ = 1; }
+		if (movement == 5) { directionY = 1; }
 
         Vector3 fwd = transform.TransformDirection(Vector3.down);
         if (!Physics.Raycast(transform.position, fwd, 0.55f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.45f, 0f, 0f), fwd, 0.55f) && 
-            !Physics.Raycast(transform.position + new Vector3(-0.45f, 0f, 0f), fwd, 0.55f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, 0.45f), fwd, 0.55f) && 
-            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, -0.45f), fwd, 0.55f))
+            !Physics.Raycast(transform.position + new Vector3(0.49f, 0f, 0f), fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(-0.49f, 0f, 0f), fwd, 0.55f) && 
+            !Physics.Raycast(transform.position + new Vector3(0.0f, 0f, 0.49f), fwd, 0.55f) &&
+			!Physics.Raycast(transform.position + new Vector3(0.0f, 0f, -0.49f), fwd, 0.55f) &&
+			!Physics.Raycast(transform.position + new Vector3(0.49f, 0f, 0.49f), fwd, 0.55f) &&
+			!Physics.Raycast(transform.position + new Vector3(-0.49f, 0f, 0.49f), fwd, 0.55f) &&
+			!Physics.Raycast(transform.position + new Vector3(0.49f, 0f, 0.49f), fwd, 0.55f) &&
+			!Physics.Raycast(transform.position + new Vector3(-0.49f, 0f, -0.49f), fwd, 0.55f))
         { 
             directionY = 0f;
             directionX = directionX / 5f;
@@ -77,7 +81,7 @@ public class WallAgent : Agent
         transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1.1f, -9f) + area.transform.position;
 		GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
         goalHolder.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 0.1f, 0f) + area.transform.position;
-        wall.transform.localScale = new Vector3(12f, Random.Range(1f, 3f), 1f);
+        wall.transform.localScale = new Vector3(12f, Random.Range(0f, 2f), 1f);
         block.transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 1f, Random.Range(-4f, -8f)) + area.transform.position;
 	}
 
