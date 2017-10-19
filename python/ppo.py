More fixes for normalization code

python/ppo.py
@@ -21,7 +21,7 @@ Options:
   --train                    Whether to train model, or only run inference [default: True].
   --summary-freq=<n>         Frequency at which to save training statistics [default: 10000].
   --save-freq=<n>            Frequency at which to save model [default: 50000].
-  --gamma=<n>                Reward discount rate [default: 0.99].
+  --gamma=<n>                Reward discount rate [default: 0.995].
   --lambd=<n>                Lambda parameter for GAE [default: 0.95].
   --time-horizon=<n>         How many steps to collect per agent before adding to buffer [default: 2048].
   --beta=<n>                 Strength of entropy regularization [default: 5e-3].
@@ -96,8 +96,8 @@ with tf.Session() as sess:
         sess.run(init)
     steps = sess.run(ppo_model.global_step)
     summary_writer = tf.summary.FileWriter(summary_path, sess.graph)
-    if "step" in env._resetParameters:
-        config = {"steps": int(steps)}
+    if "progress" in env._resetParameters:
+        config = {"progress": int(steps)}
     else:
         config = {}
     info = env.reset(train_mode=train_model, config=config)[brain_name]

python/ppo/models.py
@@ -268,7 +268,8 @@ class DiscreteControlModel(PPOModel):
         self.policy = tf.layers.dense(hidden, a_size, activation=None, use_bias=False,
                                       kernel_initializer=c_layers.variance_scaling_initializer(factor=0.1))
         self.probs = tf.nn.softmax(self.policy, name="action_probs")
-        self.action = tf.multinomial(self.policy, 1, name='action')
+        self.output = tf.multinomial(self.policy, 1)
+        self.output = tf.identity(self.output, name="action")
         self.value = tf.layers.dense(hidden, 1, activation=None, use_bias=False)
         self.value = tf.identity(self.value, name="value_estimate")
 

python/ppo/trainer.py
@@ -45,13 +45,9 @@ class Trainer(object):
         """
         epsi = None
 
-        new_mean, new_variance = self.running_average(info.states, steps, self.model.running_mean, self.model.running_variance)
-
-        feed_dict = {self.model.batch_size: len(info.states),
-                     self.model.new_mean: new_mean,
-                     self.model.new_variance: new_variance}
+        feed_dict = {self.model.batch_size: len(info.states)}
         run_list = [self.model.output, self.model.probs, self.model.value, self.model.entropy,
-                    self.model.learning_rate, self.model.normalized_state]
+                    self.model.learning_rate]
         if self.is_continuous:
             epsi = np.random.randn(len(info.states), env.brains[brain_name].action_space_size)
             feed_dict[self.model.epsilon] = epsi
@@ -59,13 +55,15 @@ class Trainer(object):
             feed_dict[self.model.observation_in] = np.vstack(info.observations)
         if self.use_states:
             feed_dict[self.model.state_in] = info.states
-        if self.is_training:
+        if self.is_training and env.brains[brain_name].state_space_type == "continuous":
+            new_mean, new_variance = self.running_average(info.states, steps, self.model.running_mean,
+                                                          self.model.running_variance)
+            feed_dict[self.model.new_mean] = new_mean
+            feed_dict[self.model.new_variance] = new_variance
             run_list = run_list + [self.model.update_mean, self.model.update_variance]
-            actions, a_dist, value, ent, learn_rate, norm_state, _, _ = self.sess.run(run_list, feed_dict=feed_dict)
+            actions, a_dist, value, ent, learn_rate, _, _ = self.sess.run(run_list, feed_dict=feed_dict)
         else:
-            actions, a_dist, value, ent, learn_rate, norm_state = self.sess.run(run_list, feed_dict=feed_dict)
-        #print(norm_state)
-        #print(a_dist)
+            actions, a_dist, value, ent, learn_rate = self.sess.run(run_list, feed_dict=feed_dict)
         self.stats['value_estimate'].append(value)
         self.stats['entropy'].append(ent)
         self.stats['learning_rate'].append(learn_rate)