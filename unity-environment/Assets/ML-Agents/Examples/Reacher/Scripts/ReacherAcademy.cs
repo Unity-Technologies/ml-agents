c3fa3e0e598ddbb3543f715025a4e1fb1d462ad2
Arthur Juliani <awjuliani@gmail.com>
2017-10-22 09:56:49 -0700

    Add Reacher environment

---
+++ unity-environment/Assets/ML-Agents/Examples/Reacher/Scripts/ReacherAcademy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReacherAcademy : Academy {
+
+    public float goalSize;
+
+	public override void AcademyReset()
+	{
+        goalSize = (float)resetParameters["goal_size"];
+	}
+
+	public override void AcademyStep()
+	{
+
+
+	}
+
+}
