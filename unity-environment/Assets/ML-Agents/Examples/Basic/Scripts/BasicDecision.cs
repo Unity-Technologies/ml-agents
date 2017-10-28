932ef162dd21e11d836a601d859d305c58abbe32
Arthur Juliani <awjuliani@gmail.com>
2017-10-28 14:19:28 -0400

    Add decision for Basic env

---
+++ unity-environment/Assets/ML-Agents/Examples/Basic/Scripts/BasicDecision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDecision : MonoBehaviour, Decision
+{
+
+    public float[] Decide(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+    {
+        return new float[1]{ 1f };
+
+    }
+
+    public float[] MakeMemory(List<float> state, List<Camera> observation, float reward, bool done, float[] memory)
+    {
+        return new float[0];
+
+    }
+}
