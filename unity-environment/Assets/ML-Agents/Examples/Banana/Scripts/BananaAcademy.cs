9c79bd17ec2fc9dbcee2fd519d3e4e54d2aba704
vincentpierre <vincentpierre@unity3d.com>
2017-12-13 15:26:03 -0800

    made the banana parallel for training

--- unity-environment/Assets/ML-Agents/Examples/Banana/Scripts/BananaAcademy.cs
+++ unity-environment/Assets/ML-Agents/Examples/Banana/Scripts/BananaAcademy.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BananaAcademy : Academy {
-
+    [HideInInspector]
+    public GameObject[] agents;
+	[HideInInspector]
+    public List<BananaArea> listArea;
 	public override void AcademyReset()
 	{
-        GameObject.Find("AreaPB").GetComponent<BananaArea>().ResetArea();
+		GameObject[] oldBanans = GameObject.FindGameObjectsWithTag("banana");
+		foreach (GameObject bana in oldBanans)
+		{
+			Destroy(bana);
+		}
+		agents = GameObject.FindGameObjectsWithTag("agent");
+        foreach ( BananaArea ba in listArea){
+            ba.ResetArea();
+        }
+
 	}
 
 	public override void AcademyStep()
--- unity-environment/Assets/ML-Agents/Examples/Banana/Scripts/BananaArea.cs
+++ unity-environment/Assets/ML-Agents/Examples/Banana/Scripts/BananaArea.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class BananaArea : Area {
 
+    BananaAcademy aca;
     public GameObject banana;
     public int numBananas;
     public bool respawnBananas;
 
 	// Use this for initialization
 	void Start () {
+        aca = GameObject.Find("Academy").GetComponent<BananaAcademy>();
+        aca.listArea.Add(this);
 	}
 	
 	// Update is called once per frame
@@ -21,21 +24,17 @@ public class BananaArea : Area {
 	{
         float range = 45f;
 
-        GameObject[] oldBanans = GameObject.FindGameObjectsWithTag("banana");
-        foreach (GameObject bana in oldBanans) {
-            Destroy(bana);
-        }
-
-        GameObject[] agents = GameObject.FindGameObjectsWithTag("agent");
-        foreach (GameObject agent in agents)
+        foreach (GameObject agent in aca.agents)
         {
+            if (agent.transform.parent == gameObject.transform) { 
             agent.transform.position = new Vector3(Random.Range(-range, range), 2f, Random.Range(-range, range)) + transform.position;
             agent.transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(0, 360)));
         }
+        }
 
         for (int i = 0; i < numBananas; i++) {
-            GameObject bana = Instantiate(banana, new Vector3(Random.Range(-range, range), 2f, Random.Range(-range, range)) + transform.position, banana.gameObject.transform.rotation);
-            bana.GetComponent<BananaLogic>().respawn = true;
+            GameObject bana = Instantiate(banana, new Vector3(Random.Range(-range, range),  2f, Random.Range(-range, range)) + transform.position, banana.gameObject.transform.rotation);
+            bana.GetComponent<BananaLogic>().respawn = respawnBananas;
         }
 	}
 
