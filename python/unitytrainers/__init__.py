d7b23609e854c2ba21b4b4eed98f1c5ac94a8e00
Arthur Juliani <awjuliani@gmail.com>
2018-02-20 14:32:31 -0800

    Add init for unitytrainers package

---
+++ python/unitytrainers/__init__.py
@@ -0,0 +1,7 @@
+from .buffer import *
+from .models import *
+from .trainer_controller import *
+from .bc.bc_models import *
+from .bc.bc_trainer import *
+from .ppo.ppo_models import *
+from .ppo.ppo_trainer import *
