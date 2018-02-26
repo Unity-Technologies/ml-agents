Buffer fix for properly masking gradients

python/unitytrainers/models.py
@@ -30,6 +30,11 @@ class LearningModel(object):
         return global_step, increment_step
 
     @staticmethod
+    def swish(input_activation):
+        """Swish activation function. For more info: https://arxiv.org/abs/1710.05941"""
+        return tf.multiply(input_activation, tf.nn.sigmoid(input_activation))
+
+    @staticmethod
     def create_visual_input(o_size_h, o_size_w, bw):
         if bw:
             c_channels = 1
@@ -109,9 +114,13 @@ class LearningModel(object):
             hidden = tf.layers.dense(hidden, h_size, use_bias=False, activation=activation)
         return hidden
 
-    def create_new_obs(self, num_streams, h_size, num_layers, activation_fn):
+    def create_new_obs(self, num_streams, h_size, num_layers):
         brain = self.brain
         s_size = brain.state_space_size * brain.stacked_states
+        if brain.action_space_type == "continuous":
+            activation_fn = tf.nn.tanh
+        else:
+            activation_fn = self.swish
 
         self.observation_in = []
         for i in range(brain.number_observations):
@@ -168,18 +177,20 @@ class LearningModel(object):
                                                                 initial_state=lstm_state_in,
                                                                 time_major=False,
                                                                 dtype=tf.float32)
+
         recurrent_state = tf.reshape(recurrent_state, shape=[-1, _half_point])
         return recurrent_state, tf.concat([lstm_state_out.c, lstm_state_out.h], axis=1)
 
     def create_dc_actor_critic(self, h_size, num_layers):
         num_streams = 1
-        hidden_streams = self.create_new_obs(num_streams, h_size, num_layers, tf.nn.elu)
+        hidden_streams = self.create_new_obs(num_streams, h_size, num_layers)
         hidden = hidden_streams[0]
 
-        self.prev_action = tf.placeholder(shape=[None], dtype=tf.int32, name='prev_action')
-        self.prev_action_oh = c_layers.one_hot_encoding(self.prev_action, self.a_size)
-        hidden = tf.concat([hidden, self.prev_action_oh], axis=1)
         if self.use_recurrent:
+            self.prev_action = tf.placeholder(shape=[None], dtype=tf.int32, name='prev_action')
+            self.prev_action_oh = c_layers.one_hot_encoding(self.prev_action, self.a_size)
+            hidden = tf.concat([hidden, self.prev_action_oh], axis=1)
+
             self.memory_in = tf.placeholder(shape=[None, self.m_size], dtype=tf.float32, name='recurrent_in')
             hidden, self.memory_out = self.create_recurrent_encoder(hidden, self.memory_in)
             self.memory_out = tf.identity(self.memory_out, name='recurrent_out')
@@ -193,10 +204,8 @@ class LearningModel(object):
 
         self.value = tf.layers.dense(hidden, 1, activation=None)
         self.value = tf.identity(self.value, name="value_estimate")
-
         self.entropy = -tf.reduce_sum(self.all_probs * tf.log(self.all_probs + 1e-10), axis=1)
-
-        self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32, name="action_input")
+        self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32)
         self.selected_actions = c_layers.one_hot_encoding(self.action_holder, self.a_size)
 
         self.all_old_probs = tf.placeholder(shape=[None, self.a_size], dtype=tf.float32, name='old_probabilities')
@@ -205,19 +214,20 @@ class LearningModel(object):
 
     def create_cc_actor_critic(self, h_size, num_layers):
         num_streams = 2
-        hidden_streams = self.create_new_obs(num_streams, h_size, num_layers, tf.nn.tanh)
+        hidden_streams = self.create_new_obs(num_streams, h_size, num_layers)
 
         if self.use_recurrent:
             self.memory_in = tf.placeholder(shape=[None, self.m_size], dtype=tf.float32, name='recurrent_in')
             _half_point = int(self.m_size / 2)
             hidden_policy, memory_policy_out = self.create_recurrent_encoder(
                 hidden_streams[0], self.memory_in[:, :_half_point], name='lstm_policy')
+
             hidden_value, memory_value_out = self.create_recurrent_encoder(
                 hidden_streams[1], self.memory_in[:, _half_point:], name='lstm_value')
             self.memory_out = tf.concat([memory_policy_out, memory_value_out], axis=1, name='recurrent_out')
-
-        hidden_policy = hidden_streams[0]
-        hidden_value = hidden_streams[1]
+        else:
+            hidden_policy = hidden_streams[0]
+            hidden_value = hidden_streams[1]
 
         self.mu = tf.layers.dense(hidden_policy, self.a_size, activation=None, use_bias=False,
                                   kernel_initializer=c_layers.variance_scaling_initializer(factor=0.01))
@@ -232,10 +242,10 @@ class LearningModel(object):
         a = tf.exp(-1 * tf.pow(tf.stop_gradient(self.output) - self.mu, 2) / (2 * self.sigma_sq))
         b = 1 / tf.sqrt(2 * self.sigma_sq * np.pi)
         self.all_probs = tf.multiply(a, b, name="action_probs")
-        self.probs = tf.identity(self.all_probs)
+        self.probs = tf.reduce_prod(self.all_probs, axis=1)
         self.entropy = tf.reduce_sum(0.5 * tf.log(2 * np.pi * np.e * self.sigma_sq))
         self.value = tf.layers.dense(hidden_value, 1, activation=None)
         self.value = tf.identity(self.value, name="value_estimate")
         self.all_old_probs = tf.placeholder(shape=[None, self.a_size], dtype=tf.float32,
                                             name='old_probabilities')
-        self.old_probs = tf.identity(self.all_old_probs)
+        self.old_probs = tf.reduce_prod(self.all_old_probs, axis=1)

python/unitytrainers/ppo/trainer.py
@@ -168,6 +168,8 @@ class PPOTrainer(Trainer):
                     self.model.learning_rate]
         if self.is_continuous:
             run_list.append(self.model.epsilon)
+        elif self.use_recurrent:
+            feed_dict[self.model.prev_action] = np.reshape(info.previous_actions, [-1])
         if self.use_observations:
             for i, _ in enumerate(info.observations):
                 feed_dict[self.model.observation_in[i]] = info.observations[i]
@@ -224,6 +226,8 @@ class PPOTrainer(Trainer):
                     if self.is_continuous:
                         self.training_buffer[agent_id]['epsilons'].append(epsi[idx])
                     self.training_buffer[agent_id]['actions'].append(actions[idx])
+                    self.training_buffer[agent_id]['prev_action'].append(info.previous_actions[idx])
+                    self.training_buffer[agent_id]['masks'].append(1.0)
                     self.training_buffer[agent_id]['rewards'].append(next_info.rewards[next_idx])
                     self.training_buffer[agent_id]['action_probs'].append(a_dist[idx])
                     self.training_buffer[agent_id]['value_estimates'].append(value[idx][0])
@@ -257,8 +261,11 @@ class PPOTrainer(Trainer):
                         feed_dict[self.model.state_in] = info.states
                     if self.use_recurrent:
                         feed_dict[self.model.memory_in] = info.memories
+                    if not self.is_continuous:
+                        feed_dict[self.model.prev_action] = np.reshape(info.previous_actions, [-1])
                     value_next = self.sess.run(self.model.value, feed_dict)[l]
                 agent_id = info.agents[l]
+
                 self.training_buffer[agent_id]['advantages'].set(
                     get_gae(
                         rewards=self.training_buffer[agent_id]['rewards'].get_batch(),
@@ -308,7 +315,7 @@ class PPOTrainer(Trainer):
         total_v, total_p = 0, 0
         advantages = self.training_buffer.update_buffer['advantages'].get_batch()
         self.training_buffer.update_buffer['advantages'].set(
-            (advantages - advantages.mean()) / advantages.std())
+            (advantages - advantages.mean()) / advantages.std() + 1e-10)
         for k in range(num_epoch):
             self.training_buffer.update_buffer.shuffle()
             for l in range(len(self.training_buffer.update_buffer['actions']) // batch_size):
@@ -317,9 +324,12 @@ class PPOTrainer(Trainer):
                 _buffer = self.training_buffer.update_buffer
                 feed_dict = {self.model.batch_size: batch_size,
                              self.model.sequence_length: self.sequence_length,
+                             self.model.mask_input: np.array(_buffer['masks'][start:end]).reshape(
+                                 [-1]),
                              self.model.returns_holder: np.array(_buffer['discounted_returns'][start:end]).reshape(
                                  [-1]),
-                             self.model.advantage: np.array(_buffer['advantages'][start:end]).reshape([-1, 1]),
+                             self.model.old_value: np.array(_buffer['value_estimates'][start:end]).reshape([-1]),
+                             self.model.advantage: np.array(_buffer['advantages'][start:end]).reshape([-1]),
                              self.model.all_old_probs: np.array(
                                  _buffer['action_probs'][start:end]).reshape([-1, self.brain.action_space_size])}
                 if self.is_continuous:
@@ -328,6 +338,9 @@ class PPOTrainer(Trainer):
                 else:
                     feed_dict[self.model.action_holder] = np.array(
                         _buffer['actions'][start:end]).reshape([-1])
+                    if self.use_recurrent:
+                        feed_dict[self.model.prev_action] = np.array(
+                            _buffer['prev_action'][start:end]).reshape([-1])
                 if self.use_states:
                     if self.brain.state_space_type == "continuous":
                         feed_dict[self.model.state_in] = np.array(
@@ -343,9 +356,11 @@ class PPOTrainer(Trainer):
                         feed_dict[self.model.observation_in[i]] = _obs.reshape([-1, _w, _h, _c])
                 # Memories are zeros
                 if self.use_recurrent:
-                    feed_dict[self.model.memory_in] = np.zeros([batch_size, self.m_size])
-                v_loss, p_loss, _ = self.sess.run([self.model.value_loss, self.model.policy_loss,
-                                                   self.model.update_batch], feed_dict=feed_dict)
+                    # feed_dict[self.model.memory_in] = np.zeros([batch_size, self.m_size])
+                    feed_dict[self.model.memory_in] = np.array(_buffer['memory'][start:end])[:, 0, :]
+                v_loss, p_loss, _ = self.sess.run(
+                    [self.model.value_loss, self.model.policy_loss,
+                     self.model.update_batch], feed_dict=feed_dict)
                 total_v += v_loss
                 total_p += p_loss
         self.stats['value_loss'].append(total_v)