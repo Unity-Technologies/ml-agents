b43daaf6693c939821c8bb3bcda2b48679fbaf23
Vincent Gao <vincentg@unity3d.com>
2018-03-05 11:34:41 -0800

    make the Ball3DDecision code's comment less than 80 characters in length

--- unity-environment/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
+++ unity-environment/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
@@ -11,14 +11,20 @@ public class Ball3DDecision : MonoBehaviour, Decision
         if (gameObject.GetComponent<Brain>().brainParameters.vectorActionSpaceType == StateType.continuous)
         {
             List<float> act = new List<float>();
-            // state[5] is the velocity of the ball in the x orientation. We use this number to control the Platform's z axis rotation speed, 
+
+            // state[5] is the velocity of the ball in the x orientation. 
+            // We use this number to control the Platform's z axis rotation speed, 
             // so that the Platform is tilted in the x orientation correspondingly. 
             act.Add(state[5] * rotationSpeed);
-            // state[7] is the velocity of the ball in the z orientation. We use this number to control the Platform's x axis rotation speed,  
+
+            // state[7] is the velocity of the ball in the z orientation. 
+            // We use this number to control the Platform's x axis rotation speed,  
             // so that the Platform is tilted in the z orientation correspondingly. 
             act.Add(-state[7] * rotationSpeed);
+
             return act.ToArray();
         }
+
         // If the vector action space type is discrete, then we don't do anything.     
         else
         {
