d87abe91386185a0631e0a0a27333ba16194b190
Marwan Mattar <marwan@unity3d.com>
2018-03-13 02:02:05 -0700

    Fixes to Decision.cs
    - Added API doc
    - Fixed semantics of state / observation to vector and visual observations
    - Updated decision scripts for sample environments accordingly

--- unity-environment/Assets/ML-Agents/Examples/Shared Assets/Scripts/RandomDecision.cs
+++ unity-environment/Assets/ML-Agents/Examples/Shared Assets/Scripts/RandomDecision.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomDecision : MonoBehaviour, Decision
@@ -10,36 +9,40 @@ public class RandomDecision : MonoBehaviour, Decision
 
     public void Awake()
     {
-        brainParameters = 
+        brainParameters =
             gameObject.GetComponent<Brain>().brainParameters;
         actionSpaceType = brainParameters.vectorActionSpaceType;
         actionSpaceSize = brainParameters.vectorActionSize;
     }
 
-    public float[] Decide(List<float> state, List<Texture2D> observation, 
-                          float reward, bool done, List<float> memory)
+    public float[] Decide(
+        List<float> vectorObs,
+        List<Texture2D> visualObs,
+        float reward,
+        bool done,
+        List<float> memory)
     {
-        
         if (actionSpaceType == SpaceType.continuous)
         {
             List<float> act = new List<float>();
 
             for (int i = 0; i < actionSpaceSize; i++)
             {
-                act.Add(2*Random.value-1);
+                act.Add(2 * Random.value - 1);
             }
 
             return act.ToArray();
         }
-        else
-        {
-            return new float[1] { Random.Range(0,actionSpaceSize)  };
-        }
+
+        return new float[1] { Random.Range(0, actionSpaceSize) };
     }
 
-    public List<float> MakeMemory(List<float> state,
-                                  List<Texture2D> observation, float reward,
-                                  bool done, List<float> memory)
+    public List<float> MakeMemory(
+        List<float> vectorObs,
+        List<Texture2D> visualObs,
+        float reward,
+        bool done,
+        List<float> memory)
     {
         return new List<float>();
     }
