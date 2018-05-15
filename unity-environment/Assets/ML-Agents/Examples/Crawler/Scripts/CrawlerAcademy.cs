ee45b03a74167554d6ba52c826bbf2ea5f99bdf7
Hunter <hunter@unity3d.com>
2018-05-15 12:12:56 -0700

    added ground material. updated crawlerAcademy

--- unity-environment/Assets/ML-Agents/Examples/Crawler/Scripts/CrawlerAcademy.cs
+++ unity-environment/Assets/ML-Agents/Examples/Crawler/Scripts/CrawlerAcademy.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class CrawlerAcademy : Academy
 {
+    public bool randomTargetSpawnPos;
+    public float targetSpawnRadius;
+    public Transform target;
+
     public override void InitializeAcademy()
     {
         Monitor.verticalOffset = 1f;
+        if(randomTargetSpawnPos)
+        {
+            GetRandomTargetPos();
+        }
+    }
+
+    /// <summary>
+    /// Moves target to a random position within specified radius.
+    /// </summary>
+    /// <returns>
+    /// Move target to random position.
+    /// </returns>
+    public void GetRandomTargetPos()
+    {
+        Vector3 newTargetPos = Random.insideUnitSphere * targetSpawnRadius;
+		newTargetPos.y = 5;
+		target.position = newTargetPos;
     }
 
     public override void AcademyReset()
@@ -20,4 +41,5 @@ public class CrawlerAcademy : Academy
 
 
     }
+
 }
