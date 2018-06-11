cbcc7e4443dc6e115ed23277951716a95e740fca
Karavaev Vadim <5665tm@gmail.com>
2018-06-11 21:57:53 +0300

    Develop axis support (#829)

--- unity-environment/Assets/ML-Agents/Scripts/CoreBrainPlayer.cs
+++ unity-environment/Assets/ML-Agents/Scripts/CoreBrainPlayer.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Serialization;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -13,6 +13,7 @@ namespace MLAgents
     {
         [SerializeField] private bool broadcast = true;
 
+
         [System.Serializable]
         private struct DiscretePlayerAction
         {
@@ -21,26 +22,40 @@ namespace MLAgents
         }
 
         [System.Serializable]
-        private struct ContinuousPlayerAction
+        private struct KeyContinuousPlayerAction
         {
             public KeyCode key;
             public int index;
             public float value;
         }
+        
+        [System.Serializable]
+        private struct AxisContinuousPlayerAction
+        {
+            public string axis;
+            public int index;
+            public float scale;
+        }
 
         MLAgents.Batcher brainBatcher;
 
         [SerializeField]
+        [FormerlySerializedAs("continuousPlayerActions")]
         [Tooltip("The list of keys and the value they correspond to for continuous control.")]
         /// Contains the mapping from input to continuous actions
-        private ContinuousPlayerAction[] continuousPlayerActions;
-
+        private KeyContinuousPlayerAction[] keyContinuousPlayerActions;
+        
+        [SerializeField]
+        [Tooltip("The list of axis actions.")]
+        /// Contains the mapping from input to continuous actions
+        private AxisContinuousPlayerAction[] axisContinuousPlayerActions;
+        
         [SerializeField]
         [Tooltip("The list of keys and the value they correspond to for discrete control.")]
         /// Contains the mapping from input to discrete actions
         private DiscretePlayerAction[] discretePlayerActions;
-
-        [SerializeField] private int defaultAction = 0;
+        [SerializeField]
+        private int defaultAction = 0;
 
         /// Reference to the brain that uses this CoreBrainPlayer
         public Brain brain;
@@ -81,17 +96,26 @@ namespace MLAgents
                 foreach (Agent agent in agentInfo.Keys)
                 {
                     var action = new float[brain.brainParameters.vectorActionSize];
-                    foreach (ContinuousPlayerAction cha in continuousPlayerActions)
-                    {
-                        if (Input.GetKey(cha.key))
+                    foreach (KeyContinuousPlayerAction cha in keyContinuousPlayerActions)
                         {
-                            action[cha.index] = cha.value;
+                            if (Input.GetKey(cha.key))
+                            {
+                                action[cha.index] = cha.value;
+                            }
+                        }
+    
+
+                    foreach (AxisContinuousPlayerAction axisAction in axisContinuousPlayerActions)
+                    {
+                        var axisValue = Input.GetAxis(axisAction.axis);
+                        axisValue *= axisAction.scale;
+                        if (Mathf.Abs(axisValue) > 0.0001)
+                        {
+                            action[axisAction.index] = axisValue;
                         }
                     }
-
                     agent.UpdateVectorAction(action);
-                }
-
+                } 
             }
             else
             {
@@ -119,45 +143,66 @@ namespace MLAgents
         public void OnInspector()
         {
 #if UNITY_EDITOR
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            broadcast = EditorGUILayout.Toggle(new GUIContent("Broadcast",
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        broadcast = EditorGUILayout.Toggle(
+            new GUIContent("Broadcast",
                 "If checked, the brain will broadcast states and actions to Python."), broadcast);
-            var serializedBrain = new SerializedObject(this);
-            if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        var serializedBrain = new SerializedObject(this);
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+            GUILayout.Label("Edit the continuous inputs for your actions", EditorStyles.boldLabel);
+            var keyActionsProp = serializedBrain.FindProperty("keyContinuousPlayerActions");
+            var axisActionsProp = serializedBrain.FindProperty("axisContinuousPlayerActions");
+            serializedBrain.Update();
+            EditorGUILayout.PropertyField(keyActionsProp , true);
+            EditorGUILayout.PropertyField(axisActionsProp, true);
+            serializedBrain.ApplyModifiedProperties();
+            if (keyContinuousPlayerActions == null)
             {
-                GUILayout.Label("Edit the continuous inputs for your actions",
-                    EditorStyles.boldLabel);
-                var chas = serializedBrain.FindProperty("continuousPlayerActions");
-                serializedBrain.Update();
-                EditorGUILayout.PropertyField(chas, true);
-                serializedBrain.ApplyModifiedProperties();
-                if (continuousPlayerActions == null)
-                {
-                    continuousPlayerActions = new ContinuousPlayerAction[0];
-                }
-
-                foreach (ContinuousPlayerAction cha in continuousPlayerActions)
-                {
-                    if (cha.index >= brain.brainParameters.vectorActionSize)
-                    {
-                        EditorGUILayout.HelpBox(string.Format(
-                            "Key {0} is assigned to index {1} but the action size is only of size {2}"
-                            , cha.key.ToString(), cha.index.ToString(),
-                            brain.brainParameters.vectorActionSize.ToString()), MessageType.Error);
-                    }
-                }
-
+                keyContinuousPlayerActions = new KeyContinuousPlayerAction[0];
             }
-            else
+            if (axisContinuousPlayerActions == null)
             {
-                GUILayout.Label("Edit the discrete inputs for your actions",
-                    EditorStyles.boldLabel);
-                defaultAction = EditorGUILayout.IntField("Default Action", defaultAction);
-                var dhas = serializedBrain.FindProperty("discretePlayerActions");
-                serializedBrain.Update();
-                EditorGUILayout.PropertyField(dhas, true);
-                serializedBrain.ApplyModifiedProperties();
+                axisContinuousPlayerActions = new AxisContinuousPlayerAction[0];
             }
+            foreach (KeyContinuousPlayerAction action in keyContinuousPlayerActions)
+            {
+                if (action.index >= brain.brainParameters.vectorActionSize)
+                {
+                    EditorGUILayout.HelpBox(
+                        string.Format(
+                            "Key {0} is assigned to index {1} " +
+                            "but the action size is only of size {2}"
+                        , action.key.ToString(), action.index.ToString(), 
+                            brain.brainParameters.vectorActionSize.ToString()), 
+                        MessageType.Error);
+                }
+            }
+            foreach (AxisContinuousPlayerAction action in axisContinuousPlayerActions)
+            {
+                if (action .index >= brain.brainParameters.vectorActionSize)
+                {
+                    EditorGUILayout.HelpBox(
+                        string.Format(
+                            "Axis {0} is assigned to index {1} " +
+                            "but the action size is only of size {2}"
+                        , action.axis, action.index.ToString(),
+                            brain.brainParameters.vectorActionSize.ToString()), 
+                        MessageType.Error);
+                }
+            }
+            GUILayout.Label("You can change axis settings from Edit->Project Settings->Input", 
+                EditorStyles.helpBox );
+        }
+        else
+        {
+            GUILayout.Label("Edit the discrete inputs for your actions", EditorStyles.boldLabel);
+            defaultAction = EditorGUILayout.IntField("Default Action", defaultAction);
+            var dhas = serializedBrain.FindProperty("discretePlayerActions");
+            serializedBrain.Update();
+            EditorGUILayout.PropertyField(dhas, true);
+            serializedBrain.ApplyModifiedProperties();
+        }
 #endif
         }
     }
