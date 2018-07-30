7133d54b1fc96e9347fd2c072b0a72277b849f54
Arthur Juliani <awjuliani@gmail.com>
2018-07-30 16:40:22 -0700

    Update getting started notebook

--- gym-unity/setup.py
+++ gym-unity/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, Command, find_packages
 
 setup(name='gym_unity',
-      version='0.0.1',
+      version='0.1.0',
       description='Unity Machine Learning Agents Gym Interface',
       license='Apache License 2.0',
       author='Unity Technologies',
