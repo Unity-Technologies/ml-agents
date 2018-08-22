Fixing develop after merging action masking (#1114)
Ran into problems due to inacurate merging of develop into the branch

python/unityagents/environment.py
@@ -469,8 +469,9 @@ class UnityEnvironment(object):
             mask_actions = np.ones((len(agent_info_list), total_num_actions))
             for agent_index, agent_info in enumerate(agent_info_list):
                 if agent_info.action_mask is not None:
-                    mask_actions[agent_index, :] = [
-                        0 if agent_info.action_mask[k] else 1 for k in range(total_num_actions)]
+                    if len(agent_info.action_mask) == total_num_actions:
+                        mask_actions[agent_index, :] = [
+                            0 if agent_info.action_mask[k] else 1 for k in range(total_num_actions)]
             if any([np.isnan(x.reward) for x in agent_info_list]):
                 logger.warning("An agent had a NaN reward for brain "+b)
             if any([np.isnan(x.stacked_vector_observation).any() for x in agent_info_list]):