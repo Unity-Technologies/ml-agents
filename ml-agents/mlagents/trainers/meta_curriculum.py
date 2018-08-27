49529c278b833a5e222f1ab62f92b47678383f01
Deric Pang <dericp@cs.washington.edu>
2018-08-27 10:20:00 -0700

    Fixing meta_curriculum.py line lengths.

--- ml-agents/mlagents/trainers/meta_curriculum.py
+++ ml-agents/mlagents/trainers/meta_curriculum.py
@@ -10,8 +10,8 @@ logger = logging.getLogger('mlagents.trainers')
 
 
 class MetaCurriculum(object):
-    """A MetaCurriculum holds curriculums. Each curriculum is associated to a particular
-    brain in the environment.
+    """A MetaCurriculum holds curriculums. Each curriculum is associated to a
+    particular brain in the environment.
     """
 
     def __init__(self, curriculum_folder, default_reset_parameters):
@@ -33,10 +33,12 @@ class MetaCurriculum(object):
                 brain_name = curriculum_filename.split('.')[0]
                 curriculum_filepath = \
                     os.path.join(curriculum_folder, curriculum_filename)
-                curriculum = Curriculum(curriculum_filepath, default_reset_parameters)
+                curriculum = Curriculum(curriculum_filepath,
+                                        default_reset_parameters)
 
                 # Check if any two curriculums use the same reset params.
-                if any([(parameter in curriculum.get_config().keys()) for parameter in used_reset_parameters]):
+                if any([(parameter in curriculum.get_config().keys())
+                    for parameter in used_reset_parameters]):
                     logger.warning('Two or more curriculums will '
                                 'attempt to change the same reset '
                                 'parameter. The result will be '
@@ -70,7 +72,8 @@ class MetaCurriculum(object):
             self.brains_to_curriculums[brain_name].lesson_num = lesson
 
     def increment_lessons(self, progresses):
-        """Increments all the lessons of all the curriculums in this MetaCurriculum.
+        """Increments all the lessons of all the curriculums in this
+        MetaCurriculum.
 
         Args:
             progresses (dict): A dict of brain name to progress.
@@ -80,7 +83,8 @@ class MetaCurriculum(object):
 
 
     def set_all_curriculums_to_lesson_num(self, lesson_num):
-        """Sets all the curriculums in this meta curriculum to a specified lesson number.
+        """Sets all the curriculums in this meta curriculum to a specified
+        lesson number.
 
         Args:
             lesson_num (int): The lesson number which all the curriculums will
@@ -91,7 +95,8 @@ class MetaCurriculum(object):
 
 
     def get_config(self):
-        """Get the combined configuration of all curriculums in this MetaCurriculum.
+        """Get the combined configuration of all curriculums in this
+        MetaCurriculum.
 
         Returns:
             A dict from parameter to value.
