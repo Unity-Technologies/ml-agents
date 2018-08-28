40b978d7244e23afa74a0c0bedd3365f92f7b0e7
Deric Pang <dericp@cs.washington.edu>
2018-08-28 16:22:03 -0700

    Python version is now restricted to 3.5 or 3.6.

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -39,6 +39,8 @@ setup(
         'protobuf>=3.6,<3.7',
         'grpcio>=1.11.0,<1.12.0'],
 
+    python_requires=">=3.5,<3.7",
+
     entry_points={
         'console_scripts': [
             'mlagents-learn=mlagents.learn:main',
