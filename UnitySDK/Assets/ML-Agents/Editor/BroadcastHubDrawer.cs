192cf231c920cf84a3c9f44f924cd034de91c699
vincentpierre <vincentpierre@unity3d.com>
2018-09-27 19:22:08 -0700

    refactired the BroadcastHubDrawer

--- UnitySDK/Assets/ML-Agents/Editor/BroadcastHubDrawer.cs
+++ UnitySDK/Assets/ML-Agents/Editor/BroadcastHubDrawer.cs
@@ -6,33 +6,79 @@ using UnityEditor.SceneManagement;
 
 namespace MLAgents
 {
+    /// <summary>
+    /// PropertyDrawer for BroadcastHub. Defines how BroadcastHub are displayed in the
+    /// Inspector.
+    /// </summary>
     [CustomPropertyDrawer(typeof(BroadcastHub))]
     public class BroadcastHubDrawer : PropertyDrawer
     {
-        private BroadcastHub hub;
-        private const float lineHeight = 17f;
+        private BroadcastHub _hub;
+        private const float LineHeight = 17f;
 
+        /// <summary>
+        /// Computes the height of the Drawer depending on the property it is showing
+        /// </summary>
+        /// <param name="property">The property that is being drawn.</param>
+        /// <param name="label">The label of the property being drawn.</param>
+        /// <returns>The vertical space needed to draw the property.</returns>
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             CheckInitialize(property, label);
-            var addOne = (hub.Count > 0) ? 1 : 0;
-            return (hub.Count + 2 + addOne) * lineHeight + 10f;
+            var addOne = (_hub.Count > 0) ? 1 : 0;
+            return (_hub.Count + 2 + addOne) * LineHeight + 10f;
         }
 
+        /// <summary>
+        /// Draws the BroadcastHub property
+        /// </summary>
+        /// <param name="position">Rectangle on the screen to use for the property GUI.</param>
+        /// <param name="property">The SerializedProperty of the BroadcastHub
+        /// to make the custom GUI for.</param>
+        /// <param name="label">The label of this property.</param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             CheckInitialize(property, label);
-            position.height = lineHeight;
+            position.height = LineHeight;
             EditorGUI.LabelField(position, new GUIContent(label.text, 
                 "The Broadcast Hub helps you define which Brains you want to expose to " +
                 "the external process"));
+            position.y += LineHeight;
 
             EditorGUI.BeginProperty(position, label, property);
+
+            DrawButtons(position);
+            position.y += LineHeight;
+            
+            // This is the labels for each columns
+            var brainRect = new Rect(
+                position.x + 40, position.y, position.width - 145, position.height);
+            var controlRect = new Rect(
+                position.x + position.width - 80, position.y, 80, position.height);
+            if (_hub.Count > 0)
+            {
+                EditorGUI.LabelField(brainRect, "Brains");
+                brainRect.y += LineHeight;
+                brainRect.x -= 20;
+                EditorGUI.LabelField(controlRect, "Control");
+                controlRect.y += LineHeight;
+                controlRect.x += 14;
+            }
+            DrawBrains(brainRect, controlRect);
+     
+            EditorGUI.EndProperty();
+        }
+        
+        /// <summary>
+        /// Draws the Add and Remove buttons.
+        /// </summary>
+        /// <param name="position">The position at which to draw.</param>
+        private void DrawButtons(Rect position)
+        {
             // This is the rectangle for the Add button
-            position.y += lineHeight;
             var addButtonRect = position;
             addButtonRect.x += 20;
-            if (hub.Count > 0)
+            if (_hub.Count > 0)
             {
                 addButtonRect.width /= 2;
                 addButtonRect.width -= 24;
@@ -42,7 +88,6 @@ namespace MLAgents
                     MarkSceneAsDirty();
                     AddNewItem();
                 }
-
                 // This is the rectangle for the Remove button
                 var removeButtonRect = position;
                 removeButtonRect.x = position.width / 2 + 15;
@@ -65,99 +110,72 @@ namespace MLAgents
                     AddNewItem();
                 }
             }
-            
-            // This is the labels for each columns
-            position.y += lineHeight;
-            var labelRect = position;
-            if (hub.Count > 0)
-            {
-                labelRect.x += 40;
-                labelRect.width -= 144;
-                EditorGUI.LabelField(labelRect, "Brains");
-                labelRect = position;
-                labelRect.x = position.width - 84;
-                labelRect.width = 80;
-                EditorGUI.LabelField(labelRect, "Control");
-            }
-
-            
-            // Iterate over the elements
-            for (var index = 0; index < hub.Count; index++)
-            {
-                var exposedBrains = hub.broadcastingBrains;
-                var brain = exposedBrains[index];
-                position.y += lineHeight;
-
-                // This is the rectangle for the key
-                var keyRect = position;
-                keyRect.x += 20;
-                keyRect.width -= 144;
-                EditorGUI.BeginChangeCheck();
-                var newBrain = EditorGUI.ObjectField(
-                    keyRect, brain, typeof(Brain), true) as Brain;
-                if (EditorGUI.EndChangeCheck())
-                {
-                    MarkSceneAsDirty();
-                    try
-                    {
-                        hub.broadcastingBrains.RemoveAt(index);
-                        if (!exposedBrains.Contains(newBrain))
-                        {
-                            exposedBrains.Insert(index, newBrain);
-                        }
-                        else
-                        {
-                            exposedBrains.Insert(index, null);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.Log(e.Message);
-                    }
-
-                    break;
-                }
-
-                // This is the Rectangle for the value
-                var valueRect = position;
-                valueRect.x = position.width - 64;
-                valueRect.width = 80;
-                EditorGUI.BeginChangeCheck();
-                if (brain != null)
-                {
-                    if (brain is LearningBrain)
-                    {
-                        var isTraining = hub.IsControlled(brain);
-                        isTraining = 
-                            EditorGUI.Toggle(valueRect, isTraining);
-                        hub.SetTraining(brain, isTraining);
-                    }
-                }
-
-                if (EditorGUI.EndChangeCheck())
-                {
-                    MarkSceneAsDirty();
-                }
-
-            }
-
-            EditorGUI.EndProperty();
         }
 
+        /// <summary>
+        /// Draws the Brain and Control checkbox for the brains contained in the BroadCastHub.
+        /// </summary>
+        /// <param name="brainRect">The Rect to draw the Brains.</param>
+        /// <param name="controlRect">The Rect to draw the control checkbox.</param>
+        private void DrawBrains(Rect brainRect, Rect controlRect)
+        {
+            for (var index = 0; index < _hub.Count; index++)
+            {
+                var exposedBrains = _hub.broadcastingBrains;
+                var brain = exposedBrains[index];
+                // This is the rectangle for the brain
+                EditorGUI.BeginChangeCheck();
+                var newBrain = EditorGUI.ObjectField(
+                    brainRect, brain, typeof(Brain), true) as Brain;
+                brainRect.y += LineHeight;
+                if (EditorGUI.EndChangeCheck())
+                {
+                    MarkSceneAsDirty();
+                    _hub.broadcastingBrains.RemoveAt(index);
+                    var brainToInsert = exposedBrains.Contains(newBrain) ? null : newBrain;
+                    exposedBrains.Insert(index, brainToInsert);
+                    break;
+                }
+                // This is the Rectangle for the control checkbox
+                EditorGUI.BeginChangeCheck();
+                if (brain is LearningBrain)
+                {
+                    var isTraining = _hub.IsControlled(brain);
+                    isTraining = EditorGUI.Toggle(controlRect, isTraining);
+                    _hub.SetTraining(brain, isTraining);
+                }
+                controlRect.y += LineHeight;
+                if (EditorGUI.EndChangeCheck())
+                {
+                    MarkSceneAsDirty();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ensures that the state of the Drawer is synchronized with the property.
+        /// </summary>
+        /// <param name="property">The SerializedProperty of the BroadcastHub
+        /// to make the custom GUI for.</param>
+        /// <param name="label">The label of this property.</param>
         private void CheckInitialize(SerializedProperty property, GUIContent label)
         {
-            if (hub == null)
+            if (_hub == null)
             {
                 var target = property.serializedObject.targetObject;
-                hub = fieldInfo.GetValue(target) as BroadcastHub;
-                if (hub == null)
+                _hub = fieldInfo.GetValue(target) as BroadcastHub;
+                if (_hub == null)
                 {
-                    hub = new BroadcastHub();
-                    fieldInfo.SetValue(target, hub);
+                    _hub = new BroadcastHub();
+                    fieldInfo.SetValue(target, _hub);
                 }
             }
         }
         
+        /// <summary>
+        /// Signals that the property has been modified and requires the scene to be saved for
+        /// the changes to persist. Only works when the Editor is not playing.
+        /// </summary>
         private static void MarkSceneAsDirty()
         {
             if (!EditorApplication.isPlaying)
@@ -166,24 +184,25 @@ namespace MLAgents
             }
         }
 
-        private void ClearAllBrains()
-        {
-            hub.Clear();
-        }
-
+        /// <summary>
+        /// Removes the last Brain from the BroadcastingHub
+        /// </summary>
         private void RemoveLastItem()
         {
-            if (hub.Count > 0)
+            if (_hub.Count > 0)
             {
-                hub.broadcastingBrains.RemoveAt(hub.broadcastingBrains.Count - 1);
+                _hub.broadcastingBrains.RemoveAt(_hub.broadcastingBrains.Count - 1);
             }
         }
 
+        /// <summary>
+        /// Adds a new Brain to the BroadcastHub. The value of this brain will be null.
+        /// </summary>
         private void AddNewItem()
         {
             try
             {
-                hub.broadcastingBrains.Add(null);
+                _hub.broadcastingBrains.Add(null);
             }
             catch (Exception e)
             {
