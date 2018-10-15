b874b8193170be4b586703db851bd80e6a0cb447
vincentpierre <vincentpierre@unity3d.com>
2018-10-15 14:15:01 -0700

    Removed useless imports

--- UnitySDK/Assets/ML-Agents/Editor/BrainEditor.cs
+++ UnitySDK/Assets/ML-Agents/Editor/BrainEditor.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 using UnityEditor;
-using System.Linq;
-using System.Reflection;
+
 
 namespace MLAgents
 {
