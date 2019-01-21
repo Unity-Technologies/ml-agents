03476a69693e7a4c88328e9d4aa6b18e417a8637
Mantas Puida <mantas@unity3d.com>
2019-01-21 22:46:30 +0200

    Fixed converter test to actually fail when conversion fails

--- ml-agents/tests/trainers/test_barracuda_converter.py
+++ ml-agents/tests/trainers/test_barracuda_converter.py
@@ -6,7 +6,6 @@ import tempfile
 from mlagents.trainers.tensorflow_to_barracuda import ModelConverter
 
 
-
 def test_barracuda_converter():
     path_prefix = os.path.dirname(os.path.abspath(__file__))
     tmpfile = os.path.join(tempfile._get_default_tempdir(), next(tempfile._get_candidate_names()) + '.nn')
@@ -15,10 +14,11 @@ def test_barracuda_converter():
     assert (not os.path.isfile(tmpfile))
 
     mc = ModelConverter()
-    mc.process(path_prefix+'/test.demo', tmpfile)
+    mc.process(path_prefix+'/BasicLearning.pb', tmpfile)
 
     # test if file exists after conversion
     assert (os.path.isfile(tmpfile))
+    assert (os.path.getsize(tmpfile) > 100)
 
     # cleanup
     os.remove(tmpfile)
