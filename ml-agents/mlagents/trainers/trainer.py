9010f2d3379b28f17cf078a65ba2aaa56997fca1
TashaSkyUp <TashaSkyUp@gmail.com>
2019-01-22 15:38:59 +0100

    Hopefully a fix for reported bug..
    time horizon can not be greater than buffer size.. I'm not a python programmer please check my code.

--- ml-agents/mlagents/trainers/trainer.py
+++ ml-agents/mlagents/trainers/trainer.py
@@ -40,11 +40,14 @@ class Trainer(object):
         return '''{} Trainer'''.format(self.__class__)
 
     def check_param_keys(self):
-        for k in self.param_keys:
+        for k in self.param_keys:            
             if k not in self.trainer_parameters:
                 raise UnityTrainerException(
                     "The hyper-parameter {0} could not be found for the {1} trainer of "
-                    "brain {2}.".format(k, self.__class__, self.brain_name))
+                    "brain {2}.".format(k, self.__class__, self.brain_name))        
+        if self.trainer_parameters['time_horizon'] > self.trainer_parameters['buffer_size']:
+            raise UnityTrainerException(
+                "time_horizon can not be greater than buffer_size")
 
     @property
     def parameters(self):
