67e5d873b2407e9e83d7ca89b3a12811c07b9b45
Mantas Puida <mantas@unity3d.com>
2019-01-30 15:35:45 +0200

    Improved handling of initial filesystem state

--- ml-agents/tests/trainers/test_barracuda_converter.py
+++ ml-agents/tests/trainers/test_barracuda_converter.py
@@ -10,13 +10,16 @@ def test_barracuda_converter():
     path_prefix = os.path.dirname(os.path.abspath(__file__))
     tmpfile = os.path.join(tempfile._get_default_tempdir(), next(tempfile._get_candidate_names()) + '.nn')
 
-    # make sure file doesn't exist before conversion
-    assert (not os.path.isfile(tmpfile))
+    # make sure there are no left-over files
+    if os.path.isfile(tmpfile):
+        os.remove(tmpfile)
 
     tf2bc.convert(path_prefix+'/BasicLearning.pb', tmpfile)
 
     # test if file exists after conversion
     assert (os.path.isfile(tmpfile))
+    # currently converter produces small output file even if input file is empty
+    # 100 bytes is high enough to prove that conversion was successful 
     assert (os.path.getsize(tmpfile) > 100)
 
     # cleanup
