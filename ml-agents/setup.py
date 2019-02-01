c08de6e5663a4a2f09c333b640c595311401503e
Vincent Gao <vincentg@unity3d.com>
2019-01-31 19:20:28 -0800

    added the pytest-cov package

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -37,7 +37,8 @@ setup(
         'docopt',
         'pyyaml',
         'protobuf>=3.6,<3.7',
-        'grpcio>=1.11.0,<1.12.0'],
+        'grpcio>=1.11.0,<1.12.0', 
+        'pytest-cov==2.6.1'],
 
     python_requires=">=3.6,<3.7",
 
