e0faaebc4dd047ae08a38de9b20721d06f64c7ba
Vincent-Pierre BERGES <vincentpierre@unity3d.com>
2019-02-11 10:09:45 -0800

    Ticked API : (#1696)
    * Ticked API :
     - Ticked API for pypi for mlagents
     - Ticked API for pypi for unity-gym
     - Ticked Communication number for API
     - Ticked Model Loader number for API
    
    * Ticked the API for the pytest

--- ml-agents/mlagents/envs/environment.py
+++ ml-agents/mlagents/envs/environment.py
@@ -42,7 +42,7 @@ class UnityEnvironment(object):
         atexit.register(self._close)
         self.port = base_port + worker_id
         self._buffer_size = 12000
-        self._version_ = "API-6"
+        self._version_ = "API-7"
         self._loaded = False  # If true, this means the environment was successfully loaded
         self.proc1 = None  # The process that is started. If None, no process was started
         self.communicator = self.get_communicator(worker_id, base_port)
--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mlagents',
-    version='0.6.0',
+    version='0.7.0',
     description='Unity Machine Learning Agents',
     long_description=long_description,
     long_description_content_type='text/markdown',
