4fe4db17310bd906a876afb93132d074889dbd6e
Arthur Juliani <awjuliani@gmail.com>
2019-03-13 10:31:47 -0700

    Make abstract class

--- UnitySDK/Assets/ML-Agents/Examples/SharedAssets/Scripts/RayPerception.cs
+++ UnitySDK/Assets/ML-Agents/Examples/SharedAssets/Scripts/RayPerception.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayPerception : MonoBehaviour {
+public abstract class RayPerception : MonoBehaviour {
 
 	protected List<float> perceptionBuffer = new List<float>();
 
