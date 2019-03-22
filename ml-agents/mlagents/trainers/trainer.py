4fb5819ac64c77616825d7fb1f934f900d07b667
eshvk <807898+eshvk@users.noreply.github.com>
2019-03-22 13:36:49 -0700

    Autopep8

--- ml-agents/mlagents/trainers/trainer.py
+++ ml-agents/mlagents/trainers/trainer.py
@@ -18,24 +18,31 @@ class UnityTrainerException(UnityException):
     """
     pass
 
+
 class TrainerMetrics(object):
     """
         Helper class to track, write training metrics. Tracks time since object
         of this class is initialized.
     """
-    def __init__(self, path:str, brain_name: str):
+
+    def __init__(self, path: str, brain_name: str):
         """
         :str path: Fully qualified path where CSV is stored.
         :str brain_name: Identifier for the Brain which we are training
         """
         self.path = path
         self.brain_name = brain_name
-        self.FIELD_NAMES = ['Brain name', 'Time to update policy', 
-            'Time since start of training', 'Time for last experience collection',
-            'Number of experiences used for training', 'Mean return']
+        self.FIELD_NAMES = ['Brain name', 'Time to update policy',
+                            'Time since start of training', 'Time for last experience collection',
+                            'Number of experiences used for training', 'Mean return']
         self.rows = []
         self.time_start_experience_collection = None
         self.time_training_start = time()
+        self.last_buffer_length = None
+        self.last_mean_return = None
+        self.time_policy_update_start = None
+        self.delta_last_experience_collection = None
+        self.delta_policy_update = None
 
     def start_experience_collection_timer(self):
         """
@@ -43,14 +50,14 @@ class TrainerMetrics(object):
         """
         if self.time_start_experience_collection is None:
             self.time_start_experience_collection = time()
-    
+
     def end_experience_collection_timer(self):
         """
         Inform Metrics class that experience collection is done.
         """
-        self.delta_last_experience_collection  = time() - self.time_start_experience_collection
+        self.delta_last_experience_collection = time() - self.time_start_experience_collection
         self.time_start_experience_collection = None
-    
+
     def start_policy_update_timer(self,  number_experiences: int, mean_return: float):
         """
         Inform Metrics class that policy update has started.
@@ -60,25 +67,27 @@ class TrainerMetrics(object):
         self.last_buffer_length = number_experiences
         self.last_mean_return = mean_return
         self.time_policy_update_start = time()
-    
+
     def end_policy_update(self):
         """
         Inform Metrics class that policy update has started.
         """
         self.delta_policy_update = time() - self.time_policy_update_start
-        delta_train_start =  time() - self.time_training_start
+        delta_train_start = time() - self.time_training_start
         logger.debug(" Policy Update Training Metrics for {}: "
-                    "\n\t\tTime to update Policy: {:0.3f} s \n"
-                    "\t\tTime elapsed since training: {:0.3f} s \n"
-                    "\t\tTime for experience collection: {:0.3f} s \n"
-                    "\t\tBuffer Length: {} \n"
-                    "\t\tReturns : {:0.3f}\n"
-                    .format(self.brain_name, self.delta_policy_update, delta_train_start, self.delta_last_experience_collection,
-                    self.last_buffer_length, self.last_mean_return))
+                     "\n\t\tTime to update Policy: {:0.3f} s \n"
+                     "\t\tTime elapsed since training: {:0.3f} s \n"
+                     "\t\tTime for experience collection: {:0.3f} s \n"
+                     "\t\tBuffer Length: {} \n"
+                     "\t\tReturns : {:0.3f}\n"
+                     .format(self.brain_name, self.delta_policy_update,
+                             delta_train_start, self.delta_last_experience_collection,
+                             self.last_buffer_length, self.last_mean_return))
         row = [self.brain_name]
         row.extend(format(c, '.3f') if isinstance(c) is float else c
-            for c in [self.delta_policy_update, delta_train_start, self.delta_last_experience_collection, 
-                                                self.last_buffer_length, self.last_mean_return])
+                   for c in [self.delta_policy_update, delta_train_start,
+                             self.delta_last_experience_collection,
+                             self.last_buffer_length, self.last_mean_return])
         self.rows.append(row)
 
     def write_training_metrics(self):
@@ -90,6 +99,7 @@ class TrainerMetrics(object):
             writer.writerow(self.FIELD_NAMES)
             for r in self.rows:
                 writer.writerow(r)
+
 
 class Trainer(object):
     """This class is the base class for the mlagents.trainers"""
@@ -109,12 +119,11 @@ class Trainer(object):
         self.summary_path = trainer_parameters['summary_path']
         if not os.path.exists(self.summary_path):
             os.makedirs(self.summary_path)
-        self.trainer_metrics = TrainerMetrics(path=self.summary_path + '.csv', 
+        self.trainer_metrics = TrainerMetrics(path=self.summary_path + '.csv',
                                               brain_name=self.brain_name)
         self.cumulative_returns_since_policy_update = []
         self.is_training = training
         self.stats = {}
-
 
         self.summary_writer = tf.summary.FileWriter(self.summary_path)
         self.policy = None
@@ -134,14 +143,16 @@ class Trainer(object):
         """
         Returns the trainer parameters of the trainer.
         """
-        raise UnityTrainerException("The parameters property was not implemented.")
+        raise UnityTrainerException(
+            "The parameters property was not implemented.")
 
     @property
     def graph_scope(self):
         """
         Returns the graph scope of the trainer.
         """
-        raise UnityTrainerException("The graph_scope property was not implemented.")
+        raise UnityTrainerException(
+            "The graph_scope property was not implemented.")
 
     @property
     def get_max_steps(self):
@@ -149,7 +160,8 @@ class Trainer(object):
         Returns the maximum number of steps. Is used to know when the trainer should be stopped.
         :return: The maximum number of steps of the trainer
         """
-        raise UnityTrainerException("The get_max_steps property was not implemented.")
+        raise UnityTrainerException(
+            "The get_max_steps property was not implemented.")
 
     @property
     def get_step(self):
@@ -157,7 +169,8 @@ class Trainer(object):
         Returns the number of training steps the trainer has performed
         :return: the step count of the trainer
         """
-        raise UnityTrainerException("The get_step property was not implemented.")
+        raise UnityTrainerException(
+            "The get_step property was not implemented.")
 
     @property
     def get_last_reward(self):
@@ -165,7 +178,8 @@ class Trainer(object):
         Returns the last reward the trainer has had
         :return: the new last reward
         """
-        raise UnityTrainerException("The get_last_reward property was not implemented.")
+        raise UnityTrainerException(
+            "The get_last_reward property was not implemented.")
 
     def increment_step_and_update_last_reward(self):
         """
@@ -191,7 +205,8 @@ class Trainer(object):
         :param next_info: Next AllBrainInfo.
         :param take_action_outputs: The outputs of the take action method.
         """
-        raise UnityTrainerException("The add_experiences method was not implemented.")
+        raise UnityTrainerException(
+            "The add_experiences method was not implemented.")
 
     def process_experiences(self, current_info: AllBrainInfo, next_info: AllBrainInfo):
         """
@@ -200,27 +215,31 @@ class Trainer(object):
         :param current_info: Dictionary of all current-step brains and corresponding BrainInfo.
         :param next_info: Dictionary of all next-step brains and corresponding BrainInfo.
         """
-        raise UnityTrainerException("The process_experiences method was not implemented.")
+        raise UnityTrainerException(
+            "The process_experiences method was not implemented.")
 
     def end_episode(self):
         """
-        A signal that the Episode has ended. The buffer must be reset. 
+        A signal that the Episode has ended. The buffer must be reset.
         Get only called when the academy resets.
         """
-        raise UnityTrainerException("The end_episode method was not implemented.")
+        raise UnityTrainerException(
+            "The end_episode method was not implemented.")
 
     def is_ready_update(self):
         """
         Returns whether or not the trainer has enough elements to run update model
         :return: A boolean corresponding to wether or not update_model() can be run
         """
-        raise UnityTrainerException("The is_ready_update method was not implemented.")
+        raise UnityTrainerException(
+            "The is_ready_update method was not implemented.")
 
     def update_policy(self):
         """
         Uses demonstration_buffer to update model.
         """
-        raise UnityTrainerException("The update_model method was not implemented.")
+        raise UnityTrainerException(
+            "The update_model method was not implemented.")
 
     def save_model(self):
         """
@@ -251,7 +270,8 @@ class Trainer(object):
         if global_step % self.trainer_parameters['summary_freq'] == 0 and global_step != 0:
             is_training = "Training." if self.is_training and self.get_step <= self.get_max_steps else "Not Training."
             if len(self.stats['Environment/Cumulative Reward']) > 0:
-                mean_reward = np.mean(self.stats['Environment/Cumulative Reward'])
+                mean_reward = np.mean(
+                    self.stats['Environment/Cumulative Reward'])
                 logger.info(" {}: {}: Step: {}. "
                             "Time Elapsed: {:0.3f} s "
                             "Mean "
@@ -260,7 +280,8 @@ class Trainer(object):
                             .format(self.run_id, self.brain_name,
                                     min(self.get_step, self.get_max_steps),
                                     delta_train_start,
-                                    mean_reward, np.std(self.stats['Environment/Cumulative Reward']),
+                                    mean_reward, np.std(
+                                        self.stats['Environment/Cumulative Reward']),
                                     is_training))
             else:
                 logger.info(" {}: {}: Step: {}. No episode was completed since last summary. {}"
@@ -269,7 +290,8 @@ class Trainer(object):
             for key in self.stats:
                 if len(self.stats[key]) > 0:
                     stat_mean = float(np.mean(self.stats[key]))
-                    summary.value.add(tag='{}'.format(key), simple_value=stat_mean)
+                    summary.value.add(tag='{}'.format(
+                        key), simple_value=stat_mean)
                     self.stats[key] = []
             summary.value.add(tag='Environment/Lesson', simple_value=lesson_num)
             self.summary_writer.add_summary(summary, self.get_step)
