24b14b8f9ff4bc7ea603f563bb95682e4f9385ff
Yuan Gao <vincentg@unity3d.com>
2019-04-17 11:56:54 -0700

    Added the scene for switching

---
+++ UnitySDK/Assets/ML-Agents/Scripts/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Grpc.Core;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace MLAgents
+{
+    public class Startup: MonoBehaviour
+    {
+        void Awake()
+        {   
+            string sceneName = Environment.GetEnvironmentVariable("SCENE_NAME");
+
+
+            if (SceneUtility.GetBuildIndexByScenePath(sceneName) >= 0)
+            {
+                SceneManager.LoadSceneAsync(sceneName);
+            }
+            else
+            {
+                throw new ArgumentException("The scene " + sceneName.ToString() + " doesn't exist within your build. ");
+            }
+            return;
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
