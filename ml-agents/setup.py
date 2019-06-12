a83abaad7f9b2488d311086e61b6cf35b5ca2a47
Chris Elion <chris.elion@unity3d.com>
2019-06-11 17:18:24 -0700

    Allow more flexible versions

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -27,7 +27,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "mlagents_envs==0.8.1",
-        "tensorflow>=1.7,<1.8",
+        "tensorflow>=1.7,<1.14.0",
         "Pillow>=4.2.1",
         "matplotlib",
         "numpy>=1.13.3,<=1.14.5",
@@ -36,9 +36,9 @@ setup(
         "docopt",
         "pyyaml",
         "protobuf>=3.6,<3.7",
-        "grpcio>=1.11.0,<1.12.0",
+        "grpcio>=1.11.0,<1.14.0",
         'pypiwin32==223;platform_system=="Windows"',
     ],
-    python_requires=">=3.6,<3.7",
+    python_requires=">=3.6,<3.8",
     entry_points={"console_scripts": ["mlagents-learn=mlagents.trainers.learn:main"]},
 )
