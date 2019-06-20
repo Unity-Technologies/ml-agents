137e7b0d7877cb111ee89132a4162d2837467776
Yuan Gao <xiaomaogy88@gmail.com>
2019-06-19 17:23:03 -0700

    Bumping the ml-agents, ml-agents-envs, gym_unity versions

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="mlagents",
-    version="0.8.1",
+    version="0.8.2",
     description="Unity Machine Learning Agents",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,7 +26,7 @@ setup(
     packages=["mlagents.trainers"],  # Required
     zip_safe=False,
     install_requires=[
-        "mlagents_envs==0.8.1",
+        "mlagents_envs==0.8.2",
         "tensorflow>=1.7,<1.8",
         "Pillow>=4.2.1",
         "matplotlib",
