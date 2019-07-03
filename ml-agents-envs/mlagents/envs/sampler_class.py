0a002fd895cec07af074ebccaf5cec41c8239776
sankalp04 <sankalp_patro@hotmail.com>
2019-07-03 16:35:35 -0700

    Removed check_key and replaced with **param_dict for implicit type checks

--- ml-agents-envs/mlagents/envs/sampler_class.py
+++ ml-agents-envs/mlagents/envs/sampler_class.py
@@ -1,11 +1,22 @@
 import numpy as np
+<<<<<<< HEAD
 from typing import *
 from functools import *
 from collections import OrderedDict
+=======
+from functools import *
+>>>>>>> Removed check_key and replaced with **param_dict for implicit type checks
 from abc import ABC, abstractmethod
 
 from .exception import SamplerException
 
+<<<<<<< HEAD
+=======
+class SamplerException(Exception):
+    pass
+
+class Sampler(ABC): 
+>>>>>>> Removed check_key and replaced with **param_dict for implicit type checks
 
 class Sampler(ABC):
     @abstractmethod
@@ -14,6 +25,7 @@ class Sampler(ABC):
 
 
 class UniformSampler(Sampler):
+<<<<<<< HEAD
     """
     Uniformly draws a single sample in the range [min_value, max_value).
     """
@@ -25,8 +37,17 @@ class UniformSampler(Sampler):
         self.max_value = max_value
 
     def sample_parameter(self) -> float:
+=======
+    # kwargs acts as a sink for extra unneeded args
+    def __init__(self, min_value, max_value, **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def sample_parameter(self):
+>>>>>>> Removed check_key and replaced with **param_dict for implicit type checks
         return np.random.uniform(self.min_value, self.max_value)
 
+<<<<<<< HEAD
 
 class MultiRangeUniformSampler(Sampler):
     """
@@ -48,10 +69,26 @@ class MultiRangeUniformSampler(Sampler):
         cur_min, cur_max = self.intervals[
             np.random.choice(len(self.intervals), p=self.interval_weights)
         ]
+=======
+class MultiRangeUniformSampler(Sampler):
+    def __init__(self, intervals, **kwargs):
+        self.intervals = intervals
+        # Measure the length of the intervals
+        self.interval_lengths = list(map(lambda x: abs(x[1] - x[0]), self.intervals))
+        # Cumulative size of the intervals
+        self.cum_interval_length = reduce(lambda x,y: x + y, self.interval_lengths, 0)
+        # Assign weights to an interval proportionate to the interval size
+        self.interval_weights = list(map(lambda x: x/self.cum_interval_length, self.interval_lengths))
+    
+    
+    def sample_parameter(self):
+        cur_min, cur_max = self.intervals[np.random.choice(len(self.intervals), p=self.interval_weights)]
+>>>>>>> Removed check_key and replaced with **param_dict for implicit type checks
         return np.random.uniform(cur_min, cur_max)
 
 
 class GaussianSampler(Sampler):
+<<<<<<< HEAD
     """
     Draw a single sample value from a normal (gaussian) distribution.
     This sampler is characterized by the mean and the standard deviation.
@@ -65,6 +102,14 @@ class GaussianSampler(Sampler):
 
     def sample_parameter(self) -> float:
         return np.random.normal(self.mean, self.st_dev)
+=======
+    def __init__(self, mean, var, **kwargs):
+        self.mean = mean
+        self.var = var
+    
+    def sample_parameter(self):
+        return np.random.normal(self.mean, self.var)
+>>>>>>> Removed check_key and replaced with **param_dict for implicit type checks
 
 
 class SamplerFactory:
