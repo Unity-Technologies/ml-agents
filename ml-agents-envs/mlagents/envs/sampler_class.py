65884b33728ea42f24468ddd81227961950cef4f
sankalp04 <sankalp_patro@hotmail.com>
2019-07-03 16:35:35 -0700

    Removed check_key and replaced with **param_dict for implicit type checks

--- ml-agents-envs/mlagents/envs/sampler_class.py
+++ ml-agents-envs/mlagents/envs/sampler_class.py
@@ -1,7 +1,11 @@
-import numpy as np 
+import numpy as np
+from functools import *
 from abc import ABC, abstractmethod
 
 from .exception import SamplerException
+
+class SamplerException(Exception):
+    pass
 
 class Sampler(ABC): 
 
@@ -11,69 +15,38 @@ class Sampler(ABC):
 
 
 class UniformSampler(Sampler):
-    def __init__(self, param_dict):
-        self.check_keys(param_dict)
-        self.min_value = param_dict["min"]
-        self.max_value = param_dict["max"]
-
-
-    def check_keys(self, param_dict):
-        keys = ["min", "max"]
-        for key in keys:
-            if key not in param_dict:
-                raise SamplerException(
-                    key + " is required to use Uniform Sampling but" 
-                    " this value isn't provided. Please recheck"
-                    " the documentation to ensure that the sampler file"
-                    " matches the required format."
-                )
+    # kwargs acts as a sink for extra unneeded args
+    def __init__(self, min_value, max_value, **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
 
     def sample_parameter(self):
         return np.random.uniform(self.min_value, self.max_value)
     
 
 class MultiRangeUniformSampler(Sampler):
-    def __init__(self, param_dict):
-        self.check_keys(param_dict)
-        self.intervals = param_dict["intervals"]
+    def __init__(self, intervals, **kwargs):
+        self.intervals = intervals
+        # Measure the length of the intervals
+        self.interval_lengths = list(map(lambda x: abs(x[1] - x[0]), self.intervals))
+        # Cumulative size of the intervals
+        self.cum_interval_length = reduce(lambda x,y: x + y, self.interval_lengths, 0)
+        # Assign weights to an interval proportionate to the interval size
+        self.interval_weights = list(map(lambda x: x/self.cum_interval_length, self.interval_lengths))
     
-    def check_keys(self, param_dict):
-        keys = ["intervals"]
-        for key in keys:
-            if key not in param_dict:
-                raise SamplerException(
-                    key + " is required to use MultiRange Uniform sampler but" 
-                    " this value isn't provided. Please recheck"
-                    " the documentation to ensure that the sampler file"
-                    " matches the required format."
-                )
-
     
     def sample_parameter(self):
-        cur_min, cur_max = self.intervals[np.random.randint(len(self.intervals))]
+        cur_min, cur_max = self.intervals[np.random.choice(len(self.intervals), p=self.interval_weights)]
         return np.random.uniform(cur_min, cur_max)
 
 
 class GaussianSampler(Sampler):
-    def __init__(self, param_dict):
-        self.check_keys(param_dict)
-        self.mean = param_dict["mean"]
-        self.var = param_dict["var"]
+    def __init__(self, mean, var, **kwargs):
+        self.mean = mean
+        self.var = var
     
     def sample_parameter(self):
         return np.random.normal(self.mean, self.var)
-
-    def check_keys(self, param_dict):
-        keys = ["mean", "var"]
-        for key in keys:
-            if key not in param_dict:
-                raise SamplerException(
-                    key + " is required to use MultiRange Uniform sampler but" 
-                    " this value isn't provided. Please recheck"
-                    " the documentation to ensure that the sampler file"
-                    " matches the required format."
-                )
-
 
 
 # To introduce new sampling methods, just need to 'register' them to this sampler factory
@@ -88,25 +61,39 @@ class SamplerFactory:
     def register_sampler(name, sampler_cls):
         SamplerFactory.NAME_TO_CLASS[name] = sampler_cls
     
+    @staticmethod
     def init_sampler_class(name, param_dict):
+        if name not in SamplerFactory.NAME_TO_CLASS:
+            raise SamplerException(
+                name + " sampler is not registered in the SamplerFactory."
+                " Use the register_sample method to register the string"
+                " associated to your sampler in the SamplerFactory."
+            )
         sampler_cls = SamplerFactory.NAME_TO_CLASS[name]
-        return sampler_cls(param_dict)
+        return sampler_cls(**param_dict)
 
 
 class SamplerManager:
     def __init__(self, reset_param_dict):
         self.reset_param_dict = reset_param_dict
-        self.sampler_factory = SamplerFactory()
         self.samplers = {}
+        if reset_param_dict == None:
+            return
         for param_name, cur_param_dict in self.reset_param_dict.items():
-            # cur_param_dict = reset_param_dict[param_name]
-            sampler_name = cur_param_dict["sampler-type"]
+            if "sampler-type" not in cur_param_dict:
+                raise SamplerException(
+                    "'sampler_type' argument hasn't been supplied for the {0} parameter".format(param_name)
+                )
+            sampler_name = cur_param_dict.pop("sampler-type")
             param_sampler = SamplerFactory.init_sampler_class(sampler_name, cur_param_dict)
 
             self.samplers[param_name] = param_sampler
 
-    def sample_all(self, train_mode = True):
+    def sample_all(self):
         res = {}
-        for param_name, param_sampler in self.samplers.items():
-            res[param_name] = param_sampler.sample_parameter()
+        if self.samplers == {}:
+            pass
+        else:
+            for param_name, param_sampler in list(self.samplers.items()):
+                res[param_name] = param_sampler.sample_parameter()
         return res
