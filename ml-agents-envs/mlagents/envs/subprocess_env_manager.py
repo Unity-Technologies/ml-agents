3e242c71a644ff6cc3964a0d8580a1750e8649c8
Chris Elion <celion@gmail.com>
2019-07-11 11:37:59 -0700

    python timers (#2180)
    * Timer proof-of-concept
    
    * micro optimizations
    
    * add some timers
    
    * cleanup, add asserts
    
    * Cleanup (no start/end methods) and handle exceptions
    
    * unit test and decorator
    
    * move output code, add a decorator
    
    * cleanup
    
    * module docstring
    
    * actually write the timings when done with training
    
    * use __qualname__ instead
    
    * add a few more timers
    
    * fix mock import
    
    * fix unit test
    
    * don't need fwd reference
    
    * cleanup root
    
    * always write timers, add comments
    
    * undo accidental change

--- ml-agents-envs/mlagents/envs/subprocess_env_manager.py
+++ ml-agents-envs/mlagents/envs/subprocess_env_manager.py
@@ -6,6 +6,7 @@ from multiprocessing import Process, Pipe
 from multiprocessing.connection import Connection
 from mlagents.envs.base_unity_environment import BaseUnityEnvironment
 from mlagents.envs.env_manager import EnvManager, StepInfo
+from mlagents.envs.timers import timed, hierarchical_timer
 from mlagents.envs import AllBrainInfo, BrainParameters, ActionInfo
 
 
@@ -128,9 +129,10 @@ class SubprocessEnvManager(EnvManager):
             env_worker.previous_all_action_info = all_action_info
             env_worker.send("step", all_action_info)
 
-        step_brain_infos: List[AllBrainInfo] = [
-            self.env_workers[i].recv().payload for i in range(len(self.env_workers))
-        ]
+        with hierarchical_timer("recv"):
+            step_brain_infos: List[AllBrainInfo] = [
+                self.env_workers[i].recv().payload for i in range(len(self.env_workers))
+            ]
         steps = []
         for i in range(len(step_brain_infos)):
             env_worker = self.env_workers[i]
@@ -171,6 +173,7 @@ class SubprocessEnvManager(EnvManager):
         for env in self.env_workers:
             env.send(name, payload)
 
+    @timed
     def _take_step(self, last_step: StepInfo) -> Dict[str, ActionInfo]:
         all_action_info: Dict[str, ActionInfo] = {}
         for brain_name, brain_info in last_step.current_all_brain_info.items():
