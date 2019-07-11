533c9af0e2defeee2d1a62df2ac271f4879cba8a
sankalp04 <sankalp_patro@hotmail.com>
2019-07-11 14:41:53 -0700

    Implement the reset parameters for 3D ball using the feedback from earlier PR merged into this branch

--- UnitySDK/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAcademy.cs
+++ UnitySDK/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAcademy.cs
@@ -7,11 +7,10 @@ public class Ball3DAcademy : Academy
 {
 	public override void AcademyReset()
 	{
-		Physics.gravity = new Vector3(0, -resetParameters["gravity"], 0);
+        Physics.gravity = new Vector3(0, -resetParameters["gravity"], 0);
 	}
 
 	public override void AcademyStep()
 	{
 
 	}
-}
