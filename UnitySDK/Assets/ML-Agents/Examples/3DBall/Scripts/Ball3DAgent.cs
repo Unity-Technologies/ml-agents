3941b71a2d989a31721a49a22d73a2ee7e052bf2
sankalp04 <sankalp_patro@hotmail.com>
2019-07-11 14:41:53 -0700

    Implement the reset parameters for 3D ball using the feedback from earlier PR merged into this branch

--- UnitySDK/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
+++ UnitySDK/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
@@ -8,10 +8,14 @@ public class Ball3DAgent : Agent
     [Header("Specific to Ball3D")]
     public GameObject ball;
     private Rigidbody ballRb;
+    private ResetParameters resetParams;
 
     public override void InitializeAgent()
     {
         ballRb = ball.GetComponent<Rigidbody>();
+        var academy = Object.FindObjectOfType<Academy>() as Academy;
+        resetParams = academy.resetParameters;
+        SetResetParameters();
     }
 
     public override void CollectObservations()
@@ -24,7 +28,7 @@ public class Ball3DAgent : Agent
 
     public override void AgentAction(float[] vectorAction, string textAction)
     {
-        
+
         if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
         {
             var actionZ = 2f * Mathf.Clamp(vectorAction[0], -1f, 1f);
@@ -63,7 +67,20 @@ public class Ball3DAgent : Agent
         ballRb.velocity = new Vector3(0f, 0f, 0f);
         ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f))
                                       + gameObject.transform.position;
-
+        //Reset the parameters when the Agent is reset.
+        SetResetParameters();
     }
 
+    public void SetBall()
+    {
+        //Set the attributes of the ball by fetching the information from the academy
+        ballRb.mass = resetParams["mass"];
+        var scale = resetParams["scale"];
+        ball.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    public void SetResetParameters()
+    {
+        SetBall();
+    }
 }
