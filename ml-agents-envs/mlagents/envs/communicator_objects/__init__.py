1d79eb9adeac4b42afd51f980d5373402d9bd08e
Chris Elion <chris.elion@unity3d.com>
2019-07-19 13:54:19 -0700

    clean up communicator import *'s

--- ml-agents-envs/mlagents/envs/communicator_objects/__init__.py
+++ ml-agents-envs/mlagents/envs/communicator_objects/__init__.py
@@ -1,22 +1,25 @@
-from .agent_action_proto_pb2 import *
-from .agent_info_proto_pb2 import *
-from .brain_parameters_proto_pb2 import *
-from .command_proto_pb2 import *
-from .custom_action_pb2 import *
-from .custom_observation_pb2 import *
-from .custom_reset_parameters_pb2 import *
-from .demonstration_meta_proto_pb2 import *
-from .engine_configuration_proto_pb2 import *
-from .environment_parameters_proto_pb2 import *
-from .header_pb2 import *
-from .resolution_proto_pb2 import *
-from .space_type_proto_pb2 import *
-from .unity_input_pb2 import *
-from .unity_message_pb2 import *
-from .unity_output_pb2 import *
-from .unity_rl_initialization_input_pb2 import *
-from .unity_rl_initialization_output_pb2 import *
-from .unity_rl_input_pb2 import *
-from .unity_rl_output_pb2 import *
-from .unity_to_external_pb2 import *
-from .unity_to_external_pb2_grpc import *
+from .agent_action_proto_pb2 import AgentActionProto
+from .agent_info_proto_pb2 import AgentInfoProto
+from .brain_parameters_proto_pb2 import BrainParametersProto
+from .command_proto_pb2 import CommandProto
+from .custom_action_pb2 import CustomAction
+from .custom_observation_pb2 import CustomObservation
+from .custom_reset_parameters_pb2 import CustomResetParameters
+from .demonstration_meta_proto_pb2 import DemonstrationMetaProto
+from .engine_configuration_proto_pb2 import EngineConfigurationProto
+from .environment_parameters_proto_pb2 import EnvironmentParametersProto
+from .header_pb2 import Header
+from .resolution_proto_pb2 import ResolutionProto
+from .space_type_proto_pb2 import SpaceTypeProto
+from .unity_input_pb2 import UnityInput
+from .unity_message_pb2 import UnityMessage
+from .unity_output_pb2 import UnityOutput
+from .unity_rl_initialization_input_pb2 import UnityRLInitializationInput
+from .unity_rl_initialization_output_pb2 import UnityRLInitializationOutput
+from .unity_rl_input_pb2 import UnityRLInput
+from .unity_rl_output_pb2 import UnityRLOutput
+from .unity_to_external_pb2_grpc import (
+    UnityToExternalServicer,
+    UnityToExternalStub,
+    add_UnityToExternalServicer_to_server,
+)
