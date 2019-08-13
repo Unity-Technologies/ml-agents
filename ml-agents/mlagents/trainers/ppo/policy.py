8555646a4c86e46547db38c3cfd239e37630450c
Ervin Teng <ervin@unity3d.com>
2019-08-12 18:42:40 -0700

    Fix issue with merge

--- ml-agents/mlagents/trainers/ppo/policy.py
+++ ml-agents/mlagents/trainers/ppo/policy.py
@@ -94,7 +94,8 @@ class PPOPolicy(TFPolicy):
             {
                 "action": self.model.output,
                 "log_probs": self.model.all_log_probs,
-                "value": self.model.value_heads,
+                "value_heads": self.model.value_heads,
+                "value": self.model.value,
                 "entropy": self.model.entropy,
                 "learning_rate": self.model.learning_rate,
             }
