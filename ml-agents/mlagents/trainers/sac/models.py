2c6af82ebb67380313279fc903adbce39eb9995b
Ervin Teng <ervin@unity3d.com>
2019-09-05 11:23:33 -0700

    Decrease discrete target entropy

--- ml-agents/mlagents/trainers/sac/models.py
+++ ml-agents/mlagents/trainers/sac/models.py
@@ -8,8 +8,8 @@ import tensorflow.contrib.layers as c_layers
 LOG_STD_MAX = 2
 LOG_STD_MIN = -20
 EPSILON = 1e-6  # Small value to avoid divide by zero
-DISCRETE_TARGET_ENTROPY_SCALE = 0.3  # Roughly equal to e-greedy 0.1
-CONTINUOUS_TARGET_ENTROPY_SCALE = 1.0
+DISCRETE_TARGET_ENTROPY_SCALE = 0.2  # Roughly equal to e-greedy 0.05
+CONTINUOUS_TARGET_ENTROPY_SCALE = 1.0  # TODO: Make these an optional hyperparam.
 
 LOGGER = logging.getLogger("mlagents.trainers")
 
