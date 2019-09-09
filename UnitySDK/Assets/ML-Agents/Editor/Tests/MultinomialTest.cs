03a8f63612f869e5a4440b19b16b1113ce6cc50a
Marwan Mattar <marwan@unity3d.com>
2019-09-09 15:38:18 -0700

    Improvements to Multinomial(Test).cs
    Fixed the tests for Multinomial as they didn’t include cumulative mass functions.
    
    Also, improved the documentation.

--- UnitySDK/Assets/ML-Agents/Editor/Tests/MultinomialTest.cs
+++ UnitySDK/Assets/ML-Agents/Editor/Tests/MultinomialTest.cs
@@ -17,11 +17,35 @@ namespace MLAgents.Tests
         }
 
         [Test]
+        public void TestDim1Unscaled()
+        {
+            var m = new Multinomial(2018);
+            var cdf = new[] {0.1f};
+
+            Assert.AreEqual(0, m.Sample(cdf));
+            Assert.AreEqual(0, m.Sample(cdf));
+            Assert.AreEqual(0, m.Sample(cdf));
+        }
+
+        [Test]
         public void TestDim3()
         {
             var m = new Multinomial(2018);
-            var cdf = new[] {0.1f, 0.2f, 0.7f};
+            var cdf = new[] {0.1f, 0.3f, 1.0f};
 
+            Assert.AreEqual(2, m.Sample(cdf));
+            Assert.AreEqual(2, m.Sample(cdf));
+            Assert.AreEqual(2, m.Sample(cdf));
+            Assert.AreEqual(1, m.Sample(cdf));
+        }
+
+        [Test]
+        public void TestDim3Unscaled()
+        {
+            var m = new Multinomial(2018);
+            var cdf = new[] {0.05f, 0.15f, 0.5f};
+
+            Assert.AreEqual(2, m.Sample(cdf));
             Assert.AreEqual(2, m.Sample(cdf));
             Assert.AreEqual(2, m.Sample(cdf));
             Assert.AreEqual(1, m.Sample(cdf));
--- UnitySDK/Assets/ML-Agents/Scripts/InferenceBrain/Utils/Multinomial.cs
+++ UnitySDK/Assets/ML-Agents/Scripts/InferenceBrain/Utils/Multinomial.cs
@@ -1,22 +1,43 @@
 ﻿namespace MLAgents.InferenceBrain.Utils
 {
     /// <summary>
-    /// Multinomial - Draws samples from a multinomial distribution given the CDF.
+    /// Multinomial - Draws samples from a multinomial distribution given a (potentially unscaled)
+    /// cumulative mass function (CMF). This means that the CMF need not "end" with probability
+    /// mass of 1.0. For instance: [0.1, 0.2, 0.5] is a valid (unscaled). What is important is
+    /// that it is a cumulative function, not a probability function. In other words,
+    /// entry[i] = P(x \le i), NOT P(i - 1 \le x \lt i).
+    /// (\le stands for less than or equal to while \lt is strictly less than).
     /// </summary>
     public class Multinomial
     {
         private readonly System.Random _random;
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="seed">
+        /// Seed for the random number generator used in the sampling process.
+        /// </param>
         public Multinomial(int seed)
         {
             _random = new System.Random(seed);
         }
 
-        public int Sample(float[] cdf)
+        /// <summary>
+        /// Samples from the Multinomial distribution defined by the provided cumulative
+        /// mass function.
+        /// </summary>
+        /// <param name="cmf">
+        /// Cumulative mass function, which may be unscaled. The entries in this array need
+        /// to be monotonic (always increasing). If the CMF is scaled, then the last entry in
+        /// the array will be 1.0.
+        /// </param>
+        /// <returns>A sampled index from the CMF ranging from 0 to cmf.Length-1.</returns>
+        public int Sample(float[] cmf)
         {
-            var p = (float) _random.NextDouble() * cdf[cdf.Length - 1];
+            var p = (float) _random.NextDouble() * cmf[cmf.Length - 1];
             var cls = 0;
-            while (cdf[cls] < p)
+            while (cmf[cls] < p)
             {
                 ++cls;
             }
