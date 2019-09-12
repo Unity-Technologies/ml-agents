f14251c9001569a82b00ac19eceffc9477bb647b
Chris Elion <chris.elion@unity3d.com>
2019-09-12 10:31:38 -0700

    sort deps, restore unit test, cleanup CI

--- ml-agents-envs/setup.py
+++ ml-agents-envs/setup.py
@@ -19,12 +19,11 @@ setup(
     packages=["mlagents.envs", "mlagents.envs.communicator_objects"],  # Required
     zip_safe=False,
     install_requires=[
-        "Pillow>=4.2.1",
-        "numpy>=1.13.3,<=1.17.0",
-        "pytest>=3.2.2,<4.0.0",
-        "protobuf>=3.6,<3.7",
-        "grpcio>=1.11.0",
         "cloudpickle",
+        "grpcio>=1.11.0",
+        "numpy>=1.13.3,<=1.17.0",
+        "Pillow>=4.2.1",
+        "protobuf>=3.6,<3.7",
     ],
     python_requires=">=3.5",
 )
