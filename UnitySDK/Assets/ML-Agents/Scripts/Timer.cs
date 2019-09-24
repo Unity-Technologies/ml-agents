df1b0f9509726e0b519214d25e6af88612348219
Chris Elion <chris.elion@unity3d.com>
2019-09-24 14:55:20 -0700

    docstrings

--- UnitySDK/Assets/ML-Agents/Scripts/Timer.cs
+++ UnitySDK/Assets/ML-Agents/Scripts/Timer.cs
@@ -19,19 +19,42 @@ namespace MLAgents
         static string s_Separator = ".";
         static double s_TicksToSeconds = 1.0 / 10000000.0; // 100 ns per tick
 
+        /// <summary>
+        ///  Full name of the node. This is the node's parents full name concatenated with this node's name
+        /// </summary>
         string m_FullName;
 
+        /// <summary>
+        /// Child nodes, indexed by name.
+        /// </summary>
         [DataMember(Name="Children", Order=999)]
         Dictionary<string, TimerNode> m_Children;
 
+        /// <summary>
+        /// Custom sampler used to add timings to the profiler.
+        /// </summary>
         private CustomSampler m_Sampler;
 
+        /// <summary>
+        /// Number of total ticks elapsed for this node.
+        /// </summary>
         long m_RawTicks = 0;
+
+        /// <summary>
+        /// If the node is currently running, the time (in ticks) when the node was started.
+        /// If the node is not running, is set to 0.
+        /// </summary>
         long m_TickStart = 0;
 
+        /// <summary>
+        /// Number of times the corresponding code block has been called.
+        /// </summary>
         [DataMember(Name="RawTotalCalls")]
         int m_RawCalls = 0;
 
+        /// <summary>
+        /// Total elapsed seconds.
+        /// </summary>
         [DataMember]
         public double RawTotalSeconds
         {
@@ -43,17 +66,20 @@ namespace MLAgents
         {
             m_FullName = name;
             m_Sampler = CustomSampler.Create(m_FullName);
-
-            // TODO Don't create child dict until needed?
-            m_Children = new Dictionary<string, TimerNode>();
         }
 
+        /// <summary>
+        /// Start timing a block of code.
+        /// </summary>
         public void Begin()
         {
             m_Sampler.Begin();
             m_TickStart = System.DateTime.Now.Ticks;
         }
 
+        /// <summary>
+        /// Stop timing a block of code, and increment internal counts.
+        /// </summary>
         public void End()
         {
             var elapsed = System.DateTime.Now.Ticks - m_TickStart;
@@ -63,8 +89,22 @@ namespace MLAgents
             m_Sampler.End();
         }
 
+        /// <summary>
+        /// Return a child node for the given name.
+        /// The children dictionary will be created if it does not already exist, and
+        /// a new Node will be created if it's not already in the dictionary.
+        /// Note that these allocations only happen once for a given timed block.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public TimerNode GetChild(string name)
         {
+            // Lazily create the children dictionary.
+            if (m_Children == null)
+            {
+                m_Children = new Dictionary<string, TimerNode>();
+            }
+
             if (!m_Children.ContainsKey(name))
             {
                 var childFullName = m_FullName + s_Separator + name;
@@ -76,6 +116,12 @@ namespace MLAgents
             return m_Children[name];
         }
 
+        /// <summary>
+        /// Recursively form a string representing the current timer information.
+        /// </summary>
+        /// <param name="parentName"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
         public string DebugGetTimerString(string parentName = "", int level = 0)
         {
             string indent = new string(' ', 2 * level); // TODO generalize
@@ -91,23 +137,41 @@ namespace MLAgents
             }
 
             // TODO stringbuilder? overkill?
-            foreach (TimerNode c in m_Children.Values)
+            if (m_Children != null)
             {
-                timerString += c.DebugGetTimerString(m_FullName, level + 1);
+                foreach (TimerNode c in m_Children.Values)
+                {
+                    timerString += c.DebugGetTimerString(m_FullName, level + 1);
+                }
             }
-
             return timerString;
         }
     }
 
+    /// <summary>
+    /// A "stack" of timers that allows for lightweight hierarchical profiling of long-running processes.
+    /// Example usage:
+    ///
+    /// var myTimer = TimerStack("root");
+    /// using(myTimer.Scoped("foo"))
+    /// {
+    ///     doSomeWork();
+    ///     for (int i=0; i<5; i++)
+    ///     {
+    ///         using(myTimer.Scoped("bar"))
+    ///         {
+    ///             doSomeMoreWork();
+    ///         }
+    ///     }
+    /// }
+    /// </summary>
     public class TimerStack : System.IDisposable
     {
         Stack<TimerNode> m_Stack;
         public TimerNode m_RootNode;
 
-        public TimerStack(string rootName)
+        public TimerStack(string rootName = "root")
         {
-            Profiler.enabled = true;
             m_Stack = new Stack<TimerNode>();
             m_RootNode = new TimerNode(rootName);
             m_Stack.Push(m_RootNode);
@@ -118,48 +182,75 @@ namespace MLAgents
             TimerNode current = m_Stack.Peek();
             TimerNode next = current.GetChild(name);
             m_Stack.Push(next);
+            next.Begin();
             return next;
         }
 
-        private TimerNode Pop()
+        private void Pop()
         {
-            return m_Stack.Pop();
+            var node = m_Stack.Pop();
+            node.End();
         }
 
         /// <summary>
-        /// Start a scoped timer. This should be used with the "using" directive
+        /// Start a scoped timer. This should be used with the "using" statement.
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
         public TimerStack Scoped(string name)
         {
-            var node = Push(name);
-            node.Begin();
+            Push(name);
             return this;
         }
 
+        /// <summary>
+        /// Closes the current scoped timer. This should never be called directly, only
+        /// at the end of a "using" statement.
+        /// Note that the instance is not actually disposed of; this is just to allow it to be used
+        /// conveniently with "using".
+        /// </summary>
         public void Dispose()
         {
-            var node = Pop();
-            node.End();
+            Pop();
         }
 
+        /// <summary>
+        /// Get a string representation of the timers.
+        /// Potentially slow so call sparingly.
+        /// </summary>
+        /// <returns></returns>
         public string DebugGetTimerString()
         {
             return m_RootNode.DebugGetTimerString();
         }
 
-        public void SaveJsonTimers(string name)
+        /// <summary>
+        /// Save the timers in JSON format to the provided filename.
+        /// If the filename is null, a default one will be used.
+        /// </summary>
+        /// <param name="filename"></param>
+        public void SaveJsonTimers(string filename=null)
         {
-            // Create a stream to serialize the object to.
-            // TODO better interface - pass stream in?
-            var fullpath = Path.GetFullPath(".");
-            var fs = new FileStream($"{fullpath}/csharp_{name}_timers.json", FileMode.Create, FileAccess.Write);
+            if (filename == null)
+            {
+                var fullpath = Path.GetFullPath(".");
+                filename = $"{fullpath}/csharp_timers.json";
+            }
+            var fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            SaveJsonTimers(fs);
+            fs.Close();
+        }
+
+        /// <summary>
+        /// Write the timers in JSON format to the provided stream.
+        /// </summary>
+        /// <param name="stream"></param>
+        public void SaveJsonTimers(Stream stream)
+        {
             var jsonSettings = new DataContractJsonSerializerSettings();
             jsonSettings.UseSimpleDictionaryFormat = true;
             var ser = new DataContractJsonSerializer(typeof(TimerNode), jsonSettings);
-            ser.WriteObject(fs, m_RootNode);
-            fs.Close();
+            ser.WriteObject(stream, m_RootNode);
         }
     }
 
