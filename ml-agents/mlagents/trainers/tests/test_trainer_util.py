074124b577ded1a7ca8b1e8a9209fdfd2251874e
vincentpierre <vincentpierre@unity3d.com>
2019-10-01 18:45:40 -0700

    Feature Deprecation : Online Behavioral Cloning
    In this PR :
     - Delete the online_bc_trainer
     - Delete the tests for online bc
     - delete the configuration file for online bc training

--- ml-agents/mlagents/trainers/tests/test_trainer_util.py
+++ ml-agents/mlagents/trainers/tests/test_trainer_util.py
@@ -9,7 +9,6 @@ from mlagents.trainers.trainer_util import load_config, _load_config
 from mlagents.trainers.trainer_metrics import TrainerMetrics
 from mlagents.trainers.ppo.trainer import PPOTrainer
 from mlagents.trainers.bc.offline_trainer import OfflineBCTrainer
-from mlagents.trainers.bc.online_trainer import OnlineBCTrainer
 from mlagents.envs.exception import UnityEnvironmentException
 
 
@@ -19,38 +18,6 @@ def dummy_config():
         """
         default:
             trainer: ppo
-            batch_size: 32
-            beta: 5.0e-3
-            buffer_size: 512
-            epsilon: 0.2
-            gamma: 0.99
-            hidden_units: 128
-            lambd: 0.95
-            learning_rate: 3.0e-4
-            max_steps: 5.0e4
-            normalize: true
-            num_epoch: 5
-            num_layers: 2
-            time_horizon: 64
-            sequence_length: 64
-            summary_freq: 1000
-            use_recurrent: false
-            memory_size: 8
-            use_curiosity: false
-            curiosity_strength: 0.0
-            curiosity_enc_size: 1
-        """
-    )
-
-
-@pytest.fixture
-def dummy_online_bc_config():
-    return yaml.safe_load(
-        """
-        default:
-            trainer: online_bc
-            brain_to_imitate: ExpertBrain
-            batches_per_epoch: 16
             batch_size: 32
             beta: 5.0e-3
             buffer_size: 512
@@ -190,49 +157,6 @@ def test_initialize_trainer_parameters_override_defaults(BrainParametersMock):
         )
         assert "testbrain" in trainers
         assert isinstance(trainers["testbrain"], OfflineBCTrainer)
-
-
-@patch("mlagents.envs.brain.BrainParameters")
-def test_initialize_online_bc_trainer(BrainParametersMock):
-    summaries_dir = "test_dir"
-    run_id = "testrun"
-    model_path = "model_dir"
-    keep_checkpoints = 1
-    train_model = True
-    load_model = False
-    seed = 11
-
-    base_config = dummy_online_bc_config()
-    expected_config = base_config["default"]
-    expected_config["summary_path"] = summaries_dir + f"/{run_id}_testbrain"
-    expected_config["model_path"] = model_path + "/testbrain"
-    expected_config["keep_checkpoints"] = keep_checkpoints
-
-    brain_params_mock = BrainParametersMock()
-    external_brains = {"testbrain": brain_params_mock}
-
-    def mock_constructor(self, brain, trainer_parameters, training, load, seed, run_id):
-        assert brain == brain_params_mock
-        assert trainer_parameters == expected_config
-        assert training == train_model
-        assert load == load_model
-        assert seed == seed
-        assert run_id == run_id
-
-    with patch.object(OnlineBCTrainer, "__init__", mock_constructor):
-        trainers = trainer_util.initialize_trainers(
-            trainer_config=base_config,
-            external_brains=external_brains,
-            summaries_dir=summaries_dir,
-            run_id=run_id,
-            model_path=model_path,
-            keep_checkpoints=keep_checkpoints,
-            train_model=train_model,
-            load_model=load_model,
-            seed=seed,
-        )
-        assert "testbrain" in trainers
-        assert isinstance(trainers["testbrain"], OnlineBCTrainer)
 
 
 @patch("mlagents.envs.brain.BrainParameters")
