2f8b430bdd1a87c18bbce8546c78c5313d50a112
Chris Elion <chris.elion@unity3d.com>
2019-10-02 21:52:28 -0700

    fix imports for 1.14

--- ml-agents/mlagents/trainers/tf.py
+++ ml-agents/mlagents/trainers/tf.py
@@ -1,26 +1,11 @@
-import logging
-
-
-def warnings_as_errors(log_record):
-    # Raise deprecated warnings as exceptions.
-    if log_record.levelno == logging.WARNING and "deprecated" in log_record.msg:
-        merged = log_record.getMessage()
-        raise RuntimeError(merged)
-    return True
-
-
-# TODO only enable this with a environment variable
-if False:
-    logging.getLogger("tensorflow").addFilter(warnings_as_errors)
-
-try:
-    import tensorflow.compat.v1 as tf
-except ImportError:
-    import tensorflow as tf
+import tensorflow as tf
 
 # TODO better version check, this will do for now though
 is_tf2 = tf.__version__ == "2.0.0"
+
 if is_tf2:
+    import tensorflow.compat.v1 as tf
+
     tf_variance_scaling = tf.initializers.variance_scaling
     tf_flatten = tf.layers.flatten
     tf_rnn = tf.nn.rnn_cell
