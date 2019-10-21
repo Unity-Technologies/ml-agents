d0137a247442b8017d19432faba9b500e1f2de19
Christopher Goy <goyenator@gmail.com>
2019-10-21 15:50:48 -0700

    Run editomode tests on yamato.

---
+++ run-tests-editmode-osx-editor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+UNITY="${UNITY_PATH:=/Applications/Unity/Hub/Editor/2017.4.33f1/Unity.app/Contents/MacOS}/Unity"
+
+pushd $(dirname "${0}") > /dev/null
+BASETPATH=$(pwd -L)
+popd > /dev/null
+
+echo "Cleaning previous results"
+
+if [ -e $BASETPATH/results.xml ]
+then
+	rm $BASETPATH/results.xml
+fi
+
+echo "Starting tests via $UNITY"
+
+CMD_LINE="$UNITY -runTests -projectPath $BASETPATH/UnitySDK -testResults $BASETPATH/results.xml -testPlatform editmode"
+
+echo "$CMD_LINE ..."
+
+$CMD_LINE
+RES=$?
+
+TOTAL=$(echo 'cat /test-run/test-suite/@total' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+PASSED=$(echo 'cat /test-run/test-suite/@passed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+FAILED=$(echo 'cat /test-run/test-suite/@failed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+DURATION=$(echo 'cat /test-run/test-suite/@duration' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+
+echo "$TOTAL tests executed in ${DURATION}s: $PASSED passed, $FAILED failed. More details in results.xml"
+
+if [ $RES -eq 0 ] && [ -e $BASETPATH/results.xml ]; then
+    echo "Test run SUCCEEDED!"
+else
+    echo "Test run FAILED!"
+fi
