86f129154e9fa79424ab264a236d258226e55efb
Chris Elion <chris.elion@unity3d.com>
2019-10-25 16:58:48 -0700

    only change cs files

--- UnitySDK/Assets/ML-Agents/Editor/Tests/StandaloneBuildTest.cs
+++ UnitySDK/Assets/ML-Agents/Editor/Tests/StandaloneBuildTest.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.Build.Reporting;
 
 namespace MLAgents
 {
@@ -9,19 +10,20 @@ namespace MLAgents
         static void BuildStandalonePlayerOSX()
         {
             string[] scenes = { "Assets/ML-Agents/Examples/3DBall/Scenes/3DBall.unity" };
-            var error = BuildPipeline.BuildPlayer(scenes, "testPlayer", BuildTarget.StandaloneOSX, BuildOptions.None);
-            if (string.IsNullOrEmpty(error))
+            var report = BuildPipeline.BuildPlayer(scenes, "testPlayer", BuildTarget.StandaloneOSX, BuildOptions.None);
+            //  if (string.IsNullOrEmpty(error))
+            if(report.summary.result == BuildResult.Succeeded)
             {
                 EditorApplication.Exit(0);
             }
             else
             {
-                Console.Error.WriteLine(error);
+                //Console.Error.WriteLine(error);
                 EditorApplication.Exit(1);
-                
+
             }
-            Debug.Log(error);
+            //Debug.Log(error);
         }
-        
+
     }
 }
