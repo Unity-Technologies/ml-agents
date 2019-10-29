abf02d6bff3d984cb72c28a9daf3918365aaead1
Christopher Goy <goyenator@gmail.com>
2019-10-29 11:58:35 -0700

    Redirect editor log to stdout.

--- run-tests-editmode-osx-editor.sh
+++ run-tests-editmode-osx-editor.sh
@@ -2,49 +2,56 @@
 
 set -eo pipefail
 
-EDITOR_VERSION="2017.4.33f1"
-BOKKEN_UNITY="/Users/bokken/${EDITOR_VERSION}/Unity.app/Contents/MacOS/Unity"
-HUB_UNITY="/Applications/Unity/Hub/Editor/${EDITOR_VERSION}/Unity.app/Contents/MacOS/Unity"
+if [[ -z "${UNITY_VERSION}" ]]; then
 
-if [[ -f ${BOKKEN_UNITY} ]]; then 
-    UNITY=${BOKKEN_UNITY}
+    echo "Environment Variable UNITY_VERSION was not set"
+    exit 1
+
 else
-    UNITY=${HUB_UNITY}
-fi 
+    BOKKEN_UNITY="/Users/bokken/${UNITY_VERSION}/Unity.app/Contents/MacOS/Unity"
+    HUB_UNITY="/Applications/Unity/Hub/Editor/${UNITY_VERSION}/Unity.app/Contents/MacOS/Unity"
 
-pushd $(dirname "${0}") > /dev/null
-BASETPATH=$(pwd -L)
-popd > /dev/null
+    if [[ -f ${BOKKEN_UNITY} ]]; then
+        UNITY=${BOKKEN_UNITY}
+    else
+        UNITY=${HUB_UNITY}
+    fi
 
-echo "Cleaning previous results"
+    pushd $(dirname "${0}") > /dev/null
+    BASETPATH=$(pwd -L)
+    popd > /dev/null
 
-if [[ -e ${BASETPATH}/results.xml ]]
-then
-	rm ${BASETPATH}/results.xml
+    echo "Cleaning previous results"
+
+    if [[ -e ${BASETPATH}/results.xml ]]
+    then
+        rm ${BASETPATH}/results.xml
+    fi
+
+    echo "Starting tests via $UNITY"
+
+    CMD_LINE="$UNITY -runTests -logfile - -projectPath $BASETPATH/UnitySDK -testResults $BASETPATH/results.xml -testPlatform editmode"
+
+    echo "$CMD_LINE ..."
+
+    $CMD_LINE
+    RES=$?
+
+    TOTAL=$(echo 'cat /test-run/test-suite/@total' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+    PASSED=$(echo 'cat /test-run/test-suite/@passed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+    FAILED=$(echo 'cat /test-run/test-suite/@failed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+    DURATION=$(echo 'cat /test-run/test-suite/@duration' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
+
+    echo "$TOTAL tests executed in ${DURATION}s: $PASSED passed, $FAILED failed. More details in results.xml"
+
+    if [[ ${RES} -eq 0 ]] && [[ -e ${BASETPATH}/results.xml ]]; then
+        echo "Test run SUCCEEDED!"
+    else
+        echo "Test run FAILED!"
+    fi
+
+    rm "${BASETPATH}/results.xml"
+
+    exit ${RES}
+
 fi
-
-echo "Starting tests via $UNITY"
-
-CMD_LINE="$UNITY -runTests -projectPath $BASETPATH/UnitySDK -testResults $BASETPATH/results.xml -testPlatform editmode"
-
-echo "$CMD_LINE ..."
-
-$CMD_LINE
-RES=$?
-
-TOTAL=$(echo 'cat /test-run/test-suite/@total' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-PASSED=$(echo 'cat /test-run/test-suite/@passed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-FAILED=$(echo 'cat /test-run/test-suite/@failed' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-DURATION=$(echo 'cat /test-run/test-suite/@duration' | xmllint --shell results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-
-echo "$TOTAL tests executed in ${DURATION}s: $PASSED passed, $FAILED failed. More details in results.xml"
-
-if [[ ${RES} -eq 0 ]] && [[ -e ${BASETPATH}/results.xml ]]; then
-    echo "Test run SUCCEEDED!"
-else
-    echo "Test run FAILED!"
-fi
-
-rm "${BASETPATH}/results.xml"
-
-exit ${RES}
