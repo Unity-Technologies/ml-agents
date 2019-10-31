a58f1633b8913ed55ce949bf6ee2bda14c3c8a8f
Chris Elion <chris.elion@unity3d.com>
2019-10-31 10:58:45 -0700

    docstrings

--- UnitySDK/Assets/ML-Agents/Scripts/Sensor/WriteAdapter.cs
+++ UnitySDK/Assets/ML-Agents/Scripts/Sensor/WriteAdapter.cs
@@ -3,6 +3,9 @@ using MLAgents.InferenceBrain;
 
 namespace MLAgents.Sensor
 {
+    /// <summary>
+    /// Allows sensors to write to both TensorProxy and float arrays/lists.
+    /// </summary>
     public class WriteAdapter
     {
         IList<float> m_Data;
@@ -11,8 +14,18 @@ namespace MLAgents.Sensor
         TensorProxy m_Proxy;
         int m_Batch;
 
-        public WriteAdapter(){ }
+        /// <summary>
+        /// Create an empty Adapter. SetTarget() must be called first before using.
+        /// </summary>
+        public WriteAdapter()
+        {
+        }
 
+        /// <summary>
+        /// Set the adapter to write to an IList at the given channelOffset.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="offset"></param>
         public void SetTarget(IList<float> data, int offset)
         {
             m_Data = data;
@@ -21,14 +34,24 @@ namespace MLAgents.Sensor
             m_Batch = -1;
         }
 
-        public void SetTarget(TensorProxy tensorProxy, int batchIndex, int offset)
+        /// <summary>
+        /// Set the adapter to write to a TensorProxy at the given batch and channel offset.
+        /// </summary>
+        /// <param name="tensorProxy"></param>
+        /// <param name="batchIndex"></param>
+        /// <param name="channelOffset"></param>
+        public void SetTarget(TensorProxy tensorProxy, int batchIndex, int channelOffset)
         {
             m_Proxy = tensorProxy;
             m_Batch = batchIndex;
-            m_Offset = offset;
+            m_Offset = channelOffset;
             m_Data = null;
         }
 
+        /// <summary>
+        /// 1D write access at a specified index. Use AddRange if possible instead.
+        /// </summary>
+        /// <param name="index">Index to write to</param>
         public float this[int index]
         {
             set
@@ -44,6 +67,12 @@ namespace MLAgents.Sensor
             }
         }
 
+        /// <summary>
+        /// 3D write access at the specified height, width, and channel. Only usable with a TensorProxy target.
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="w"></param>
+        /// <param name="ch"></param>
         public float this[int h, int w, int ch]
         {
             set
@@ -53,14 +82,19 @@ namespace MLAgents.Sensor
             }
         }
 
-        public void AddRange(IEnumerable<float> data, int offset = 0)
+        /// <summary>
+        /// Write the range of floats
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="writeOffset">Optional write offset</param>
+        public void AddRange(IEnumerable<float> data, int writeOffset = 0)
         {
             if (m_Data != null)
             {
                 int index = 0;
                 foreach (var val in data)
                 {
-                    m_Data[index + m_Offset + offset] = val;
+                    m_Data[index + m_Offset + writeOffset] = val;
                     index++;
                 }
             }
@@ -69,7 +103,7 @@ namespace MLAgents.Sensor
                 int index = 0;
                 foreach (var val in data)
                 {
-                    m_Proxy.data[m_Batch, index + m_Offset + offset] = val;
+                    m_Proxy.data[m_Batch, index + m_Offset + writeOffset] = val;
                     index++;
                 }
             }
