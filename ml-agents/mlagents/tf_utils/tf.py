7576c19c14c736679d7ae87b3fb8b4e4ea883a33
Chris Elion <chris.elion@unity3d.com>
2019-11-13 18:00:07 -0800

    try getting rid of indirection

--- ml-agents/mlagents/tf_utils/tf.py
+++ ml-agents/mlagents/tf_utils/tf.py
@@ -19,8 +19,6 @@ if _is_tensorflow2:
 
     tf.disable_v2_behavior()
 else:
-    import tensorflow.contrib.layers as c_layers
-
-    tf_variance_scaling = c_layers.variance_scaling_initializer
-    tf_flatten = c_layers.flatten
-    tf_rnn = tf.contrib.rnn
+    tf_variance_scaling = tf.initializers.variance_scaling
+    tf_flatten = tf.layers.flatten
+    tf_rnn = tf.nn.rnn_cell
