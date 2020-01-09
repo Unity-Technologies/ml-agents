465ddfd2f92e6234c0f82c296c6e9f7973ed88aa
Chris Elion <chris.elion@unity3d.com>
2020-01-08 16:15:03 -0800

    fix HallwaySettings

--- UnitySDK/Assets/ML-Agents/Examples/Hallway/Scripts/HallwaySettings.cs
+++ UnitySDK/Assets/ML-Agents/Examples/Hallway/Scripts/HallwaySettings.cs
@@ -1,14 +1,10 @@
 using UnityEngine;
-using MLAgents;
 
-public class HallwaySettings : Academy
+public class HallwaySettings : MonoBehaviour
 {
     public float agentRunSpeed;
     public float agentRotationSpeed;
     public Material goalScoredMaterial; // when a goal is scored the ground will use this material for a few seconds.
     public Material failMaterial; // when fail, the ground will use this material for a few seconds.
 
-    public override void AcademyReset()
-    {
-    }
 }
