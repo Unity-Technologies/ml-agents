38a109c3f78b5debebe29a977cea5169536e2360
Christopher Goy <goyenator@gmail.com>
2020-01-29 09:57:27 -0800

    Add pre-export method to disable burst compilation.

---
+++ Project/Assets/ML-Agents/Editor/Cloudbuild/DisableBurstFromMenu.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_CLOUDBUILD
+using UnityEditor;
+
+namespace MLAgents
+{
+    public class DisableBurstFromMenu
+    {
+        public static void DisableBurstCompilation()
+        {
+            EditorApplication.ExecuteMenuItem("Jobs/Burst/Enable Compilation");
+        }
+    }
+}
+#endif
