9b8fed1790e281efc98c8b0715c5ba01f93521fd
AcelisWeaven <jeremy.graziani@gmail.com>
2020-01-30 01:30:35 +0100

    Clarified that training still doesn't work

--- com.unity.ml-agents/Plugins/IL2CPP.DL.Stubs.c
+++ com.unity.ml-agents/Plugins/IL2CPP.DL.Stubs.c
@@ -1,4 +1,5 @@
 // These stubs fix an issue compiling GRPC on Windows with IL2CPP.
+// For the moment, only Inference works. (training doesn't)
 
 void * dlopen(const char *filename, int flags) {
     return 0;
