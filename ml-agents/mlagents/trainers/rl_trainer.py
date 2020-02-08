611c93c980b1d4577b7a4eb6bb45bee8f0efea2a
Ervin Teng <ervin@unity3d.com>
2020-02-07 16:02:44 -0800

    Move optimizer to common folder

--- ml-agents/mlagents/trainers/rl_trainer.py
+++ ml-agents/mlagents/trainers/rl_trainer.py
@@ -3,7 +3,7 @@ import logging
 from typing import Dict
 from collections import defaultdict
 
-from mlagents.trainers.tf_optimizer import TFOptimizer
+from mlagents.trainers.common.tf_optimizer import TFOptimizer
 from mlagents.trainers.buffer import AgentBuffer
 from mlagents.trainers.trainer import Trainer
 from mlagents.trainers.exception import UnityTrainerException
