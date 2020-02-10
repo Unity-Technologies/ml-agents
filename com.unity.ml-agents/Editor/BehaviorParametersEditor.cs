5bf47bb7b444b8abcafef0ce44f3bee3efa502b5
Chris Elion <chris.elion@unity3d.com>
2020-02-10 13:53:54 -0800

    fix BehaviorParametersEditor for m_UseChildSensors

--- com.unity.ml-agents/Editor/BehaviorParametersEditor.cs
+++ com.unity.ml-agents/Editor/BehaviorParametersEditor.cs
@@ -34,7 +34,7 @@ namespace MLAgents
             EditorGUI.indentLevel--;
             EditorGUILayout.PropertyField(so.FindProperty("m_BehaviorType"));
             EditorGUILayout.PropertyField(so.FindProperty("m_TeamID"));
-            EditorGUILayout.PropertyField(so.FindProperty("m_useChildSensors"), true);
+            EditorGUILayout.PropertyField(so.FindProperty("m_UseChildSensors"), true);
             // EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Heuristic"), true);
             EditorGUI.indentLevel--;
             if (EditorGUI.EndChangeCheck())
