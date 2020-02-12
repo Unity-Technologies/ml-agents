95bfff1b7db720f494a3f2dff6330ec7be320511
Chris Elion <chris.elion@unity3d.com>
2020-02-12 11:49:20 -0800

    script to make the table

---
+++ utils/make_readme_table.py
@@ -0,0 +1,17 @@
+"""
+Generate the "Releases" table on the main readme. Update the versions lists, run this script, and copy the output
+into the markdown file.
+"""
+from distutils.version import LooseVersion
+
+
+def table_line(version):
+    return f"| **{version}**  | [source](https://github.com/Unity-Technologies/ml-agents/tree/{version}) |  [docs](https://github.com/Unity-Technologies/ml-agents/tree/{version}/docs) | [download](https://github.com/Unity-Technologies/ml-agents/archive/{version}.zip) |"  # noqa
+
+
+versions = ["0.10.0", "0.10.1", "0.11.0", "0.12.0", "0.12.1", "0.13.0", "0.13.1"]
+
+sorted_versions = sorted((LooseVersion(v) for v in versions), reverse=True)
+
+for v in sorted_versions:
+    print(table_line(str(v)))
