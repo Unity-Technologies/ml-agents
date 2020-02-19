07ee7a79acbfcc987fa204389c47999b3c9e22b0
Chris Elion <chris.elion@unity3d.com>
2020-02-19 15:45:13 -0800

    struct-ify the inputs and outputs, single-ray method

--- com.unity.ml-agents/Runtime/Sensor/RayPerceptionSensor.cs
+++ com.unity.ml-agents/Runtime/Sensor/RayPerceptionSensor.cs
@@ -16,7 +16,7 @@ namespace MLAgents
         Cast3D,
     }
 
-    public class RayPerceptionInput
+    public struct RayPerceptionInput
     {
         /// <summary>
         /// Length of the rays to cast. This will be scaled up or down based on the scale of the transform.
@@ -58,12 +58,12 @@ namespace MLAgents
         /// <summary>
         /// Whether to perform the casts in 2D or 3D.
         /// </summary>
-        public RayPerceptionCastType castType = RayPerceptionCastType.Cast3D;
+        public RayPerceptionCastType castType;
 
         /// <summary>
         /// Filtering options for the casts.
         /// </summary>
-        public int layerMask = Physics.DefaultRaycastLayers;
+        public int layerMask;
 
         /// <summary>
         /// Returns the expected number of floats in the output.
@@ -147,6 +147,32 @@ namespace MLAgents
             /// Normalized distance to the hit object.
             /// </summary>
             public float hitFraction;
+
+            /// <summary>
+            /// Writes the ray output information to a float array.  Each element in the rayAngles array determines a
+            /// sublist of data to the observation. The sublist contains the observation data for a single cast.
+            /// The list is composed of the following:
+            /// 1. A one-hot encoding for detectable tags. For example, if detectableTags.Length = n, the
+            ///    first n elements of the sublist will be a one-hot encoding of the detectableTag that was hit, or
+            ///    all zeroes otherwise.
+            /// 2. The 'numDetectableTags' element of the sublist will be 1 if the ray missed everything, or 0 if it hit
+            ///    something (detectable or not).
+            /// 3. The 'numDetectableTags+1' element of the sublist will contain the normalized distance to the object
+            ///    hit, or 1.0 if nothing was hit.
+            /// </summary>
+            /// <param name="numDetectableTags"></param>
+            /// <param name="numDetectableTags"></param>
+            /// <param name="buffer">Output buffer. The size must be equal to (numDetectableTags+2) * rayOutputs.Length</param>
+            public void ToFloatArray(int numDetectableTags, int rayIndex, float[] buffer)
+            {
+                var bufferOffset = (numDetectableTags + 2) * rayIndex;
+                if (hitTaggedObject)
+                {
+                    buffer[bufferOffset + hitTagIndex] = 1f;
+                }
+                buffer[bufferOffset + numDetectableTags] = hasHit ? 0f : 1f;
+                buffer[bufferOffset + numDetectableTags + 1] = hitFraction;
+            }
         }
 
         /// <summary>
@@ -166,18 +192,10 @@ namespace MLAgents
         public void ToFloatArray(int numDetectableTags, float[] buffer)
         {
             Array.Clear(buffer, 0, buffer.Length);
-            var bufferOffset = 0;
-
             for (var i = 0; i < rayOutputs.Length; i++)
             {
                 var rayOutput = rayOutputs[i];
-                if (rayOutput.hitTaggedObject)
-                {
-                    buffer[bufferOffset + rayOutput.hitTagIndex] = 1f;
-                }
-                buffer[bufferOffset + numDetectableTags] = rayOutput.hasHit ? 0f : 1f;
-                buffer[bufferOffset + numDetectableTags + 1] = rayOutput.hitFraction;
-                bufferOffset += numDetectableTags + 2;
+                rayOutput.ToFloatArray(numDetectableTags, i, buffer);
             }
         }
 
@@ -225,7 +243,6 @@ namespace MLAgents
         string m_Name;
 
         RayPerceptionInput m_RayPerceptionInput;
-        RayPerceptionOutput m_Output = new RayPerceptionOutput();
 
         DebugDisplayInfo m_DebugDisplayInfo;
 
@@ -253,8 +270,35 @@ namespace MLAgents
         {
             using (TimerStack.Instance.Scoped("RayPerceptionSensor.Perceive"))
             {
-                PerceiveStatic(m_RayPerceptionInput, m_Output, m_DebugDisplayInfo);
-                m_Output.ToFloatArray(m_RayPerceptionInput.detectableTags.Count, m_Observations);
+                Array.Clear(m_Observations, 0, m_Observations.Length);
+
+                var numRays = m_RayPerceptionInput.angles.Count;
+                var numDetectableTags = m_RayPerceptionInput.detectableTags.Count;
+
+                if (m_DebugDisplayInfo != null)
+                {
+                    // Reset the age information, and resize the buffer if needed.
+                    m_DebugDisplayInfo.Reset();
+                    if (m_DebugDisplayInfo.rayInfos == null || m_DebugDisplayInfo.rayInfos.Length != numRays)
+                    {
+                        m_DebugDisplayInfo.rayInfos = new DebugDisplayInfo.RayInfo[numRays];
+                    }
+                }
+
+                // For each ray, do the casting, and write the information to the observation buffer
+                for (var rayIndex = 0; rayIndex < numRays; rayIndex++)
+                {
+                    DebugDisplayInfo.RayInfo debugRay;
+                    var rayOutput = PerceiveSingleRay(m_RayPerceptionInput, rayIndex, out debugRay);
+
+                    if (m_DebugDisplayInfo != null)
+                    {
+                        m_DebugDisplayInfo.rayInfos[rayIndex] = debugRay;
+                    }
+
+                    rayOutput.ToFloatArray(numDetectableTags, rayIndex, m_Observations);
+                }
+                // Finally, add the observations to the WriteAdapter
                 adapter.AddRange(m_Observations);
             }
             return m_Observations.Length;
@@ -289,134 +333,115 @@ namespace MLAgents
         /// </summary>
         /// <param name="input">Input defining the rays that will be cast.</param>
         /// <param name="output">Output class that will be written to with raycast results.</param>
-        /// <param name="debugInfo">Optional debug information output, only used by RayPerceptionSensor.</param>
         ///
-        public static void PerceiveStatic(RayPerceptionInput input, RayPerceptionOutput output)
+        public static RayPerceptionOutput PerceiveStatic(RayPerceptionInput input)
         {
-            PerceiveStatic(input, output, null);
-        }
+            RayPerceptionOutput output = new RayPerceptionOutput();
+            output.rayOutputs = new RayPerceptionOutput.RayOutput[input.angles.Count];
 
-        /// <summary>
-        /// Evaluates the raycasts to be used as part of an observation of an agent.
-        /// </summary>
-        /// <param name="input">Input defining the rays that will be cast.</param>
-        /// <param name="output">Output class that will be written to with raycast results.</param>
-        /// <param name="debugInfo">Optional debug information output, only used by RayPerceptionSensor.</param>
-        ///
-        internal static void PerceiveStatic(RayPerceptionInput input, RayPerceptionOutput output, DebugDisplayInfo debugInfo)
-        {
-
-            if (output.rayOutputs == null || output.rayOutputs.Length != input.angles.Count)
-            {
-                output.rayOutputs = new RayPerceptionOutput.RayOutput[input.angles.Count];
-            }
-
-            if (debugInfo != null)
-            {
-                debugInfo.Reset();
-                if (debugInfo.rayInfos == null || debugInfo.rayInfos.Length != input.angles.Count)
-                {
-                    debugInfo.rayInfos = new DebugDisplayInfo.RayInfo[input.angles.Count];
-                }
-            }
-
-            // For each ray sublist stores categorical information on detected object
-            // along with object distance.
-            var unscaledRayLength = input.rayLength;
-            var unscaledCastRadius = input.castRadius;
             for (var rayIndex = 0; rayIndex < input.angles.Count; rayIndex++)
             {
-                var extents = input.RayExtents(rayIndex);
-                var startPositionWorld = extents.StartPositionWorld;
-                var endPositionWorld = extents.EndPositionWorld;
+                DebugDisplayInfo.RayInfo debugRay;
+                output.rayOutputs[rayIndex] = PerceiveSingleRay(input, rayIndex, out debugRay);
+            }
 
-                var rayDirection = endPositionWorld - startPositionWorld;
-                // If there is non-unity scale, |rayDirection| will be different from rayLength.
-                // We want to use this transformed ray length for determining cast length, hit fraction etc.
-                // We also it to scale up or down the sphere or circle radii
-                var scaledRayLength = rayDirection.magnitude;
-                // Avoid 0/0 if unscaledRayLength is 0
-                var scaledCastRadius = unscaledRayLength > 0 ?
-                    unscaledCastRadius * scaledRayLength / unscaledRayLength :
-                    unscaledCastRadius;
+            return output;
+        }
 
-                // Do the cast and assign the hit information for each detectable tag.
-                bool castHit;
-                float hitFraction;
-                GameObject hitObject;
+        static RayPerceptionOutput.RayOutput PerceiveSingleRay(
+            RayPerceptionInput input,
+            int rayIndex,
+            out DebugDisplayInfo.RayInfo debugRayOut
+        )
+        {
+            var unscaledRayLength = input.rayLength;
+            var unscaledCastRadius = input.castRadius;
 
-                if (input.castType == RayPerceptionCastType.Cast3D)
+            var extents = input.RayExtents(rayIndex);
+            var startPositionWorld = extents.StartPositionWorld;
+            var endPositionWorld = extents.EndPositionWorld;
+
+            var rayDirection = endPositionWorld - startPositionWorld;
+            // If there is non-unity scale, |rayDirection| will be different from rayLength.
+            // We want to use this transformed ray length for determining cast length, hit fraction etc.
+            // We also it to scale up or down the sphere or circle radii
+            var scaledRayLength = rayDirection.magnitude;
+            // Avoid 0/0 if unscaledRayLength is 0
+            var scaledCastRadius = unscaledRayLength > 0 ?
+                unscaledCastRadius * scaledRayLength / unscaledRayLength :
+                unscaledCastRadius;
+
+            // Do the cast and assign the hit information for each detectable tag.
+            bool castHit;
+            float hitFraction;
+            GameObject hitObject;
+
+            if (input.castType == RayPerceptionCastType.Cast3D)
+            {
+                RaycastHit rayHit;
+                if (scaledCastRadius > 0f)
                 {
-                    RaycastHit rayHit;
-                    if (scaledCastRadius > 0f)
-                    {
-                        castHit = Physics.SphereCast(startPositionWorld, scaledCastRadius, rayDirection, out rayHit,
-                            scaledRayLength, input.layerMask);
-                    }
-                    else
-                    {
-                        castHit = Physics.Raycast(startPositionWorld, rayDirection, out rayHit,
-                            scaledRayLength, input.layerMask);
-                    }
-
-                    // If scaledRayLength is 0, we still could have a hit with sphere casts (maybe?).
-                    // To avoid 0/0, set the fraction to 0.
-                    hitFraction = castHit ? (scaledRayLength > 0 ? rayHit.distance / scaledRayLength : 0.0f) : 1.0f;
-                    hitObject = castHit ? rayHit.collider.gameObject : null;
+                    castHit = Physics.SphereCast(startPositionWorld, scaledCastRadius, rayDirection, out rayHit,
+                        scaledRayLength, input.layerMask);
                 }
                 else
                 {
-                    RaycastHit2D rayHit;
-                    if (scaledCastRadius > 0f)
-                    {
-                        rayHit = Physics2D.CircleCast(startPositionWorld, scaledCastRadius, rayDirection,
-                            scaledRayLength, input.layerMask);
-                    }
-                    else
-                    {
-                        rayHit = Physics2D.Raycast(startPositionWorld, rayDirection, scaledRayLength, input.layerMask);
-                    }
-
-                    castHit = rayHit;
-                    hitFraction = castHit ? rayHit.fraction : 1.0f;
-                    hitObject = castHit ? rayHit.collider.gameObject : null;
+                    castHit = Physics.Raycast(startPositionWorld, rayDirection, out rayHit,
+                        scaledRayLength, input.layerMask);
                 }
 
-                var rayOutput = new RayPerceptionOutput.RayOutput();
-                rayOutput.hasHit = castHit;
-                rayOutput.hitFraction = hitFraction;
-                rayOutput.hitTaggedObject = false;
-                rayOutput.hitTagIndex = -1;
-
-                if (castHit)
-                {
-                    // Find the index of the tag of the object that was hit.
-                    for (var i = 0; i < input.detectableTags.Count; i++)
-                    {
-                        if (hitObject.CompareTag(input.detectableTags[i]))
-                        {
-                            rayOutput.hitTaggedObject = true;
-                            rayOutput.hitTagIndex = i;
-                            break;
-                        }
-                    }
-                }
-
-                if (debugInfo != null)
-                {
-                    debugInfo.rayInfos[rayIndex].worldStart = startPositionWorld;
-                    debugInfo.rayInfos[rayIndex].worldEnd = endPositionWorld;
-                    debugInfo.rayInfos[rayIndex].rayOutput = rayOutput;
-                    debugInfo.rayInfos[rayIndex].castRadius = scaledCastRadius;
-                }
-                else if (Application.isEditor)
-                {
-                    // Legacy drawing
-                    Debug.DrawRay(startPositionWorld, rayDirection, Color.black, 0.01f, true);
-                }
-
-                output.rayOutputs[rayIndex] = rayOutput;
+                // If scaledRayLength is 0, we still could have a hit with sphere casts (maybe?).
+                // To avoid 0/0, set the fraction to 0.
+                hitFraction = castHit ? (scaledRayLength > 0 ? rayHit.distance / scaledRayLength : 0.0f) : 1.0f;
+                hitObject = castHit ? rayHit.collider.gameObject : null;
             }
+            else
+            {
+                RaycastHit2D rayHit;
+                if (scaledCastRadius > 0f)
+                {
+                    rayHit = Physics2D.CircleCast(startPositionWorld, scaledCastRadius, rayDirection,
+                        scaledRayLength, input.layerMask);
+                }
+                else
+                {
+                    rayHit = Physics2D.Raycast(startPositionWorld, rayDirection, scaledRayLength, input.layerMask);
+                }
+
+                castHit = rayHit;
+                hitFraction = castHit ? rayHit.fraction : 1.0f;
+                hitObject = castHit ? rayHit.collider.gameObject : null;
+            }
+
+            var rayOutput = new RayPerceptionOutput.RayOutput
+            {
+                hasHit = castHit,
+                hitFraction = hitFraction,
+                hitTaggedObject = false,
+                hitTagIndex = -1
+            };
+
+            if (castHit)
+            {
+                // Find the index of the tag of the object that was hit.
+                for (var i = 0; i < input.detectableTags.Count; i++)
+                {
+                    if (hitObject.CompareTag(input.detectableTags[i]))
+                    {
+                        rayOutput.hitTaggedObject = true;
+                        rayOutput.hitTagIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            debugRayOut.worldStart = startPositionWorld;
+            debugRayOut.worldEnd = endPositionWorld;
+            debugRayOut.rayOutput = rayOutput;
+            debugRayOut.castRadius = scaledCastRadius;
+
+            return rayOutput;
+
         }
     }
 }
