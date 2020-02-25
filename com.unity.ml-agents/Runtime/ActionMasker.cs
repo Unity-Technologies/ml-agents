cc35ef9aa80d8837ab15bc1693496464cac57a02
Marwan Mattar <marwan@unity3d.com>
2020-02-24 17:15:40 -0800

    Fixed package validation errors in ActionMasker

--- com.unity.ml-agents/Runtime/ActionMasker.cs
+++ com.unity.ml-agents/Runtime/ActionMasker.cs
@@ -4,6 +4,13 @@ using System.Linq;
 
 namespace MLAgents
 {
+    /// <summary>
+    /// For agents that take discrete actions, they can explicitly indicate that specific actions
+    /// are not allowed at a point in time. This enables the agent to indicate that some actions
+    /// may be illegal (e.g. the King in Chess taking a move to the left if it is already in the
+    /// left side of the board). This class represents an the set of masked actions and provides
+    /// the utilities for setting and retrieving them.
+    /// </summary>
     public class ActionMasker
     {
         /// When using discrete control, is the starting indices of the actions
