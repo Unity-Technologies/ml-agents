6bbe2d793cba4c48ca8f8e236cb80a8586042e3a
vincentpierre <vincentpierre@unity3d.com>
2020-02-25 18:05:44 -0800

    Made a asmdef for CommunicatorObjects

---
+++ com.unity.ml-agents/Runtime/Grpc/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.ML-Agents")]
+[assembly: InternalsVisibleTo("Unity.ML-Agents.Editor")]
+[assembly: InternalsVisibleTo("Unity.ML-Agents.Editor.Tests")]
