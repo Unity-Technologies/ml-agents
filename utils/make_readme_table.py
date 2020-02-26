81d4ec8d7fa7475cb873b07bfb46092386e93cae
Chris Elion <chris.elion@unity3d.com>
2020-02-25 17:23:21 -0800

    update release version list (#3497)

--- utils/make_readme_table.py
+++ utils/make_readme_table.py
@@ -9,7 +9,16 @@ def table_line(version):
     return f"| **{version}**  | [source](https://github.com/Unity-Technologies/ml-agents/tree/{version}) |  [docs](https://github.com/Unity-Technologies/ml-agents/tree/{version}/docs) | [download](https://github.com/Unity-Technologies/ml-agents/archive/{version}.zip) |"  # noqa
 
 
-versions = ["0.10.0", "0.10.1", "0.11.0", "0.12.0", "0.12.1", "0.13.0", "0.13.1"]
+versions = [
+    "0.10.0",
+    "0.10.1",
+    "0.11.0",
+    "0.12.0",
+    "0.12.1",
+    "0.13.0",
+    "0.13.1",
+    "0.14.0",
+]
 
 sorted_versions = sorted((LooseVersion(v) for v in versions), reverse=True)
 
