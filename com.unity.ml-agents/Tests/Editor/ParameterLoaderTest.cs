b8852853f9c12b3e7da5b7561f1bee921126a57d
vincentpierre <vincentpierre@unity3d.com>
2020-02-27 16:20:45 -0800

    More tests

--- com.unity.ml-agents/Tests/Editor/ParameterLoaderTest.cs
+++ com.unity.ml-agents/Tests/Editor/ParameterLoaderTest.cs
@@ -8,15 +8,17 @@ using System.Linq;
 
 namespace MLAgents.Tests
 {
-
-    public class Test3DSensorComponent : SensorComponent{
+    public class Test3DSensorComponent : SensorComponent
+    {
         public ISensor Sensor;
 
-        public override ISensor CreateSensor(){
+        public override ISensor CreateSensor()
+        {
             return Sensor;
         }
 
-        public override int[] GetObservationShape(){
+        public override int[] GetObservationShape()
+        {
             return Sensor.GetObservationShape();
         }
     }
@@ -27,14 +29,16 @@ namespace MLAgents.Tests
         int m_Channels;
         string m_Name;
 
-        public Test3DSensor(string name, int width, int height, int channels){
+        public Test3DSensor(string name, int width, int height, int channels)
+        {
             m_Width = width;
             m_Height = height;
             m_Channels = channels;
             m_Name = name;
         }
 
-        public int[] GetObservationShape(){
+        public int[] GetObservationShape()
+        {
             return new int[] {m_Height, m_Width, m_Channels };
         }
 
@@ -47,29 +51,32 @@ namespace MLAgents.Tests
         {
             return new byte[0];
         }
-        public void Update(){}
+
+        public void Update() {}
 
         public SensorCompressionType GetCompressionType()
         {
             return SensorCompressionType.None;
         }
 
-        public string GetName(){
+        public string GetName()
+        {
             return m_Name;
         }
     }
 
-[TestFixture]
+    [TestFixture]
     public class ParameterLoaderTest : MonoBehaviour
     {
         const string k_continuous2vis8vec2actionPath = "Packages/com.unity.ml-agents/Tests/Editor/Ressources/continuous2vis8vec2action.nn";
-        const string k_foodcollectorModelPath = "Packages/com.unity.ml-agents/Tests/Editor/Ressources/test_foodcollector.nn";
+        const string k_discrete1vis0vec_2_3action_recurrModelPath = "Packages/com.unity.ml-agents/Tests/Editor/Ressources/discrete1vis0vec_2_3action_recurr.nn";
         NNModel continuous2vis8vec2actionModel;
-        NNModel foodcollectorModel;
+        NNModel discrete1vis0vec_2_3action_recurrModel;
         Test3DSensorComponent sensor_21_20_3;
         Test3DSensorComponent sensor_20_22_3;
 
-        private BrainParameters GetContinuous2vis8vec2actionModelBrainParameters(){
+        private BrainParameters GetContinuous2vis8vec2actionBrainParameters()
+        {
             var validBrainParameters = new BrainParameters();
             validBrainParameters.vectorObservationSize = 8;
             validBrainParameters.vectorActionSize = new int[] { 2 };
@@ -78,28 +85,37 @@ namespace MLAgents.Tests
             return validBrainParameters;
         }
 
+        private BrainParameters GetDiscrete1vis0vec_2_3action_recurrModelBrainParameters()
+        {
+            var validBrainParameters = new BrainParameters();
+            validBrainParameters.vectorObservationSize = 0;
+            validBrainParameters.vectorActionSize = new int[] { 2, 3 };
+            validBrainParameters.numStackedVectorObservations = 1;
+            validBrainParameters.vectorActionSpaceType = SpaceType.Discrete;
+            return validBrainParameters;
+        }
+
         [SetUp]
         public void SetUp()
         {
             continuous2vis8vec2actionModel = (NNModel)AssetDatabase.LoadAssetAtPath(k_continuous2vis8vec2actionPath, typeof(NNModel));
-            foodcollectorModel = (NNModel)AssetDatabase.LoadAssetAtPath(k_foodcollectorModelPath, typeof(NNModel));
+            discrete1vis0vec_2_3action_recurrModel = (NNModel)AssetDatabase.LoadAssetAtPath(k_discrete1vis0vec_2_3action_recurrModelPath, typeof(NNModel));
             var go = new GameObject("SensorA");
             sensor_21_20_3 = go.AddComponent<Test3DSensorComponent>();
             sensor_21_20_3.Sensor = new Test3DSensor("SensorA", 21, 20, 3);
             sensor_20_22_3 = go.AddComponent<Test3DSensorComponent>();
             sensor_20_22_3.Sensor = new Test3DSensor("SensorA", 20, 22, 3);
-
         }
 
         [Test]
         public void TestModelExist()
         {
             Assert.IsNotNull(continuous2vis8vec2actionModel);
-            Assert.IsNotNull(foodcollectorModel);
+            Assert.IsNotNull(discrete1vis0vec_2_3action_recurrModel);
         }
 
         [Test]
-        public void TestGetInputTensors()
+        public void TestGetInputTensors1()
         {
             var model = ModelLoader.Load(continuous2vis8vec2actionModel);
             var inputTensors = BarracudaModelParamLoader.GetInputTensors(model);
@@ -111,54 +127,134 @@ namespace MLAgents.Tests
             Assert.Contains(TensorNames.VisualObservationPlaceholderPrefix + "1", inputNames);
 
             Assert.AreEqual(2, BarracudaModelParamLoader.GetNumVisualInputs(model));
+
+            // Test if the model is null
+            Assert.AreEqual(0, BarracudaModelParamLoader.GetInputTensors(null).Count);
+            Assert.AreEqual(0, BarracudaModelParamLoader.GetNumVisualInputs(null));
         }
 
         [Test]
-        public void TestGetOutputTensors()
+        public void TestGetInputTensors2()
+        {
+            var model = ModelLoader.Load(discrete1vis0vec_2_3action_recurrModel);
+            var inputTensors = BarracudaModelParamLoader.GetInputTensors(model);
+            var inputNames = inputTensors.Select(x => x.name).ToList();
+            // Model should contain 2 inputs : recurrent and visual 1
+
+            foreach (var c in inputNames)
+            {
+                Debug.Log(c);
+            }
+
+            Assert.Contains(TensorNames.VisualObservationPlaceholderPrefix + "0", inputNames);
+            // TODO :There are some memory tensors as well
+        }
+
+        [Test]
+        public void TestGetOutputTensors1()
         {
             var model = ModelLoader.Load(continuous2vis8vec2actionModel);
             var outputNames = BarracudaModelParamLoader.GetOutputNames(model);
             Assert.Contains(TensorNames.ActionOutput, outputNames);
+            Assert.AreEqual(1, outputNames.Count());
+
+            Assert.AreEqual(0, BarracudaModelParamLoader.GetOutputNames(null).Count());
         }
 
         [Test]
-        public void TestCheckModelValid(){
+        public void TestGetOutputTensors2()
+        {
+            var model = ModelLoader.Load(discrete1vis0vec_2_3action_recurrModel);
+            var outputNames = BarracudaModelParamLoader.GetOutputNames(model);
+            Assert.Contains(TensorNames.ActionOutput, outputNames);
+            // TODO : There are some memory tensors as well
+        }
+
+        [Test]
+        public void TestCheckModelValid1()
+        {
             var model = ModelLoader.Load(continuous2vis8vec2actionModel);
-            var validBrainParameters = GetContinuous2vis8vec2actionModelBrainParameters();
+            var validBrainParameters = GetContinuous2vis8vec2actionBrainParameters();
 
             var errors = BarracudaModelParamLoader.CheckModel(model, validBrainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
             Assert.AreEqual(0, errors.Count()); // There should not be any errors
         }
 
         [Test]
-        public void TestCheckModelThrowsVectorObservation(){
-            var model = ModelLoader.Load(continuous2vis8vec2actionModel);
+        public void TestCheckModelValid2()
+        {
+            var model = ModelLoader.Load(discrete1vis0vec_2_3action_recurrModel);
+            var validBrainParameters = GetDiscrete1vis0vec_2_3action_recurrModelBrainParameters();
 
-            var brainParameters = GetContinuous2vis8vec2actionModelBrainParameters();
-            brainParameters.vectorObservationSize = 9; // Invalid observation
-            var errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
-            Assert.AreEqual(1, errors.Count()); // There should not only one error
-
-            brainParameters = GetContinuous2vis8vec2actionModelBrainParameters();
-            brainParameters.numStackedVectorObservations = 2;// Invalid stacking
-            errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
-            Assert.AreEqual(1, errors.Count()); // There should not only one error
+            var errors = BarracudaModelParamLoader.CheckModel(model, validBrainParameters, new SensorComponent[] { sensor_21_20_3 });
+            Assert.AreEqual(0, errors.Count()); // There should not be any errors
         }
 
         [Test]
-        public void TestCheckModelThrowsAction(){
+        public void TestCheckModelThrowsVectorObservation1()
+        {
             var model = ModelLoader.Load(continuous2vis8vec2actionModel);
 
-            var brainParameters = GetContinuous2vis8vec2actionModelBrainParameters();
-            brainParameters.vectorActionSize = new int[] { 3 }; // Invalid observation
+            var brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            brainParameters.vectorObservationSize = 9; // Invalid observation
             var errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
-            Assert.AreEqual(1, errors.Count()); // There should not only one error
+            Assert.Greater(errors.Count(), 0);
 
-            brainParameters = GetContinuous2vis8vec2actionModelBrainParameters();
-            brainParameters.vectorActionSpaceType = SpaceType.Discrete;// Invalid stacking
+            brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            brainParameters.numStackedVectorObservations = 2;// Invalid stacking
             errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
-            Assert.AreEqual(1, errors.Count()); // There should not only one error
+            Assert.Greater(errors.Count(), 0);
         }
 
+        [Test]
+        public void TestCheckModelThrowsVectorObservation2()
+        {
+            var model = ModelLoader.Load(discrete1vis0vec_2_3action_recurrModel);
+
+            var brainParameters = GetDiscrete1vis0vec_2_3action_recurrModelBrainParameters();
+            brainParameters.vectorObservationSize = 1; // Invalid observation
+            var errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3 });
+            Assert.Greater(errors.Count(), 0);
+        }
+
+        [Test]
+        public void TestCheckModelThrowsAction1()
+        {
+            var model = ModelLoader.Load(continuous2vis8vec2actionModel);
+
+            var brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            brainParameters.vectorActionSize = new int[] { 3 }; // Invalid action
+            var errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
+            Assert.Greater(errors.Count(), 0);
+
+            brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            brainParameters.vectorActionSpaceType = SpaceType.Discrete;// Invalid SpaceType
+            errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
+            Assert.Greater(errors.Count(), 0);
+        }
+
+        [Test]
+        public void TestCheckModelThrowsAction2()
+        {
+            var model = ModelLoader.Load(discrete1vis0vec_2_3action_recurrModel);
+
+            var brainParameters = GetDiscrete1vis0vec_2_3action_recurrModelBrainParameters();
+            brainParameters.vectorActionSize = new int[] { 3, 3 }; // Invalid action
+            var errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3 });
+            Assert.Greater(errors.Count(), 0);
+
+            brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            brainParameters.vectorActionSpaceType = SpaceType.Continuous;// Invalid SpaceType
+            errors = BarracudaModelParamLoader.CheckModel(model, brainParameters, new SensorComponent[] { sensor_21_20_3 });
+            Assert.Greater(errors.Count(), 0);
+        }
+
+        [Test]
+        public void TestCheckModelThrowsNoModel()
+        {
+            var brainParameters = GetContinuous2vis8vec2actionBrainParameters();
+            var errors = BarracudaModelParamLoader.CheckModel(null, brainParameters, new SensorComponent[] { sensor_21_20_3, sensor_20_22_3 });
+            Assert.Greater(errors.Count(), 0);
+        }
     }
 }
