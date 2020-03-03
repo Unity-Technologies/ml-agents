1bc602f7bd78d81417860ac19ee9f61f3cc9cc69
Ervin T <ervin@unity3d.com>
2020-03-02 16:51:19 -0800

    [tests] LSTM end-to-end tests (#3544)

--- ml-agents/mlagents/trainers/tests/test_simple_rl.py
+++ ml-agents/mlagents/trainers/tests/test_simple_rl.py
@@ -3,13 +3,17 @@ import tempfile
 import pytest
 import yaml
 import numpy as np
+from typing import Dict
 
-from mlagents.trainers.tests.simple_test_envs import Simple1DEnvironment
+from mlagents.trainers.tests.simple_test_envs import (
+    Simple1DEnvironment,
+    Memory1DEnvironment,
+)
 from mlagents.trainers.trainer_controller import TrainerController
 from mlagents.trainers.trainer_util import TrainerFactory
 from mlagents.trainers.simple_env_manager import SimpleEnvManager
 from mlagents.trainers.sampler_class import SamplerManager
-from mlagents.trainers.stats import StatsReporter
+from mlagents.trainers.stats import StatsReporter, StatsWriter, StatsSummary
 from mlagents_envs.side_channel.float_properties_channel import FloatPropertiesChannel
 
 BRAIN_NAME = "1D"
@@ -21,18 +25,45 @@ PPO_CONFIG = f"""
         beta: 5.0e-3
         buffer_size: 64
         epsilon: 0.2
-        hidden_units: 128
+        hidden_units: 32
         lambd: 0.95
         learning_rate: 5.0e-3
-        max_steps: 2500
+        learning_rate_schedule: constant
+        max_steps: 1500
         memory_size: 256
         normalize: false
         num_epoch: 3
-        num_layers: 2
+        num_layers: 1
         time_horizon: 64
         sequence_length: 64
         summary_freq: 500
         use_recurrent: false
+        reward_signals:
+            extrinsic:
+                strength: 1.0
+                gamma: 0.99
+    """
+
+PPO_CONFIG_RECURRENT = f"""
+    {BRAIN_NAME}:
+        trainer: ppo
+        batch_size: 64
+        beta: 5.0e-3
+        buffer_size: 128
+        epsilon: 0.2
+        hidden_units: 32
+        lambd: 0.95
+        learning_rate: 5.0e-3
+        max_steps: 3000
+        memory_size: 16
+        normalize: false
+        learning_rate_schedule: constant
+        num_epoch: 3
+        num_layers: 1
+        time_horizon: 64
+        sequence_length: 32
+        summary_freq: 500
+        use_recurrent: true
         reward_signals:
             extrinsic:
                 strength: 1.0
@@ -68,6 +99,35 @@ SAC_CONFIG = f"""
                 gamma: 0.99
     """
 
+SAC_CONFIG_RECURRENT = f"""
+    {BRAIN_NAME}:
+        trainer: sac
+        batch_size: 32
+        buffer_size: 500
+        buffer_init_steps: 100
+        hidden_units: 16
+        init_entcoef: 0.01
+        learning_rate: 5.0e-3
+        max_steps: 1000
+        memory_size: 16
+        normalize: false
+        num_update: 1
+        train_interval: 1
+        num_layers: 1
+        time_horizon: 64
+        sequence_length: 32
+        summary_freq: 100
+        tau: 0.01
+        use_recurrent: true
+        curiosity_enc_size: 128
+        demo_path: None
+        vis_encode_type: simple
+        reward_signals:
+            extrinsic:
+                strength: 1.0
+                gamma: 0.99
+    """
+
 GHOST_CONFIG_PASS = f"""
     {BRAIN_NAME}:
         trainer: ppo
@@ -75,14 +135,14 @@ GHOST_CONFIG_PASS = f"""
         beta: 5.0e-3
         buffer_size: 64
         epsilon: 0.2
-        hidden_units: 128
+        hidden_units: 32
         lambd: 0.95
         learning_rate: 5.0e-3
         max_steps: 2500
         memory_size: 256
         normalize: false
         num_epoch: 3
-        num_layers: 2
+        num_layers: 1
         time_horizon: 64
         sequence_length: 64
         summary_freq: 500
@@ -106,14 +166,14 @@ GHOST_CONFIG_FAIL = f"""
         beta: 5.0e-3
         buffer_size: 64
         epsilon: 0.2
-        hidden_units: 128
+        hidden_units: 32
         lambd: 0.95
         learning_rate: 5.0e-3
         max_steps: 2500
         memory_size: 256
         normalize: false
         num_epoch: 3
-        num_layers: 2
+        num_layers: 1
         time_horizon: 64
         sequence_length: 64
         summary_freq: 500
@@ -138,6 +198,22 @@ def default_reward_processor(rewards, last_n_rewards=5):
     return np.array(rewards[-last_n_rewards:], dtype=np.float32).mean()
 
 
+class DebugWriter(StatsWriter):
+    """
+    Print to stdout so stats can be viewed in pytest
+    """
+
+    def write_stats(
+        self, category: str, values: Dict[str, StatsSummary], step: int
+    ) -> None:
+        for val, stats_summary in values.items():
+            if val == "Environment/Cumulative Reward":
+                print(step, val, stats_summary.mean)
+
+    def write_text(self, category: str, text: str, step: int) -> None:
+        pass
+
+
 def _check_environment_trains(
     env,
     config,
@@ -151,6 +227,8 @@ def _check_environment_trains(
         save_freq = 99999
         seed = 1337
         StatsReporter.writers.clear()  # Clear StatsReporters so we don't write to file
+        debug_writer = DebugWriter()
+        StatsReporter.add_writer(debug_writer)
         trainer_config = yaml.safe_load(config)
         env_manager = SimpleEnvManager(env, FloatPropertiesChannel())
         trainer_factory = TrainerFactory(
@@ -196,6 +274,18 @@ def _check_environment_trains(
 def test_simple_ppo(use_discrete):
     env = Simple1DEnvironment([BRAIN_NAME], use_discrete=use_discrete)
     _check_environment_trains(env, PPO_CONFIG)
+
+
+@pytest.mark.parametrize("use_discrete", [True, False])
+def test_recurrent_ppo(use_discrete):
+    env = Memory1DEnvironment([BRAIN_NAME], use_discrete=use_discrete)
+    _check_environment_trains(env, PPO_CONFIG_RECURRENT)
+
+
+@pytest.mark.parametrize("use_discrete", [True, False])
+def test_recurrent_sac(use_discrete):
+    env = Memory1DEnvironment([BRAIN_NAME], use_discrete=use_discrete)
+    _check_environment_trains(env, SAC_CONFIG_RECURRENT)
 
 
 @pytest.mark.parametrize("use_discrete", [True, False])
