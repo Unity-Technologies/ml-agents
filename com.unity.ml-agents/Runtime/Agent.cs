ffc02546c5cb159d1d508d379c12853afd717c2a
Vincent-Pierre BERGES <vincentpierre@unity3d.com>
2020-03-05 12:11:45 -0800

    Added a numberOfActions get property on the BrainParameters (#3571)
    * Added a numberOfActions get property on the BrainParameters
    
    * forgot one place to replace
    
    * [skip ci] numberOfActions --> numActions

--- com.unity.ml-agents/Runtime/Agent.cs
+++ com.unity.ml-agents/Runtime/Agent.cs
@@ -455,16 +455,8 @@ namespace MLAgents
             // should stay the previous action before the Done(), so that it is properly recorded.
             if (m_Action.vectorActions == null)
             {
-                if (param.vectorActionSpaceType == SpaceType.Continuous)
-                {
-                    m_Action.vectorActions = new float[param.vectorActionSize[0]];
-                    m_Info.storedVectorActions = new float[param.vectorActionSize[0]];
-                }
-                else
-                {
-                    m_Action.vectorActions = new float[param.vectorActionSize.Length];
-                    m_Info.storedVectorActions = new float[param.vectorActionSize.Length];
-                }
+                m_Action.vectorActions = new float[param.numActions];
+                m_Info.storedVectorActions = new float[param.numActions];
             }
         }
 
@@ -492,11 +484,8 @@ namespace MLAgents
         {
             Debug.LogWarning("Heuristic method called but not implemented. Returning placeholder actions.");
             var param = m_PolicyFactory.brainParameters;
-            var actionSize = param.vectorActionSpaceType == SpaceType.Continuous ?
-                param.vectorActionSize[0] :
-                param.vectorActionSize.Length;
 
-            return new float[actionSize];
+            return new float[param.numActions];
         }
 
         /// <summary>
