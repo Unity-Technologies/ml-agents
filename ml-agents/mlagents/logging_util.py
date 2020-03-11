04feeae774c081b2faf1a50cc2ea75049f21f12c
Anupam Bhatnagar <anupambhatnagar@gmail.com>
2020-03-11 12:31:34 -0700

    [skip ci] adding horovod rank in log messages

--- ml-agents/mlagents/logging_util.py
+++ ml-agents/mlagents/logging_util.py
@@ -1,9 +1,10 @@
 import logging
+import horovod.tensorflow as hvd
 
 
 def create_logger(name, log_level):
     date_format = "%Y-%m-%d %H:%M:%S"
-    log_format = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    log_format = "%s %(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s" % hvd.rank()
 
     logging.basicConfig(level=log_level, format=log_format, datefmt=date_format)
     logger = logging.getLogger(name=name)
