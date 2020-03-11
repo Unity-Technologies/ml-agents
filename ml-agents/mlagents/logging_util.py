4ef722ac72895f5db4c5d116aebf7499e68699c2
Anupam Bhatnagar <anupambhatnagar@gmail.com>
2020-03-11 13:31:31 -0700

    [skip ci] reformatting log message

--- ml-agents/mlagents/logging_util.py
+++ ml-agents/mlagents/logging_util.py
@@ -4,7 +4,8 @@ import horovod.tensorflow as hvd
 
 def create_logger(name, log_level):
     date_format = "%Y-%m-%d %H:%M:%S"
-    log_format = "%s %(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s" % hvd.rank()
+    horovod_worker = "worker-%s " % str(hvd.rank())
+    log_format = horovod_worker + "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
 
     logging.basicConfig(level=log_level, format=log_format, datefmt=date_format)
     logger = logging.getLogger(name=name)
