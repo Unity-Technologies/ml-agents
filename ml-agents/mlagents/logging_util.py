0e1765b4d2da6997177cefee3ce48e86697503e2
Anupam Bhatnagar <anupambhatnagar@gmail.com>
2020-03-12 12:33:37 -0700

    [skip ci] Revert adding horovod rank in log messages
    This reverts commit 04feeae774c081b2faf1a50cc2ea75049f21f12c.

--- ml-agents/mlagents/logging_util.py
+++ ml-agents/mlagents/logging_util.py
@@ -1,11 +1,9 @@
 import logging
-import horovod.tensorflow as hvd
 
 
 def create_logger(name, log_level):
     date_format = "%Y-%m-%d %H:%M:%S"
-    horovod_worker = "worker-%s " % str(hvd.rank())
-    log_format = horovod_worker + "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+    log_format = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
 
     logging.basicConfig(level=log_level, format=log_format, datefmt=date_format)
     logger = logging.getLogger(name=name)
