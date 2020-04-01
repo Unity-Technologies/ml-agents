e9b65e04cb245e47d24f1a8287cc718c3076a30a
Ervin Teng <ervin@unity3d.com>
2020-04-01 15:03:08 -0700

    Better laser/flame assets and animate flames

--- Project/Assets/ML-Agents/Examples/CubeWars/Scripts/Laser.cs
+++ Project/Assets/ML-Agents/Examples/CubeWars/Scripts/Laser.cs
@@ -7,12 +7,14 @@ public class Laser : MonoBehaviour
     public bool isFired;
     public float maxLength = 25f;
     public float width = 0.5f;
+    public bool animate = false;
     LineRenderer laserRenderer;
     // Start is called before the first frame update
     void Start()
     {
         laserRenderer = GetComponentInChildren<LineRenderer>();
         laserRenderer.SetWidth(width, width);
+        laserRenderer.material.SetTextureScale("_MainTex", new Vector2(0.05f, 1.0f));
     }
 
     // Update is called once per frame
@@ -20,6 +22,10 @@ public class Laser : MonoBehaviour
     {
         if(isFired)
         {
+            if(animate)
+            {
+                laserRenderer.material.SetTextureOffset("_MainTex", new Vector2(-3*Time.time, 0.0f));
+            }
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, maxLength))
             {
