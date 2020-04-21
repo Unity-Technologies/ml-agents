bab7bd24597842718bf626cc7c7e5d192124c8ca
vincentpierre <vincentpierre@unity3d.com>
2020-04-21 11:45:38 -0700

    FoRgOt To rUn PrE-ComMiT

--- gym-unity/gym_unity/tests/test_gym.py
+++ gym-unity/gym_unity/tests/test_gym.py
@@ -20,7 +20,6 @@ def test_gym_wrapper():
         mock_env, mock_spec, mock_decision_step, mock_terminal_step
     )
 
-
     env = UnityToGymWrapper(mock_env, use_visual=False)
     assert isinstance(env, UnityToGymWrapper)
     assert isinstance(env.reset(), np.ndarray)
