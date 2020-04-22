98b807cc10deff0cbc7c958ee863b17ac559bb4f
vincentpierre <vincentpierre@unity3d.com>
2020-04-22 11:09:58 -0700

    Modified the EngineConfig to send one message per field

--- com.unity.ml-agents/Runtime/SideChannels/EngineConfigurationChannel.cs
+++ com.unity.ml-agents/Runtime/SideChannels/EngineConfigurationChannel.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 
 namespace MLAgents.SideChannels
 {
+
     /// <summary>
     /// Side channel that supports modifying attributes specific to the Unity Engine.
     /// </summary>
     internal class EngineConfigurationChannel : SideChannel
     {
+        private enum ConfigurationType : int
+        {
+            Screen = 0,
+            QualityLevel = 1,
+            TimeScale = 2,
+            TargetFrameRate = 3,
+            CaptureFrameRate = 4
+        }
+
         const string k_EngineConfigId = "e951342c-4f7e-11ea-b238-784f4387d1f7";
 
         /// <summary>
@@ -22,20 +32,32 @@ namespace MLAgents.SideChannels
         /// <inheritdoc/>
         public override void OnMessageReceived(IncomingMessage msg)
         {
-            var width = msg.ReadInt32();
-            var height = msg.ReadInt32();
-            var qualityLevel = msg.ReadInt32();
-            var timeScale = msg.ReadFloat32();
-            var targetFrameRate = msg.ReadInt32();
-            var captureFrameRate = msg.ReadInt32();
-
-            timeScale = Mathf.Clamp(timeScale, 1, 100);
-
-            Screen.SetResolution(width, height, false);
-            QualitySettings.SetQualityLevel(qualityLevel, true);
-            Time.timeScale = timeScale;
-            Time.captureFramerate = captureFrameRate;
-            Application.targetFrameRate = targetFrameRate;
+            var messageType = (ConfigurationType)msg.ReadInt32();
+            switch (messageType)
+            {
+                case ConfigurationType.Screen:
+                    var width = msg.ReadInt32();
+                    var height = msg.ReadInt32();
+                    Screen.SetResolution(width, height, false);
+                    break;
+                case ConfigurationType.QualityLevel:
+                    var qualityLevel = msg.ReadInt32();
+                    QualitySettings.SetQualityLevel(qualityLevel, true);
+                    break;
+                case ConfigurationType.TimeScale:
+                    var timeScale = msg.ReadFloat32();
+                    timeScale = Mathf.Clamp(timeScale, 1, 100);
+                    Time.timeScale = timeScale;
+                    break;
+                case ConfigurationType.TargetFrameRate:
+                    var targetFrameRate = msg.ReadInt32();
+                    Application.targetFrameRate = targetFrameRate;
+                    break;
+                case ConfigurationType.CaptureFrameRate:
+                    var captureFrameRate = msg.ReadInt32();
+                    Time.captureFramerate = captureFrameRate;
+                    break;
+            }
         }
     }
 }
