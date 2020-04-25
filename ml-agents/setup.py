ac5b3a0e3d4e09b72420eb4bed84214f26cdfdb7
Chris Elion <chris.elion@unity3d.com>
2020-04-25 16:00:46 -0700

    add six requirement (#3851)

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -64,6 +64,9 @@ setup(
         "pyyaml",
         "tensorflow>=1.7,<3.0",
         'pypiwin32==223;platform_system=="Windows"',
+        # We don't actually need six, but tensorflow does, and pip seems
+        # to get confused and install the wrong version.
+        "six>=1.12.0",
     ],
     python_requires=">=3.6.1",
     entry_points={
