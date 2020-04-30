f2ce4c7a1b10cf0ca3d6e61e95c2c2acea64520a
Ervin Teng <ervin@unity3d.com>
2020-04-29 21:12:06 -0700

    Use attrs for RunOptions and CLI

---
+++ ml-agents/mlagents/trainers/settings.py
@@ -0,0 +1,106 @@
+import attr
+import cattr
+from typing import Dict, Optional, List, Any
+import argparse
+
+from mlagents.trainers.cli_utils import StoreConfigFile, DetectDefault, parser
+from mlagents.trainers.trainer_util import load_config
+from mlagents.trainers.exception import TrainerConfigError
+
+
+@attr.s(auto_attribs=True)
+class CheckpointSettings:
+    save_freq: int = parser.get_default("save_freq")
+    keep_checkpoints: int = parser.get_default("keep_checkpoints")
+    run_id: str = parser.get_default("run_id")
+    initialize_from: str = parser.get_default("initialize_from")
+    load_model: bool = parser.get_default("load_model")
+    resume: bool = parser.get_default("resume")
+    force: bool = parser.get_default("force")
+    train_model: bool = parser.get_default("train_model")
+    inference: bool = parser.get_default("inference")
+    lesson: int = parser.get_default("lesson")
+
+
+@attr.s(auto_attribs=True)
+class EnvironmentSettings:
+    env_path: Optional[str] = parser.get_default("env_path")
+    env_args: Optional[List[str]] = parser.get_default("env_args")
+    base_port: int = parser.get_default("base_port")
+    num_envs: int = parser.get_default("num_envs")
+    seed: int = parser.get_default("seed")
+
+
+@attr.s(auto_attribs=True)
+class EngineSettings:
+    width: int = parser.get_default("width")
+    height: int = parser.get_default("height")
+    quality_level: int = parser.get_default("quality_level")
+    time_scale: float = parser.get_default("time_scale")
+    target_frame_rate: int = parser.get_default("target_frame_rate")
+    capture_frame_rate: int = parser.get_default("capture_frame_rate")
+    no_graphics: bool = parser.get_default("no_graphics")
+
+
+@attr.s(auto_attribs=True)
+class RunOptions:
+    behaviors: Dict[str, Dict]
+    env_settings: EnvironmentSettings = EnvironmentSettings()
+    engine_settings: EngineSettings = EngineSettings()
+    environment_settings: EnvironmentSettings = EnvironmentSettings()
+    parameter_randomization: Optional[Dict] = None
+    curriculum_config: Optional[Dict] = None
+    checkpoint_settings: CheckpointSettings = CheckpointSettings()
+
+    # These are options that are relevant to the run itself, and not the engine or environment.
+    # They will be left here.
+    debug: bool = parser.get_default("debug")
+    multi_gpu: bool = False
+
+    @staticmethod
+    def from_argparse(args: argparse.Namespace) -> "RunOptions":
+        """
+        Takes an argparse.Namespace as specified in `parse_command_line`, loads input configuration files
+        from file paths, and converts to a RunOptions instance.
+        :param args: collection of command-line parameters passed to mlagents-learn
+        :return: RunOptions representing the passed in arguments, with trainer config, curriculum and sampler
+          configs loaded from files.
+        """
+        argparse_args = vars(args)
+        config_path = StoreConfigFile.trainer_config_path
+
+        # Load YAML
+        configured_dict: Dict[str, Any] = {
+            "checkpoint_settings": {},
+            "env_settings": {},
+            "engine_settings": {},
+        }
+        configured_dict.update(load_config(config_path))
+        # This is the only option that is not optional and has no defaults.
+        if "behaviors" not in configured_dict:
+            raise TrainerConfigError(
+                "Trainer configurations not found. Make sure your YAML file has a section for behaviors."
+            )
+        # Use the YAML file values for all values not specified in the CLI.
+        for key in configured_dict.keys():
+            # Detect bad config options
+            if key not in attr.fields_dict(RunOptions):
+                raise TrainerConfigError(
+                    "The option {} was specified in your YAML file, but is invalid.".format(
+                        key
+                    )
+                )
+        # Override with CLI args
+        # Keep deprecated --load working, TODO: remove
+        argparse_args["resume"] = argparse_args["resume"] or argparse_args["load_model"]
+        for key, val in argparse_args.items():
+            if key in DetectDefault.non_default_args:
+                if key in attr.fields_dict(CheckpointSettings):
+                    configured_dict["checkpoint_settings"][key] = val
+                elif key in attr.fields_dict(EnvironmentSettings):
+                    configured_dict["env_settings"][key] = val
+                elif key in attr.fields_dict(EngineSettings):
+                    configured_dict["engine_settings"][key] = val
+                else:  # Base options
+                    configured_dict[key] = val
+        return cattr.structure(configured_dict, RunOptions)
