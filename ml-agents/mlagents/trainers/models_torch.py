Fix bug in probs calculation

ml-agents/mlagents/trainers/models_torch.py
@@ -210,7 +210,7 @@ class ActorCritic(nn.Module):
         log_probs = []
         entropies = []
         for idx, action_dist in enumerate(dists):
-            action = actions[:, idx]
+            action = actions[..., idx]
             log_probs.append(action_dist.log_prob(action))
             entropies.append(action_dist.entropy())
         log_probs = torch.stack(log_probs, dim=-1)

ml-agents/mlagents/trainers/policy/torch_policy.py
@@ -136,23 +136,31 @@ class TorchPolicy(Policy):
         if self.use_vec_obs and self.normalize:
             self.actor_critic.update_normalization(vector_obs)
 
-    def execute_model(
-        self, vec_obs, vis_obs, masks=None, actions=None, memories=None, seq_len=1
-    ):
+    def sample_actions(self, vec_obs, vis_obs, masks=None, memories=None, seq_len=1):
         dists, (
             value_heads,
             mean_value,
-        ), new_memories = self.actor_critic.get_dist_and_value(
+        ), memories = self.actor_critic.get_dist_and_value(
             vec_obs, vis_obs, masks, memories, seq_len
         )
 
-        if actions is None:
-            actions = self.actor_critic.sample_action(dists)
+        actions = self.actor_critic.sample_action(dists)
         log_probs, entropies = self.actor_critic.get_probs_and_entropy(actions, dists)
         if self.act_type == "continuous":
             actions.squeeze_(-1)
 
         return actions, log_probs, entropies, value_heads, memories
+
+    def evaluate_actions(
+        self, vec_obs, vis_obs, masks=None, actions=None, memories=None, seq_len=1
+    ):
+        dists, (value_heads, mean_value), _ = self.actor_critic.get_dist_and_value(
+            vec_obs, vis_obs, masks, memories, seq_len
+        )
+
+        log_probs, entropies = self.actor_critic.get_probs_and_entropy(actions, dists)
+
+        return log_probs, entropies, value_heads
 
     @timed
     def evaluate(
@@ -172,7 +180,7 @@ class TorchPolicy(Policy):
         if masks is not None:
             masks = torch.Tensor(masks)
         run_out = {}
-        action, log_probs, entropy, value_heads, memories = self.execute_model(
+        action, log_probs, entropy, value_heads, memories = self.sample_actions(
             vec_obs, vis_obs, masks=masks, memories=memories
         )
         run_out["action"] = np.array(action.detach())