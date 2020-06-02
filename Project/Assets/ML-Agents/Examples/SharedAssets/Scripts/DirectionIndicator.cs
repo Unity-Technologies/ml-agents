9946be83456313acc7d4b5af655bf7a8c6a6c2c2
HH <brandonh@unity3d.com>
2020-06-01 17:26:26 -0700

    changed property to float

--- Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/DirectionIndicator.cs
+++ Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/DirectionIndicator.cs
@@ -9,16 +9,16 @@ namespace Unity.MLAgentsExamples
         public Transform transformToFollow; //ex: hips or body
         public Transform targetToLookAt; //target in the scene the indicator will point to
         public float heightOffset;
-        private Vector3 m_StartingPos;
+        private float m_StartingYPos;
         
         void OnEnable()
         {
-            m_StartingPos = transform.position;
+            m_StartingYPos = transform.position.y;
         }
 
         void Update()
         {
-            transform.position = new Vector3(transformToFollow.position.x, m_StartingPos.y + heightOffset, transformToFollow.position.z);
+            transform.position = new Vector3(transformToFollow.position.x, m_StartingYPos + heightOffset, transformToFollow.position.z);
             Vector3 m_WalkDir = targetToLookAt.position - transform.position;
             m_WalkDir.y = 0; //flatten dir on the y
             transform.rotation = Quaternion.LookRotation(m_WalkDir);
