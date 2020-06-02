f4dfae3751ae11c25bc6a4981809b5738a079b52
HH <brandonh@unity3d.com>
2020-06-02 14:22:42 -0700

    cleeanup remove tab

--- Project/Assets/ML-Agents/Examples/Walker/Scripts/WalkerAgent.cs
+++ Project/Assets/ML-Agents/Examples/Walker/Scripts/WalkerAgent.cs
@@ -112,7 +112,7 @@ public class WalkerAgent : Agent
             sensor.AddObservation(bp.currentStrength / m_JdController.maxJointForceLimit);
         }
     }
-    
+
     /// <summary>
     /// Loop over body parts to add them to observation.
     /// </summary>
