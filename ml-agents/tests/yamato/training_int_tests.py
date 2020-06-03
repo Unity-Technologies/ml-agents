b1c0f241246f22499a8ac817e73ba34391e1011c
Chris Elion <chris.elion@unity3d.com>
2020-06-03 16:39:12 -0700

    run inference in yamato after training

--- ml-agents/tests/yamato/training_int_tests.py
+++ ml-agents/tests/yamato/training_int_tests.py
@@ -3,6 +3,7 @@ import os
 import sys
 import subprocess
 import time
+from typing import Any
 
 from .yamato_utils import (
     get_base_path,
@@ -16,7 +17,7 @@ from .yamato_utils import (
 )
 
 
-def run_training(python_version, csharp_version):
+def run_training(python_version: str, csharp_version: str) -> bool:
     latest = "latest"
     run_id = int(time.time() * 1000.0)
     print(
@@ -27,7 +28,7 @@ def run_training(python_version, csharp_version):
     if os.path.exists(nn_file_expected):
         # Should never happen - make sure nothing leftover from an old test.
         print("Artifacts from previous build found!")
-        sys.exit(1)
+        return False
 
     base_path = get_base_path()
     print(f"Running in base path {base_path}")
@@ -50,8 +51,8 @@ def run_training(python_version, csharp_version):
         build_returncode = run_standalone_build(base_path)
 
         if build_returncode != 0:
-            print("Standalone build FAILED!")
-            sys.exit(build_returncode)
+            print(f"Standalone build FAILED! with return code {build_returncode}")
+            return False
 
         # Now rename the newly-built executable, and restore the old one
         os.rename(full_player_path, final_player_path)
@@ -66,7 +67,7 @@ def run_training(python_version, csharp_version):
     # and reduce the batch_size and buffer_size enough to ensure an update step happens.
     yaml_out = "override.yaml"
     if python_version:
-        overrides = {"max_steps": 100, "batch_size": 10, "buffer_size": 10}
+        overrides: Any = {"max_steps": 100, "batch_size": 10, "buffer_size": 10}
         override_legacy_config_file(
             python_version, "config/trainer_config.yaml", yaml_out, **overrides
         )
@@ -77,9 +78,9 @@ def run_training(python_version, csharp_version):
         }
         override_config_file("config/ppo/3DBall.yaml", yaml_out, overrides)
 
+    env_path = os.path.join(get_base_output_path(), standalone_player_path)
     mla_learn_cmd = (
-        f"mlagents-learn {yaml_out} --force --env="
-        f"{os.path.join(get_base_output_path(), standalone_player_path)} "
+        f"mlagents-learn {yaml_out} --force --env={env_path} "
         f"--run-id={run_id} --no-graphics --env-args -logFile -"
     )  # noqa
     res = subprocess.run(
@@ -88,10 +89,35 @@ def run_training(python_version, csharp_version):
 
     if res.returncode != 0 or not os.path.exists(nn_file_expected):
         print("mlagents-learn run FAILED!")
-        sys.exit(1)
+        return False
+
+    if csharp_version is None and python_version is None:
+        inference_ok = run_inference(env_path, os.path.dirname(nn_file_expected))
+        if not inference_ok:
+            return False
 
     print("mlagents-learn run SUCCEEDED!")
-    sys.exit(0)
+    return True
+
+
+def run_inference(env_path: str, output_path: str) -> bool:
+    args = [
+        env_path,
+        "-nographics",
+        "-logfile",
+        "-",
+        "--mlagents-override-model-directory",
+        output_path,
+        "--mlagents-quit-on-load-failure",
+        "--mlagents-quit-after-episodes",
+        "2",
+    ]
+    res = subprocess.run(args)
+    if res.returncode != 0:
+        print(res.stdout)
+        print("Error running inference!")
+
+    return True
 
 
 def main():
@@ -101,7 +127,10 @@ def main():
     args = parser.parse_args()
 
     try:
-        run_training(args.python, args.csharp)
+        ok = run_training(args.python, args.csharp)
+        if not ok:
+            sys.exit(1)
+
     finally:
         # Cleanup - this gets executed even if we hit sys.exit()
         undo_git_checkout()
