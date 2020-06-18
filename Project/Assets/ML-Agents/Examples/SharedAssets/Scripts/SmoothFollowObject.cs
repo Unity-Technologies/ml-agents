965825bc7799f6d6b4364015744527865cd76784
HH <brandonh@unity3d.com>
2020-06-18 11:09:44 -0700

    add RewardManager

---
+++ Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/SmoothFollowObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowObject : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offsetInObjectLocalSpace;
+    public float smoothingTime = 2;
+
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position,
+            objectToFollow.transform.TransformPoint(offsetInObjectLocalSpace), ref velocity, smoothingTime);
+    }
+}
