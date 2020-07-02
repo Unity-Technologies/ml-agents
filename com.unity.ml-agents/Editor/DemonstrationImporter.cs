269b214e30bcab3580a405b32d24d76eb5ee40cb
Christopher Goy <christopherg@unity3d.com>
2020-07-02 09:59:22 -0700

    Add semi-colon.

--- com.unity.ml-agents/Editor/DemonstrationImporter.cs
+++ com.unity.ml-agents/Editor/DemonstrationImporter.cs
@@ -5,7 +5,7 @@ using Unity.MLAgents.CommunicatorObjects;
 using UnityEditor;
 using UnityEngine;
 #if UNITY_2020_2_OR_NEWER
-using UnityEditor.AssetImporters
+using UnityEditor.AssetImporters;
 #else
 using UnityEditor.Experimental.AssetImporters;
 #endif
