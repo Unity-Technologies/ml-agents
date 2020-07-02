4ad81e0e16ca64d85b18f5bf8e14fc0d1c30930b
vincentpierre <vincentpierre@unity3d.com>
2020-07-02 14:31:52 -0700

    Better printing of results

--- experiment_torch.py
+++ experiment_torch.py
@@ -9,20 +9,22 @@ from mlagents.trainers.ppo.trainer import TestingConfiguration
 from mlagents_envs.timers import _thread_timer_stacks
 
 
-results = {}
+results = [("name", "steps", "use_torch", "num_torch_threads", "total", "tc_advance_total", "tc_advance_count", "update_total", "update_count", "evaluate_total", "evaluate_count")]
 
-def run_experiment(name:str, steps:int, torch:bool, config_name=None):
+def run_experiment(name:str, steps:int, use_torch:bool, num_torch_threads:int, config_name=None):
 	TestingConfiguration.env_name = name
 	TestingConfiguration.max_steps = steps
-	TestingConfiguration.use_torch = torch
+	TestingConfiguration.use_torch = use_torch
 	if config_name is None:
 		config_name = name
 	run_options = parse_command_line([f"config/ppo/{config_name}.yaml"])
-	run_options.checkpoint_settings.run_id = f"{name}_test_" +str(steps) +"_"+("torch" if torch else "tf")
+	run_options.checkpoint_settings.run_id = f"{name}_test_" +str(steps) +"_"+("torch" if use_torch else "tf")
 	run_options.checkpoint_settings.force = True
 	for trainer_settings in run_options.behaviors.values():
 		trainer_settings.threaded = False
 	timers_path = os.path.join("results", run_options.checkpoint_settings.run_id, "run_logs", "timers.json")
+	if use_torch:
+		torch.set_num_threads(num_torch_threads)
 	run_cli(run_options)
 	StatsReporter.writers.clear()
 	StatsReporter.stats_dict.clear()
@@ -30,42 +32,49 @@ def run_experiment(name:str, steps:int, torch:bool, config_name=None):
 	with open(timers_path) as timers_json_file:
 		timers_json = json.load(timers_json_file)
 		total = timers_json["total"]
+		tc_advance = timers_json["children"]["TrainerController.start_learning"]["children"]["TrainerController.advance"]
 		evaluate = timers_json["children"]["TrainerController.start_learning"]["children"]["TrainerController.advance"]["children"]["env_step"]["children"]["SubprocessEnvManager._take_step"]["children"]
 		update = timers_json["children"]["TrainerController.start_learning"]["children"]["TrainerController.advance"]["children"]["trainer_advance"]["children"]["_update_policy"]["children"]
-	if torch:
-		update = update["TorchPPOOptimizer.update"]["total"]
-		evaluate = evaluate["TorchPolicy.evaluate"]["total"]
+		tc_advance_total = tc_advance["total"]
+		tc_advance_count = tc_advance["count"]
+	if use_torch:
+		update_total = update["TorchPPOOptimizer.update"]["total"]
+		evaluate_total = evaluate["TorchPolicy.evaluate"]["total"]
+		update_count = update["TorchPPOOptimizer.update"]["count"]
+		evaluate_count = evaluate["TorchPolicy.evaluate"]["count"]
 	else:
-		update = update["TFPPOOptimizer.update"]["total"]
-		evaluate = evaluate["NNPolicy.evaluate"]["total"]
+		update_total = update["TFPPOOptimizer.update"]["total"]
+		evaluate_total = evaluate["NNPolicy.evaluate"]["total"]
+		update_count = update["TFPPOOptimizer.update"]["count"]
+		evaluate_count= evaluate["NNPolicy.evaluate"]["count"]
 	# todo: do total / count
-	return total, update, evaluate
+	return name, steps, use_torch, num_torch_threads, total, tc_advance_total, tc_advance_count, update_total, update_count, evaluate_total, evaluate_count
 
 
 
-results["3DBall Torch"] = run_experiment("3DBall", 20000, True)
-results["3DBall TF"] = run_experiment("3DBall", 20000, False)
-results["GridWorld Torch"] = run_experiment("GridWorld", 2000, True)
-results["GridWorld TF"] = run_experiment("GridWorld", 2000, False)
-results["PushBlock Torch"] = run_experiment("PushBlock", 20000, True)
-results["PushBlock TF"] = run_experiment("PushBlock", 20000, False)
-results["Hallway Torch"] = run_experiment("Hallway", 20000, True)
-results["Hallway TF"] = run_experiment("Hallway", 20000, False)
-results["CrawlerStaticTarget Torch"] = run_experiment("CrawlerStaticTarget", 50000, True, "CrawlerStatic")
-results["CrawlerStaticTarget TF"] = run_experiment("CrawlerStaticTarget", 50000, False, "CrawlerStatic")
 
-torch.set_num_threads(1)
+results.append(run_experiment("3DBall", 20000, True, 4))
+results.append(run_experiment("3DBall", 20000, True, 1))
+results.append(run_experiment("3DBall", 20000, False, None))
 
-results["3DBall Torch 1 thread"] = run_experiment("3DBall", 20000, True)
-results["GridWorld Torch 1 thread"] = run_experiment("GridWorld", 2000, True)
-results["PushBlock Torch 1 thread"] = run_experiment("PushBlock", 20000, True)
-results["Hallway Torch 1 thread"] = run_experiment("Hallway", 20000, True)
-results["CrawlerStaticTarget Torch 1 thread"] = run_experiment("CrawlerStaticTarget", 50000, True, "CrawlerStatic")
+results.append(run_experiment("GridWorld", 2000, True, 4))
+results.append(run_experiment("GridWorld", 2000, True, 1))
+results.append(run_experiment("GridWorld", 2000, False, None))
+
+results.append(run_experiment("PushBlock", 20000, True, 4))
+results.append(run_experiment("PushBlock", 20000, True, 1))
+results.append(run_experiment("PushBlock", 20000, False, None))
+
+results.append(run_experiment("Hallway", 20000, True, 4))
+results.append(run_experiment("Hallway", 20000, True, 1))
+results.append(run_experiment("Hallway", 20000, False, None))
+
+results.append(run_experiment("CrawlerStaticTarget", 50000, True, 4, "CrawlerStatic"))
+results.append(run_experiment("CrawlerStaticTarget", 50000, True, 1, "CrawlerStatic"))
+results.append(run_experiment("CrawlerStaticTarget", 50000, False, None, "CrawlerStatic"))
 
 
-print("experiment,\t", "total,\t", "update,\t", "evaluate")
-for key, value in results.items():
-	print(key + ",\t", str(value[0])+ ",\t", str(value[1])+ ",\t", str(value[2])+ "\t")
-
+for r in results:
+	print(*r)
 
 
