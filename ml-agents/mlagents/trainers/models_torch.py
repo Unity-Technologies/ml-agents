2224fa03f04b3788da6bf949d47c7dbbf14bdbe5
vincentpierre <vincentpierre@unity3d.com>
2020-07-02 19:56:03 -0700

    _

--- ml-agents/mlagents/trainers/models_torch.py
+++ ml-agents/mlagents/trainers/models_torch.py
@@ -136,7 +136,7 @@ class NetworkBody(nn.Module):
         if self.use_lstm:
             embedding = embedding.view([sequence_length, -1, self.h_size])
             memories = torch.split(memories, self.m_size // 2, dim=-1)
-            embedding, memories = self.lstm(embedding, memories)
+            embedding, memories = self.lstm(embedding.continuous(), memories.continuous())
             embedding = embedding.view([-1, self.m_size // 2])
             memories = torch.cat(memories, dim=-1)
         return embedding, memories
