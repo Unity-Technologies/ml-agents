Allow caching to fix numerical instability

ml-agents/mlagents/trainers/distributions_torch.py
@@ -34,9 +34,13 @@ class GaussianDistInstance(nn.Module):
 
 
 class TanhGaussianDistInstance(GaussianDistInstance):
+    def __init__(self, mean, std):
+        super().__init__(mean, std)
+        self.transform = torch.distributions.transforms.TanhTransform(cache_size=1)
+
     def sample(self):
         unsquashed_sample = super().sample()
-        squashed = torch.tanh(unsquashed_sample)
+        squashed = self.transform(unsquashed_sample)
         return squashed
 
     def _inverse_tanh(self, value):
@@ -44,8 +48,9 @@ class TanhGaussianDistInstance(GaussianDistInstance):
         return 0.5 * torch.log((1 + capped_value) / (1 - capped_value) + EPSILON)
 
     def log_prob(self, value):
-        return super().log_prob(self._inverse_tanh(value)) - torch.log(
-            1 - value ** 2 + EPSILON
+        unsquashed = self.transform.inv(value)
+        return super().log_prob(unsquashed) - self.transform.log_abs_det_jacobian(
+            unsquashed, value
         )
 
 

ml-agents/mlagents/trainers/models_torch.py
@@ -336,14 +336,12 @@ class ActorCritic(nn.Module):
         for action_dist in dists:
             action = action_dist.sample()
             actions.append(action)
-        actions = torch.stack(actions, dim=-1)
         return actions
 
-    def get_probs_and_entropy(self, actions, dists):
+    def get_probs_and_entropy(self, action_list, dists):
         log_probs = []
         entropies = []
-        for idx, action_dist in enumerate(dists):
-            action = actions[..., idx]
+        for action, action_dist in zip(action_list, dists):
             log_prob = action_dist.log_prob(action)
             log_probs.append(log_prob)
             entropies.append(action_dist.entropy())
@@ -379,7 +377,8 @@ class ActorCritic(nn.Module):
         dists, value_outputs, memories = self.get_dist_and_value(
             vec_inputs, vis_inputs, masks, memories, sequence_length
         )
-        sampled_actions = self.sample_action(dists)
+        action_list = self.sample_action(dists)
+        sampled_actions = torch.stack(action_list, dim=-1)
         return (
             sampled_actions,
             dists[0].pdf(sampled_actions),

ml-agents/mlagents/trainers/policy/torch_policy.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List
 import numpy as np
 import torch
+
 import os
 from torch import onnx
 from mlagents.trainers.action_info import ActionInfo
@@ -151,8 +152,11 @@ class TorchPolicy(Policy):
             vec_obs, vis_obs, masks, memories, seq_len
         )
 
-        actions = self.actor_critic.sample_action(dists)
-        log_probs, entropies = self.actor_critic.get_probs_and_entropy(actions, dists)
+        action_list = self.actor_critic.sample_action(dists)
+        log_probs, entropies = self.actor_critic.get_probs_and_entropy(
+            action_list, dists
+        )
+        actions = torch.stack(action_list, dim=-1)
         if self.use_continuous_act:
             actions = actions[:, :, 0]
         else:
@@ -166,8 +170,10 @@ class TorchPolicy(Policy):
         dists, (value_heads, mean_value), _ = self.actor_critic.get_dist_and_value(
             vec_obs, vis_obs, masks, memories, seq_len
         )
-
-        log_probs, entropies = self.actor_critic.get_probs_and_entropy(actions, dists)
+        action_list = [actions[..., i] for i in range(actions.shape[2])]
+        log_probs, entropies = self.actor_critic.get_probs_and_entropy(
+            action_list, dists
+        )
 
         return log_probs, entropies, value_heads
 