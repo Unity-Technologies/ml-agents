102e4cfbfb36a31fdb71070bb23043a84975fae4
yanchaosun <yanchao.sun@unity3d.com>
2020-07-28 13:34:12 -0400

    reuse encoder and linear

--- Project/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DHardAgent.cs
+++ Project/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DHardAgent.cs
@@ -48,7 +48,8 @@ public class Ball3DHardAgent : Agent
         }
         else
         {
-            SetReward(0.1f);
+            // SetReward(0.1f);
+            SetReward(0.1f - (0.05f * actionZ * actionZ + 0.05f * actionX * actionX));
         }
     }
 
