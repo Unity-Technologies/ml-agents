3b994b03ee4636d67df2777e6ad71079e1c9512b
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2020-08-10 16:08:50 -0700

    fix test

--- ml-agents/mlagents/trainers/tests/test_simple_rl.py
+++ ml-agents/mlagents/trainers/tests/test_simple_rl.py
@@ -74,9 +74,9 @@ SAC_CONFIG = TrainerSettings(
 
 
 # The reward processor is passed as an argument to _check_environment_trains.
-# It is applied to the list pf all final rewards for each brain individually.
+# It is applied to the list of all final rewards for each brain individually.
 # This is so that we can process all final rewards in different ways for different algorithms.
-# Custom reward processors shuld be built within the test function and passed to _check_environment_trains
+# Custom reward processors should be built within the test function and passed to _check_environment_trains
 # Default is average over the last 5 final rewards
 def default_reward_processor(rewards, last_n_rewards=5):
     rewards_to_use = rewards[-last_n_rewards:]
@@ -300,9 +300,9 @@ def test_visual_advanced_sac(vis_encode_type, num_visual):
 
 @pytest.mark.parametrize("use_discrete", [True, False])
 def test_recurrent_sac(use_discrete):
-    step_size = 0.2 if use_discrete else 1.0
+    step_size = 0.2
     env = MemoryEnvironment(
-        [BRAIN_NAME], use_discrete=use_discrete, step_size=step_size
+        [BRAIN_NAME], use_discrete=use_discrete, step_size=step_size,
     )
     new_networksettings = attr.evolve(
         SAC_CONFIG.network_settings,
