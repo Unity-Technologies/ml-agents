9430fb39a746852a0831159c3ebbb739abf7e0a1
Ervin T <ervin@unity3d.com>
2020-08-10 14:41:05 -0700

    [add-fire] Fix CategoricalDistInstance test and replace `range` with `arange` (#4327)

--- ml-agents/mlagents/trainers/torch/distributions.py
+++ ml-agents/mlagents/trainers/torch/distributions.py
@@ -100,8 +100,12 @@ class CategoricalDistInstance(DiscreteDistInstance):
         return torch.multinomial(self.probs, 1)
 
     def pdf(self, value):
-        idx = torch.range(end=len(value)).unsqueeze(-1)
-        return torch.gather(self.probs.permute(1, 0)[value.flatten().long()], -1, idx).squeeze(-1)
+        # This function is equivalent to torch.diag(self.probs.T[value.flatten().long()]),
+        # but torch.diag is not supported by ONNX export.
+        idx = torch.arange(start=0, end=len(value)).unsqueeze(-1)
+        return torch.gather(
+            self.probs.permute(1, 0)[value.flatten().long()], -1, idx
+        ).squeeze(-1)
 
     def log_prob(self, value):
         return torch.log(self.pdf(value))
