cb15552090c8f934f2bd7bdb63684ca8f1cdba33
Chris Elion <chris.elion@unity3d.com>
2020-08-13 13:12:58 -0700

    process->node

--- ml-agents/mlagents/tf_utils/globals.py
+++ ml-agents/mlagents/tf_utils/globals.py
@@ -5,7 +5,7 @@ _rank: Optional[int] = None
 
 def get_rank() -> Optional[int]:
     """
-    Returns the rank (in the MPI sense) of the current process.
+    Returns the rank (in the MPI sense) of the current node.
     For local training, this will always be None.
     If this needs to be used, it should be done from outside ml-agents.
     :return:
