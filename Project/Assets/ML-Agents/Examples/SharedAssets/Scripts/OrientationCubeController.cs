43abe49c33bc8707699c54839fd676f9faa4ac35
HH <brandonh@unity3d.com>
2020-08-13 13:07:37 -0700

    reset script to master

--- Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/OrientationCubeController.cs
+++ Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/OrientationCubeController.cs
@@ -7,11 +7,11 @@ namespace Unity.MLAgentsExamples
     /// </summary>
     public class OrientationCubeController : MonoBehaviour
     {
-        //Public method to allow Agent to set look rotation of this transform
+        //Update position and Rotation
         public void UpdateOrientation(Transform rootBP, Transform target)
         {
             var dirVector = target.position - transform.position;
-            dirVector.y = 0; //flatten dir on the y. this will only work on level surfaces
+            dirVector.y = 0; //flatten dir on the y. this will only work on level, uneven surfaces
             var lookRot =
                 dirVector == Vector3.zero
                     ? Quaternion.identity
