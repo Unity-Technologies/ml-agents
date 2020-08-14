23d9f0af8dcf4868931eb5d999f9c41e3c455a62
Christopher Goy <christopherg@unity3d.com>
2020-08-14 14:45:03 -0700

    Import Actuator namespace.

--- DevProject/Assets/ML-Agents/Scripts/Tests/Performance/SensorPerformanceTests.cs
+++ DevProject/Assets/ML-Agents/Scripts/Tests/Performance/SensorPerformanceTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Policies;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Sensors.Reflection;
