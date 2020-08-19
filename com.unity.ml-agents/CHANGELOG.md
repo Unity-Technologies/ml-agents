2a3bf811669063e64a830ea1d128d5ec1a71f7be
Christopher Goy <christopherg@unity3d.com>
2020-08-19 13:40:36 -0700

    Update version numbers.

--- com.unity.ml-agents/CHANGELOG.md
+++ com.unity.ml-agents/CHANGELOG.md
@@ -6,7 +6,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to
 [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [1.0.3] - 2020-07-07
+## [1.0.4] - 2020-08-19
 ### Minor Changes
 #### com.unity.ml-agents (C#)
 - Update Barracuda to 1.0.2. (#4385)
--- com.unity.ml-agents/Runtime/Academy.cs
+++ com.unity.ml-agents/Runtime/Academy.cs
@@ -68,7 +68,7 @@ namespace Unity.MLAgents
         /// Unity package version of com.unity.ml-agents.
         /// This must match the version string in package.json and is checked in a unit test.
         /// </summary>
-        internal const string k_PackageVersion = "1.0.3";
+        internal const string k_PackageVersion = "1.0.4";
 
         const int k_EditorTrainingPort = 5004;
 
