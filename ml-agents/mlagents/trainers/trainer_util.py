166e3c4248292dd2ed94ada0ec818096c45ae557
Anupam Bhatnagar <anupambhatnagar@gmail.com>
2020-08-20 12:04:15 -0700

    [skip ci] renaming trainer to ppo_trainer and sac_trainer to avoid name collision

--- ml-agents/mlagents/trainers/trainer_util.py
+++ ml-agents/mlagents/trainers/trainer_util.py
@@ -11,8 +11,8 @@ from mlagents.trainers.sac.trainer import SACTrainer
 from mlagents.trainers.ghost.trainer import GhostTrainer
 from mlagents.trainers.ghost.controller import GhostController
 from mlagents.trainers.settings import TrainerSettings, TrainerType
-from mlagents_distributed.trainers.ppo.trainer import DistributedPPOTrainer
-from mlagents_distributed.trainers.sac.trainer import DistributedSACTrainer
+from mlagents_distributed.trainers.ppo.ppo_trainer import DistributedPPOTrainer
+from mlagents_distributed.trainers.sac.sac_trainer import DistributedSACTrainer
 
 logger = get_logger(__name__)
 
