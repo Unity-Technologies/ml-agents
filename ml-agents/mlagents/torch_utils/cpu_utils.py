d3ccff92d0d390425d6933e4a8409268cd2f7331
Ervin Teng <ervin@unity3d.com>
2020-09-11 11:27:13 -0700

    Change function names

--- ml-agents/mlagents/torch_utils/cpu_utils.py
+++ ml-agents/mlagents/torch_utils/cpu_utils.py
@@ -9,11 +9,11 @@ def get_num_threads_to_use() -> Optional[int]:
     need, but for smaller machines, we'd like to scale to less than that.
     By default, PyTorch uses 1/2 of the available cores.
     """
-    num_cpus = _get_num_cpus()
+    num_cpus = _get_num_available_cpus()
     return max(min(num_cpus // 2, 4), 1) if num_cpus is not None else None
 
 
-def _get_num_cpus() -> Optional[int]:
+def _get_num_available_cpus() -> Optional[int]:
     """
     Returns number of CPUs using cgroups if possible. This accounts
     for Docker containers that are limited in cores.
