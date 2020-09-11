dab488344ab0ec1f1efd5a051f3eec97af996eea
Ervin Teng <ervin@unity3d.com>
2020-09-10 17:22:32 -0700

    Pylint-friendly logic

--- ml-agents/mlagents/torch_utils/cpu_utils.py
+++ ml-agents/mlagents/torch_utils/cpu_utils.py
@@ -18,17 +18,17 @@ def _get_num_cpus() -> Optional[int]:
     Returns number of CPUs using cgroups if possible. This accounts
     for Docker containers that are limited in cores.
     """
-    try:
-        period = _read_in_integer_file("/sys/fs/cgroup/cpu/cpu.cfs_period_us")
-        quota = _read_in_integer_file("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")
-        num_cpus = quota // period
-        if num_cpus > 0:
-            return num_cpus
-    except FileNotFoundError:
-        pass
-    return os.cpu_count()
+    period = _read_in_integer_file("/sys/fs/cgroup/cpu/cpu.cfs_period_us")
+    quota = _read_in_integer_file("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")
+    if period > 0 and quota > 0:
+        return int(quota // period)
+    else:
+        return os.cpu_count()
 
 
 def _read_in_integer_file(filename: str) -> int:
-    with open(filename) as f:
-        return int(f.readlines()[0])
+    try:
+        with open(filename) as f:
+            return int(f.readlines()[0])
+    except FileNotFoundError:
+        return -1
