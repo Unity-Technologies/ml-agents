a16b8b0dcbe6ad06bc8984d6f7d8ff291d5e1da7
Chris Elion <chris.elion@unity3d.com>
2020-09-23 16:58:47 -0700

    reduce dependency on Match3Agent

--- Project/Assets/ML-Agents/Examples/Match3/Scripts/Match3SensorComponent.cs
+++ Project/Assets/ML-Agents/Examples/Match3/Scripts/Match3SensorComponent.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Unity.MLAgents;
+using Unity.MLAgents.Extensions.Match3;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 
@@ -13,7 +14,7 @@ namespace Unity.MLAgentsExamples
 
         public override ISensor CreateSensor()
         {
-            return new Match3Sensor(Agent, UseVectorObservations);
+            return new Match3Sensor(Agent.Board, UseVectorObservations);
         }
 
         public override int[] GetObservationShape()
@@ -31,17 +32,17 @@ namespace Unity.MLAgentsExamples
 
     public class Match3Sensor : ISensor
     {
-        private Match3Agent m_Agent;
+        private AbstractBoard m_Board;
         private bool m_UseVectorObservations;
         private int[] m_shape;
 
-        public Match3Sensor(Match3Agent agent, bool useVectorObservations)
+        public Match3Sensor(AbstractBoard board, bool useVectorObservations)
         {
-            m_Agent = agent;
+            m_Board = board;
             m_UseVectorObservations = useVectorObservations;
             m_shape = useVectorObservations ?
-                new[] { agent.Rows * agent.Cols * agent.NumCellTypes } :
-                new[] { agent.Rows, agent.Cols, agent.NumCellTypes };
+                new[] { m_Board.Rows * m_Board.Columns * m_Board.NumCellTypes } :
+                new[] { m_Board.Rows, m_Board.Columns, m_Board.NumCellTypes };
         }
 
         public int[] GetObservationShape()
@@ -54,12 +55,12 @@ namespace Unity.MLAgentsExamples
             if (m_UseVectorObservations)
             {
                 int offset = 0;
-                for (var r = 0; r < m_Agent.Rows; r++)
+                for (var r = 0; r < m_Board.Rows; r++)
                 {
-                    for (var c = 0; c < m_Agent.Cols; c++)
+                    for (var c = 0; c < m_Board.Columns; c++)
                     {
-                        var val = m_Agent.Board.Cells[c, r];
-                        for (var i = 0; i < m_Agent.NumCellTypes; i++)
+                        var val = m_Board.GetCellType(r, c);
+                        for (var i = 0; i < m_Board.NumCellTypes; i++)
                         {
                             writer[offset] = (i == val) ? 1.0f : 0.0f;
                             offset++;
@@ -73,12 +74,12 @@ namespace Unity.MLAgentsExamples
             {
                 // TODO combine loops? Only difference is inner-most statement.
                 int offset = 0;
-                for (var r = 0; r < m_Agent.Rows; r++)
+                for (var r = 0; r < m_Board.Rows; r++)
                 {
-                    for (var c = 0; c < m_Agent.Cols; c++)
+                    for (var c = 0; c < m_Board.Columns; c++)
                     {
-                        var val = m_Agent.Board.Cells[c, r];
-                        for (var i = 0; i < m_Agent.NumCellTypes; i++)
+                        var val = m_Board.GetCellType(r, c);
+                        for (var i = 0; i < m_Board.NumCellTypes; i++)
                         {
                             writer[r, c, i] = (i == val) ? 1.0f : 0.0f;
                             offset++;
@@ -92,15 +93,15 @@ namespace Unity.MLAgentsExamples
 
         public byte[] GetCompressedObservation()
         {
-            var height = m_Agent.Rows;
-            var width = m_Agent.Cols;
+            var height = m_Board.Rows;
+            var width = m_Board.Columns;
             var tempTexture = new Texture2D(width, height, TextureFormat.RGB24, false);
             var converter = new OneHotToTextureUtil(height, width);
             var bytesOut = new List<byte>();
-            var numImages = (m_Agent.NumCellTypes + 2) / 3;
+            var numImages = (m_Board.NumCellTypes + 2) / 3;
             for (var i = 0; i < numImages; i++)
             {
-                converter.EncodeToTexture(m_Agent.Board.Cells, tempTexture, 3 * i);
+                converter.EncodeToTexture(m_Board, tempTexture, 3 * i);
                 bytesOut.AddRange(tempTexture.EncodeToPNG());
             }
 
@@ -161,14 +162,14 @@ namespace Unity.MLAgentsExamples
             m_Width = width;
         }
 
-        public void EncodeToTexture(int[,] oneHotValues, Texture2D texture, int channelOffset)
+        public void EncodeToTexture(AbstractBoard board, Texture2D texture, int channelOffset)
         {
             var i = 0;
             for (var h = 0; h < m_Height; h++)
             {
                 for (var w = 0; w < m_Width; w++)
                 {
-                    int oneHotValue = oneHotValues[w, h]; // TODO swap storage for board
+                    int oneHotValue = board.GetCellType(h, w);
                     if (oneHotValue < channelOffset || oneHotValue >= channelOffset + 3)
                     {
                         m_Colors[i++] = Color.black;
