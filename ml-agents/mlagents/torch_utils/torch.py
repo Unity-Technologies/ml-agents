69b1a4dd5067e123a00a01a154544e46e97cdc56
vincentpierre <vincentpierre@unity3d.com>
2020-10-01 10:00:52 -0700

    Torch not imported error to raise at first usage

--- ml-agents/mlagents/torch_utils/torch.py
+++ ml-agents/mlagents/torch_utils/torch.py
@@ -1,10 +1,33 @@
 import os
 
+from distutils.version import LooseVersion
+import pkg_resources
 from mlagents.torch_utils import cpu_utils
+
+
+def assert_torch_installed():
+    # Check that torch version 1.6.0 or later has been installed. If not, refer
+    # user to the PyTorch webpage for install instructions.
+    torch_pkg = None
+    try:
+        torch_pkg = pkg_resources.get_distribution("torch")
+    except pkg_resources.DistributionNotFound:
+        pass
+    assert torch_pkg is not None and LooseVersion(torch_pkg.version) >= LooseVersion(
+        "1.6.0"
+    ), (
+        "A compatible version of PyTorch was not installed. Please visit the PyTorch homepage ",
+        "(https://pytorch.org/get-started/locally/) and follow the instructions to install. ",
+        "Version 1.6.0 and later are supported.",
+    )
+
+
+assert_torch_installed()
 
 # This should be the only place that we import torch directly.
 # Everywhere else is caught by the banned-modules setting for flake8
 import torch  # noqa I201
+
 
 torch.set_num_threads(cpu_utils.get_num_threads_to_use())
 os.environ["KMP_BLOCKTIME"] = "0"
