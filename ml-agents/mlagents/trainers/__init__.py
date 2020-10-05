6ed23c9247621800e82fb1e8092b54110a54bcba
Chris Elion <chris.elion@unity3d.com>
2020-10-05 10:20:06 -0700

    cleanup step names

--- ml-agents/mlagents/trainers/__init__.py
+++ ml-agents/mlagents/trainers/__init__.py
@@ -1,5 +1,5 @@
 # Version of the library that will be used to upload to pypi
-__version__ = "0.21.0.dev0"
+__version__ = "0.21.0.dev1"
 
 # Git tag that will be checked to determine whether to trigger upload to pypi
-__release_tag__ = "release_8_test0"  # TODO DO NOT MERGE
+__release_tag__ = "release_8_test1"  # TODO DO NOT MERGE
