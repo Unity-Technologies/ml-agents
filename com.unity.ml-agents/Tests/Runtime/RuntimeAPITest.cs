72f6598ba13d63f298e67f2b63288d557f40e89e
Chris Elion <chris.elion@unity3d.com>
2020-10-06 17:33:18 -0700

    Run DevProject tests in yamato (#4542)
    * run devproject tests
    
    * loosen trigger for testing
    
    * reset Academy at start of test
    
    * redo trigger, add comment

--- com.unity.ml-agents/Tests/Runtime/RuntimeAPITest.cs
+++ com.unity.ml-agents/Tests/Runtime/RuntimeAPITest.cs
@@ -55,6 +55,10 @@ namespace Tests
         [SetUp]
         public static void Setup()
         {
+            if (Academy.IsInitialized)
+            {
+                Academy.Instance.Dispose();
+            }
             Academy.Instance.AutomaticSteppingEnabled = false;
         }
 
