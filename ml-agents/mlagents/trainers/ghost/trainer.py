58960627b881a205317fc5e416a801cc6c25865a
Ervin Teng <ervin@unity3d.com>
2020-10-21 16:34:47 -0700

    Add torch test and fix torch.

--- ml-agents/mlagents/trainers/ghost/trainer.py
+++ ml-agents/mlagents/trainers/ghost/trainer.py
@@ -319,9 +319,6 @@ class GhostTrainer(Trainer):
         policy = self.trainer.create_policy(
             parsed_behavior_id, behavior_spec, create_graph=True
         )
-        # Initialize policy. You never want to load it, since that should only
-        # be done to the wrapped policy
-        policy.initialize()
         team_id = parsed_behavior_id.team_id
         self.controller.subscribe_team_id(team_id, self)
 
