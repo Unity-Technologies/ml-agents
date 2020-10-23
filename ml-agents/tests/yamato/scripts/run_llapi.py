c05c40e88072fe055da87cb506a7997ba22fcd3f
Andrew Cohen <andrew.cohen@unity3d.com>
2020-10-23 09:08:45 -0400

    fix yamato

--- ml-agents/tests/yamato/scripts/run_llapi.py
+++ ml-agents/tests/yamato/scripts/run_llapi.py
@@ -53,11 +53,13 @@ def test_run_environment(env_name):
             episode_rewards = 0
             tracked_agent = -1
             while not done:
-                if group_spec.is_continuous():
-                    action = np.random.randn(len(decision_steps), group_spec.size)
+                if group_spec.action_spec.is_continuous():
+                    action = np.random.randn(
+                        len(decision_steps), group_spec.action_spec.size
+                    )
 
-                elif group_spec.is_discrete():
-                    branch_size = group_spec.discrete_branches
+                elif group_spec.action_spec.is_discrete():
+                    branch_size = group_spec.action_spec.discrete_branches
                     action = np.column_stack(
                         [
                             np.random.randint(
