10944f1c660226cf89f6e7d0bb5a91fbc0abfcb5
Andrew Cohen <andrew.cohen@unity3d.com>
2020-11-06 11:31:27 -0500

    add docstrings to AgentAction and ActionLogProbs

--- ml-agents/mlagents/trainers/torch/utils.py
+++ ml-agents/mlagents/trainers/torch/utils.py
@@ -16,14 +16,30 @@ from mlagents.trainers.torch.distributions import DistInstance, DiscreteDistInst
 
 
 class AgentAction(NamedTuple):
+    """
+    A NamedTuple containing the tensor for continuous actions and list of tensors for
+    discrete actions. Utility functions provide numpy <=> tensor conversions to be
+    sent as actions to the environment manager as well as used by the optimizers.
+    :param continuous_tensor: Torch tensor corresponding to continuous actions
+    :param discrete_list: List of Torch tensors each corresponding to discrete actions
+    """
+
     continuous_tensor: torch.Tensor
     discrete_list: List[torch.Tensor]
 
     @property
     def discrete_tensor(self):
+        """
+        Returns the discrete action list as a stacked tensor
+        """
         return torch.stack(self.discrete_list, dim=-1)
 
     def to_numpy_dict(self) -> Dict[str, np.ndarray]:
+        """
+        Returns a Dict of np arrays with an entry correspinding to the continuous action
+        and an entry corresponding to the discrete action. "continuous_action" and
+        "discrete_action" are added to the agents buffer individually to maintain a flat buffer.
+        """
         array_dict: Dict[str, np.ndarray] = {}
         if self.continuous_tensor is not None:
             array_dict["continuous_action"] = ModelUtils.to_numpy(
@@ -36,6 +52,10 @@ class AgentAction(NamedTuple):
         return array_dict
 
     def to_tensor_list(self) -> List[torch.Tensor]:
+        """
+        Returns the tensors in the AgentAction as a flat List of torch Tensors. This will be removed
+        when the ActionModel is merged.
+        """
         tensor_list: List[torch.Tensor] = []
         if self.continuous_tensor is not None:
             tensor_list.append(self.continuous_tensor)
@@ -49,6 +69,10 @@ class AgentAction(NamedTuple):
     def create(
         tensor_list: List[torch.Tensor], action_spec: ActionSpec
     ) -> "AgentAction":
+        """
+        A static method that converts a list of torch Tensors into an AgentAction using the ActionSpec.
+        This will change (and may be removed) in the ActionModel.
+        """
         continuous: torch.Tensor = None
         discrete: List[torch.Tensor] = None  # type: ignore
         _offset = 0
@@ -61,6 +85,10 @@ class AgentAction(NamedTuple):
 
     @staticmethod
     def from_dict(buff: Dict[str, np.ndarray]) -> "AgentAction":
+        """
+        A static method that accesses continuous and discrete action fields in an AgentBuffer
+        and constructs the corresponding AgentAction from the retrieved np arrays.
+        """
         continuous: torch.Tensor = None
         discrete: List[torch.Tensor] = None  # type: ignore
         if "continuous_action" in buff:
@@ -76,19 +104,40 @@ class AgentAction(NamedTuple):
 
 
 class ActionLogProbs(NamedTuple):
+    """
+    A NamedTuple containing the tensor for continuous log probs and list of tensors for
+    discrete log probs of individual actions as well as all the log probs for an entire branch.
+    Utility functions provide numpy <=> tensor conversions to be used by the optimizers.
+    :param continuous_tensor: Torch tensor corresponding to log probs of continuous actions
+    :param discrete_list: List of Torch tensors each corresponding to log probs of discrete actions
+    :param all_discrete_list: List of Torch tensors each corresponding to all log probs of
+    a discrete action branch
+    """
+
     continuous_tensor: torch.Tensor
     discrete_list: List[torch.Tensor]
-    all_discrete_list: List[torch.Tensor]
+    all_discrete_list: Optional[List[torch.Tensor]]
 
     @property
     def discrete_tensor(self):
+        """
+        Returns the discrete log probs list as a stacked tensor
+        """
         return torch.stack(self.discrete_list, dim=-1)
 
     @property
     def all_discrete_tensor(self):
+        """
+        Returns the discrete log probs of each branch as a tensor
+        """
         return torch.cat(self.all_discrete_list, dim=1)
 
     def to_numpy_dict(self) -> Dict[str, np.ndarray]:
+        """
+        Returns a Dict of np arrays with an entry correspinding to the continuous log probs
+        and an entry corresponding to the discrete log probs. "continuous_log_probs" and
+        "discrete_log_probs" are added to the agents buffer individually to maintain a flat buffer.
+        """
         array_dict: Dict[str, np.ndarray] = {}
         if self.continuous_tensor is not None:
             array_dict["continuous_log_probs"] = ModelUtils.to_numpy(
@@ -99,7 +148,11 @@ class ActionLogProbs(NamedTuple):
             array_dict["discrete_log_probs"] = ModelUtils.to_numpy(self.discrete_tensor)
         return array_dict
 
-    def to_tensor_list(self) -> List[torch.Tensor]:
+    def _to_tensor_list(self) -> List[torch.Tensor]:
+        """
+        Returns the tensors in the ActionLogProbs as a flat List of torch Tensors. This
+        is private and serves as a utility for self.flatten()
+        """
         tensor_list: List[torch.Tensor] = []
         if self.continuous_tensor is not None:
             tensor_list.append(self.continuous_tensor)
@@ -110,7 +163,11 @@ class ActionLogProbs(NamedTuple):
         return tensor_list
 
     def flatten(self) -> torch.Tensor:
-        return torch.cat(self.to_tensor_list(), dim=1)
+        """
+        A utility method that returns all log probs in ActionLogProbs as a flattened tensor.
+        This is useful for algorithms like PPO which can treat all log probs in the same way.
+        """
+        return torch.cat(self._to_tensor_list(), dim=1)
 
     @staticmethod
     def create(
@@ -118,6 +175,10 @@ class ActionLogProbs(NamedTuple):
         action_spec: ActionSpec,
         all_log_prob_list: List[torch.Tensor] = None,
     ) -> "ActionLogProbs":
+        """
+        A static method that converts a list of torch Tensors into an ActionLogProbs using the ActionSpec.
+        This will change (and may be removed) in the ActionModel.
+        """
         continuous: torch.Tensor = None
         discrete: List[torch.Tensor] = None  # type: ignore
         _offset = 0
@@ -130,8 +191,13 @@ class ActionLogProbs(NamedTuple):
 
     @staticmethod
     def from_dict(buff: Dict[str, np.ndarray]) -> "ActionLogProbs":
+        """
+        A static method that accesses continuous and discrete log probs fields in an AgentBuffer
+        and constructs the corresponding ActionLogProbs from the retrieved np arrays.
+        """
         continuous: torch.Tensor = None
         discrete: List[torch.Tensor] = None  # type: ignore
+
         if "continuous_log_probs" in buff:
             continuous = ModelUtils.list_to_tensor(buff["continuous_log_probs"])
         if "discrete_log_probs" in buff:
