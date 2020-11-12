7868abf0cba05bf88871b4a20e57a1e2bac6fc4e
Andrew Cohen <andrew.cohen@unity3d.com>
2020-11-12 17:07:57 -0500

    action model and network tests

--- ml-agents/mlagents/trainers/tests/torch/test_networks.py
+++ ml-agents/mlagents/trainers/tests/torch/test_networks.py
@@ -4,16 +4,10 @@ from mlagents.torch_utils import torch
 from mlagents.trainers.torch.networks import (
     NetworkBody,
     ValueNetwork,
-    SimpleActor,
     SharedActorCritic,
     SeparateActorCritic,
 )
 from mlagents.trainers.settings import NetworkSettings
-from mlagents.trainers.torch.distributions import (
-    GaussianDistInstance,
-    CategoricalDistInstance,
-)
-
 from mlagents_envs.base_env import ActionSpec
 
 
@@ -132,9 +126,11 @@ def test_actor_critic(ac_type, lstm):
         memory=NetworkSettings.MemorySettings() if lstm else None
     )
     obs_shapes = [(obs_size,)]
-    act_size = [2]
+    act_size = 2
+    mask = torch.ones([1, act_size * 2])
     stream_names = [f"stream_name{n}" for n in range(4)]
-    action_spec = ActionSpec.create_continuous(act_size[0])
+    # action_spec = ActionSpec.create_continuous(act_size[0])
+    action_spec = ActionSpec(act_size, tuple(act_size for _ in range(act_size)))
     actor = ac_type(obs_shapes, network_settings, action_spec, stream_names)
     if lstm:
         sample_obs = torch.ones((1, network_settings.memory.sequence_length, obs_size))
@@ -155,14 +151,24 @@ def test_actor_critic(ac_type, lstm):
         else:
             assert value_out[stream].shape == (1,)
 
-    # Test get_dist_and_value
-    dists, value_out, mem_out = actor.get_dist_and_value(
-        [sample_obs], [], memories=memories
+    # Test get action stats and_value
+    action, log_probs, entropies, value_out, mem_out = actor.get_action_stats_and_value(
+        [sample_obs], [], memories=memories, masks=mask
     )
+    if lstm:
+        assert action.continuous_tensor.shape == (64, 2)
+    else:
+        assert action.continuous_tensor.shape == (1, 2)
+
+    assert len(action.discrete_list) == 2
+    for _disc in action.discrete_list:
+        if lstm:
+            assert _disc.shape == (64, 1)
+        else:
+            assert _disc.shape == (1, 1)
+
     if mem_out is not None:
         assert mem_out.shape == memories.shape
-    for dist in dists:
-        assert isinstance(dist, GaussianDistInstance)
     for stream in stream_names:
         if lstm:
             assert value_out[stream].shape == (network_settings.memory.sequence_length,)
