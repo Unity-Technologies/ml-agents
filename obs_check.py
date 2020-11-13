20d7285a5e4039f23c1f5e93eb5ade753db6771c
Ervin Teng <ervin@unity3d.com>
2020-11-12 18:53:02 -0800

    Revert "Invert divide by 3 in log prob"
    This reverts commit a708af66e740f19df5082b4b4e152a566c703385.

--- obs_check.py
+++
@@ -1,8 +0,0 @@
-from mlagents_envs.environment import UnityEnvironment
-
-
-# Getting observation types
-env = UnityEnvironment()
-env.reset()
-bspec = list(env.behavior_specs.values())[0]
-print(bspec.observation_shapes)
