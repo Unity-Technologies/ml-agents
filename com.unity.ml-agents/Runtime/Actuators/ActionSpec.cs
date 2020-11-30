e9b9af637ab19b82e0bed5074f64619af7039736
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2020-11-30 11:07:02 -0800

    make actionSpec not read-only

--- com.unity.ml-agents/Runtime/Actuators/ActionSpec.cs
+++ com.unity.ml-agents/Runtime/Actuators/ActionSpec.cs
@@ -1,15 +1,25 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Unity.MLAgents.Policies;
+using UnityEngine;
 
 namespace Unity.MLAgents.Actuators
 {
     /// <summary>
     /// Defines the structure of an Action Space to be used by the Actuator system.
     /// </summary>
-    public readonly struct ActionSpec
+    [Serializable]
+    public struct ActionSpec
     {
+        [SerializeField]
+        int[] m_BranchSizes;
+        [SerializeField]
+        int m_NumContinuousActions;
+        [SerializeField]
+        int m_NumDiscreteActions;
+        [SerializeField]
+        int m_SumOfDiscreteBranchSizes;
+
         /// <summary>
         /// An array of branch sizes for our action space.
         ///
@@ -20,23 +30,23 @@ namespace Unity.MLAgents.Actuators
         ///
         /// For an IActuator with a Continuous it will be null.
         /// </summary>
-        public readonly int[] BranchSizes;
+        public int[] BranchSizes { get { return m_BranchSizes; } set { m_BranchSizes = value; } }
 
         /// <summary>
         /// The number of actions for a Continuous <see cref="SpaceType"/>.
         /// </summary>
-        public int NumContinuousActions { get; }
+        public int NumContinuousActions { get { return m_NumContinuousActions; } set { m_NumContinuousActions = value; } }
 
         /// <summary>
         /// The number of branches for a Discrete <see cref="SpaceType"/>.
         /// </summary>
-        public int NumDiscreteActions { get; }
+        public int NumDiscreteActions { get { return m_NumDiscreteActions; } set { m_NumDiscreteActions = value; } }
 
         /// <summary>
         /// Get the total number of Discrete Actions that can be taken by calculating the Sum
         /// of all of the Discrete Action branch sizes.
         /// </summary>
-        public int SumOfDiscreteBranchSizes { get; }
+        public int SumOfDiscreteBranchSizes { get { return m_SumOfDiscreteBranchSizes; } set { m_SumOfDiscreteBranchSizes = value; } }
 
         /// <summary>
         /// Creates a Continuous <see cref="ActionSpec"/> with the number of actions available.
@@ -65,10 +75,10 @@ namespace Unity.MLAgents.Actuators
 
         internal ActionSpec(int numContinuousActions, int numDiscreteActions, int[] branchSizes = null)
         {
-            NumContinuousActions = numContinuousActions;
-            NumDiscreteActions = numDiscreteActions;
-            BranchSizes = branchSizes;
-            SumOfDiscreteBranchSizes = branchSizes?.Sum() ?? 0;
+            m_NumContinuousActions = numContinuousActions;
+            m_NumDiscreteActions = numDiscreteActions;
+            m_BranchSizes = branchSizes;
+            m_SumOfDiscreteBranchSizes = branchSizes?.Sum() ?? 0;
         }
 
         /// <summary>
