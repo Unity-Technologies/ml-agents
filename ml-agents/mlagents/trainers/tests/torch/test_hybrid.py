8601f828cb79dc94dc4835932e989387735eac1c
Andrew Cohen <andrew.cohen@unity3d.com>
2020-12-02 10:21:09 -0500

    expand hybrid tests

--- ml-agents/mlagents/trainers/tests/torch/test_hybrid.py
+++ ml-agents/mlagents/trainers/tests/torch/test_hybrid.py
@@ -20,9 +20,18 @@ SAC_TORCH_CONFIG = attr.evolve(sac_dummy_config(), framework=FrameworkType.PYTOR
 
 @pytest.mark.parametrize("action_size", [(1, 1), (2, 2), (1, 2), (2, 1)])
 def test_hybrid_ppo(action_size):
-    env = SimpleEnvironment([BRAIN_NAME], action_sizes=action_size)
-    config = attr.evolve(PPO_TORCH_CONFIG)
-    check_environment_trains(env, {BRAIN_NAME: config}, success_threshold=1.0)
+    env = SimpleEnvironment([BRAIN_NAME], action_sizes=action_size, step_size=0.8)
+    new_network_settings = attr.evolve(PPO_TORCH_CONFIG.network_settings)
+    new_hyperparams = attr.evolve(
+        PPO_TORCH_CONFIG.hyperparameters, batch_size=64, buffer_size=1024
+    )
+    config = attr.evolve(
+        PPO_TORCH_CONFIG,
+        hyperparameters=new_hyperparams,
+        network_settings=new_network_settings,
+        max_steps=10000,
+    )
+    check_environment_trains(env, {BRAIN_NAME: config}, success_threshold=0.9)
 
 
 @pytest.mark.parametrize("num_visual", [1, 2])
@@ -47,26 +56,29 @@ def test_hybrid_recurrent_ppo():
         PPO_TORCH_CONFIG.hyperparameters,
         learning_rate=1.0e-3,
         batch_size=64,
-        buffer_size=128,
+        buffer_size=512,
     )
     config = attr.evolve(
         PPO_TORCH_CONFIG,
         hyperparameters=new_hyperparams,
         network_settings=new_network_settings,
-        max_steps=2000,
+        max_steps=3000,
     )
     check_environment_trains(env, {BRAIN_NAME: config}, success_threshold=0.9)
 
 
 @pytest.mark.parametrize("action_size", [(1, 1), (2, 2), (1, 2), (2, 1)])
 def test_hybrid_sac(action_size):
-    env = SimpleEnvironment([BRAIN_NAME], action_sizes=action_size)
+    env = SimpleEnvironment([BRAIN_NAME], action_sizes=action_size, step_size=0.8)
 
     new_hyperparams = attr.evolve(
-        SAC_TORCH_CONFIG.hyperparameters, buffer_size=50000, batch_size=128
+        SAC_TORCH_CONFIG.hyperparameters,
+        buffer_size=50000,
+        batch_size=256,
+        buffer_init_steps=2000,
     )
     config = attr.evolve(
-        SAC_TORCH_CONFIG, hyperparameters=new_hyperparams, max_steps=3000
+        SAC_TORCH_CONFIG, hyperparameters=new_hyperparams, max_steps=5000
     )
     check_environment_trains(env, {BRAIN_NAME: config}, success_threshold=0.9)
 
