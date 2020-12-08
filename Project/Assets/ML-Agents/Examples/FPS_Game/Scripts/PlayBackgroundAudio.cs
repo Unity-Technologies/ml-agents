04fd7a9e142a681465c02d492931737098b7404d
HH <brandonh@unity3d.com>
2020-12-08 09:40:16 -0800

    added music

---
+++ Project/Assets/ML-Agents/Examples/FPS_Game/Scripts/PlayBackgroundAudio.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+
+//songs used
+// https://www.youtube.com/watch?v=gXm1W2eJ0e0
+[RequireComponent(typeof(AudioSource))]
+public class PlayBackgroundAudio : MonoBehaviour
+{
+
+    public AudioClip BackgroundSong;
+
+    private AudioSource m_AudioSource;
+    void Awake()
+    {
+        if (m_AudioSource == null)
+        {
+            m_AudioSource = GetComponent<AudioSource>();
+            //            m_AudioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        if (BackgroundSong)
+        {
+            m_AudioSource.clip = BackgroundSong;
+            m_AudioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
