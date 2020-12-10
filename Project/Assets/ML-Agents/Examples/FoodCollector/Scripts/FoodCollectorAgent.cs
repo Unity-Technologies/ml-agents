47bc8e894e953a78ada57abb2a883099d56e6ccf
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2020-12-09 17:29:10 -0800

    use continuous action for moving and discrete for shooting

--- Project/Assets/ML-Agents/Examples/FoodCollector/Scripts/FoodCollectorAgent.cs
+++ Project/Assets/ML-Agents/Examples/FoodCollector/Scripts/FoodCollectorAgent.cs
@@ -70,7 +70,7 @@ public class FoodCollectorAgent : Agent
         return new Color32(r, g, b, 255);
     }
 
-    public void MoveAgent(ActionSegment<int> act)
+    public void MoveAgent(ActionSegment<float> act)
     {
         m_Shoot = false;
 
@@ -95,41 +95,31 @@ public class FoodCollectorAgent : Agent
 
         if (!m_Frozen)
         {
+            var forward = Mathf.Clamp(act[0], -1f, 1f);
+            var right = Mathf.Clamp(act[1], -1f, 1f);
+            var rotate = Mathf.Clamp(act[2], -1f, 1f);
+
+            dirToGo = transform.forward * forward;
+            dirToGo += transform.right * right;
+            rotateDir = -transform.up * rotate;
+
+            m_AgentRb.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
+            transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
+        }
+
+        if (m_AgentRb.velocity.sqrMagnitude > 25f) // slow it down
+        {
+            m_AgentRb.velocity *= 0.95f;
+        }
+    }
+
+    public void Shoot(ActionSegment<int> act)
+    {
+        m_Shoot = false;
+        if (!m_Frozen)
+        {
             var shootCommand = false;
-            var forwardAxis = (int)act[0];
-            var rightAxis = (int)act[1];
-            var rotateAxis = (int)act[2];
-            var shootAxis = (int)act[3];
-
-            switch (forwardAxis)
-            {
-                case 1:
-                    dirToGo = transform.forward;
-                    break;
-                case 2:
-                    dirToGo = -transform.forward;
-                    break;
-            }
-
-            switch (rightAxis)
-            {
-                case 1:
-                    dirToGo = transform.right;
-                    break;
-                case 2:
-                    dirToGo = -transform.right;
-                    break;
-            }
-
-            switch (rotateAxis)
-            {
-                case 1:
-                    rotateDir = -transform.up;
-                    break;
-                case 2:
-                    rotateDir = transform.up;
-                    break;
-            }
+            var shootAxis = (int)act[0];
             switch (shootAxis)
             {
                 case 1:
@@ -139,16 +129,7 @@ public class FoodCollectorAgent : Agent
             if (shootCommand)
             {
                 m_Shoot = true;
-                dirToGo *= 0.5f;
-                m_AgentRb.velocity *= 0.75f;
             }
-            m_AgentRb.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
-            transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
-        }
-
-        if (m_AgentRb.velocity.sqrMagnitude > 25f) // slow it down
-        {
-            m_AgentRb.velocity *= 0.95f;
         }
 
         if (m_Shoot)
@@ -216,32 +197,8 @@ public class FoodCollectorAgent : Agent
     public override void OnActionReceived(ActionBuffers actionBuffers)
 
     {
-        MoveAgent(actionBuffers.DiscreteActions);
-    }
-
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var discreteActionsOut = actionsOut.DiscreteActions;
-        discreteActionsOut[0] = 0;
-        discreteActionsOut[1] = 0;
-        discreteActionsOut[2] = 0;
-        if (Input.GetKey(KeyCode.D))
-        {
-            discreteActionsOut[2] = 2;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            discreteActionsOut[0] = 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            discreteActionsOut[2] = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            discreteActionsOut[0] = 2;
-        }
-        discreteActionsOut[3] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+        MoveAgent(actionBuffers.ContinuousActions);
+        Shoot(actionBuffers.DiscreteActions);
     }
 
     public override void OnEpisodeBegin()
