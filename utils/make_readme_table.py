ffac78e533a96e1a159bd36c51fb8f6fa3d5a5d2
Andrew Cohen <andrew.cohen@unity3d.com>
2020-12-15 11:36:30 -0500

    update readme table and README

--- utils/make_readme_table.py
+++ utils/make_readme_table.py
@@ -76,6 +76,7 @@ versions = [
     ReleaseInfo("release_8", "1.5.0", "0.21.0", "October 14, 2020"),
     ReleaseInfo("release_9", "1.5.0", "0.21.1", "November 4, 2020"),
     ReleaseInfo("release_10", "1.6.0", "0.22.0", "November 18, 2020"),
+    ReleaseInfo("release_11", "1.7.0", "0.23.0", "December 21, 2020"),
 ]
 
 MAX_DAYS = 150  # do not print releases older than this many days
