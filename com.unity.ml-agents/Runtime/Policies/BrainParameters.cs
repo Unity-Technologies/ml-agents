cfaa01de7d38e705146da3bac1439ccf8856df2e
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2020-12-15 16:18:43 -0800

    set flag to true

--- com.unity.ml-agents/Runtime/Policies/BrainParameters.cs
+++ com.unity.ml-agents/Runtime/Policies/BrainParameters.cs
@@ -147,9 +147,8 @@ namespace Unity.MLAgents.Policies
                     m_ActionSpec.NumContinuousActions = 0;
                     m_ActionSpec.BranchSizes = (int[])VectorActionSize.Clone();
                 }
-
-                hasUpgradedBrainParametersWithActionSpec = true;
             }
+            hasUpgradedBrainParametersWithActionSpec = true;
         }
 
         /// <summary>
