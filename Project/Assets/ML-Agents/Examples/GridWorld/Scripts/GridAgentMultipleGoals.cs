f76c0e5b9f4b99a24ecca0762f135f72a17816c4
brccabral <cabral.brc@gmail.com>
2020-12-15 20:44:19 -0800

    prepare scene

---
+++ Project/Assets/ML-Agents/Examples/GridWorld/Scripts/GridAgentMultipleGoals.cs
@@ -0,0 +1,175 @@
+using System;
+using UnityEngine;
+using System.Linq;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine.Serialization;
+
+public class GridAgentMultipleGoals : Agent
+{
+    [FormerlySerializedAs("m_Area")]
+    [Header("Specific to GridWorld")]
+    public GridAreaMultipleGoals area;
+    public float timeBetweenDecisionsAtInference;
+    float m_TimeSinceDecision;
+
+    [Tooltip("Because we want an observation right before making a decision, we can force " +
+        "a camera to render before making a decision. Place the agentCam here if using " +
+        "RenderTexture as observations.")]
+    public Camera renderCamera;
+
+    [Tooltip("Selecting will turn on action masking. Note that a model trained with action " +
+        "masking turned on may not behave optimally when action masking is turned off.")]
+    public bool maskActions = true;
+
+    const int k_NoAction = 0;  // do nothing!
+    const int k_Up = 1;
+    const int k_Down = 2;
+    const int k_Left = 3;
+    const int k_Right = 4;
+
+    EnvironmentParameters m_ResetParams;
+
+    public override void Initialize()
+    {
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+    }
+
+    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+    {
+        // Mask the necessary actions if selected by the user.
+        if (maskActions)
+        {
+            // Prevents the agent from picking an action that would make it collide with a wall
+            var positionX = (int)transform.position.x;
+            var positionZ = (int)transform.position.z;
+            var maxPosition = (int)m_ResetParams.GetWithDefault("gridSize", 5f) - 1;
+
+            if (positionX == 0)
+            {
+                actionMask.WriteMask(0, new[] { k_Left });
+            }
+
+            if (positionX == maxPosition)
+            {
+                actionMask.WriteMask(0, new[] { k_Right });
+            }
+
+            if (positionZ == 0)
+            {
+                actionMask.WriteMask(0, new[] { k_Down });
+            }
+
+            if (positionZ == maxPosition)
+            {
+                actionMask.WriteMask(0, new[] { k_Up });
+            }
+        }
+    }
+
+    // to be implemented by the developer
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+
+    {
+        AddReward(-0.01f);
+        var action = actionBuffers.DiscreteActions[0];
+
+        var targetPos = transform.position;
+        switch (action)
+        {
+            case k_NoAction:
+                // do nothing
+                break;
+            case k_Right:
+                targetPos = transform.position + new Vector3(1f, 0, 0f);
+                break;
+            case k_Left:
+                targetPos = transform.position + new Vector3(-1f, 0, 0f);
+                break;
+            case k_Up:
+                targetPos = transform.position + new Vector3(0f, 0, 1f);
+                break;
+            case k_Down:
+                targetPos = transform.position + new Vector3(0f, 0, -1f);
+                break;
+            default:
+                throw new ArgumentException("Invalid action value");
+        }
+
+        var hit = Physics.OverlapBox(
+            targetPos, new Vector3(0.3f, 0.3f, 0.3f));
+        if (hit.Where(col => col.gameObject.CompareTag("wall")).ToArray().Length == 0)
+        {
+            transform.position = targetPos;
+
+            if (hit.Where(col => col.gameObject.CompareTag("goal")).ToArray().Length == 1)
+            {
+                SetReward(1f);
+                EndEpisode();
+            }
+            else if (hit.Where(col => col.gameObject.CompareTag("pit")).ToArray().Length == 1)
+            {
+                SetReward(-1f);
+                EndEpisode();
+            }
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = k_NoAction;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = k_Right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = k_Up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = k_Left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = k_Down;
+        }
+    }
+
+    // to be implemented by the developer
+    public override void OnEpisodeBegin()
+    {
+        area.AreaReset();
+    }
+
+    public void FixedUpdate()
+    {
+        WaitTimeInference();
+    }
+
+    void WaitTimeInference()
+    {
+        if (renderCamera != null)
+        {
+            renderCamera.Render();
+        }
+
+        if (Academy.Instance.IsCommunicatorOn)
+        {
+            RequestDecision();
+        }
+        else
+        {
+            if (m_TimeSinceDecision >= timeBetweenDecisionsAtInference)
+            {
+                m_TimeSinceDecision = 0f;
+                RequestDecision();
+            }
+            else
+            {
+                m_TimeSinceDecision += Time.fixedDeltaTime;
+            }
+        }
+    }
+}
---
+++ Project/Assets/ML-Agents/Examples/GridWorld/Scripts/GridAreaMultipleGoals.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.MLAgents;
+using UnityEngine;
+
+
+public class GridAreaMultipleGoals : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> actorObjs;
+    [HideInInspector]
+    public int[] players;
+
+    public GameObject trueAgent;
+
+    Camera m_AgentCam;
+
+    public List<GoalConfig> goalConfigList;
+    public GameObject pitPref;
+    GameObject[] m_Objects;
+    public int numberOfObstacles = 1;
+
+    GameObject m_Plane;
+    GameObject m_Sn;
+    GameObject m_Ss;
+    GameObject m_Se;
+    GameObject m_Sw;
+
+    Vector3 m_InitialPosition;
+
+    EnvironmentParameters m_ResetParams;
+
+    public void Start()
+    {
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+
+        m_Objects = new GameObject[goalConfigList.Count + numberOfObstacles];
+        var playersList = new List<int>();
+
+        for (int i = 0; i < goalConfigList.Count; i++)
+        {
+            m_Objects[i] = goalConfigList[i].prefab;
+            playersList.Add(i);
+        }
+        for (int o = 0; o < numberOfObstacles; o++)
+        {
+            m_Objects[goalConfigList.Count + o] = pitPref;
+            playersList.Add(goalConfigList.Count + o);
+        }
+        players = playersList.ToArray();
+
+
+        m_AgentCam = transform.Find("agentCam").GetComponent<Camera>();
+
+        actorObjs = new List<GameObject>();
+
+        var sceneTransform = transform.Find("scene");
+
+        m_Plane = sceneTransform.Find("Plane").gameObject;
+        m_Sn = sceneTransform.Find("sN").gameObject;
+        m_Ss = sceneTransform.Find("sS").gameObject;
+        m_Sw = sceneTransform.Find("sW").gameObject;
+        m_Se = sceneTransform.Find("sE").gameObject;
+        m_InitialPosition = transform.position;
+    }
+
+    void SetEnvironment()
+    {
+        transform.position = m_InitialPosition * (m_ResetParams.GetWithDefault("gridSize", 5f) + 1);
+
+        var gridSize = (int)m_ResetParams.GetWithDefault("gridSize", 5f);
+        m_Plane.transform.localScale = new Vector3(gridSize / 10.0f, 1f, gridSize / 10.0f);
+        m_Plane.transform.localPosition = new Vector3((gridSize - 1) / 2f, -0.5f, (gridSize - 1) / 2f);
+        m_Sn.transform.localScale = new Vector3(1, 1, gridSize + 2);
+        m_Ss.transform.localScale = new Vector3(1, 1, gridSize + 2);
+        m_Sn.transform.localPosition = new Vector3((gridSize - 1) / 2f, 0.0f, gridSize);
+        m_Ss.transform.localPosition = new Vector3((gridSize - 1) / 2f, 0.0f, -1);
+        m_Se.transform.localScale = new Vector3(1, 1, gridSize + 2);
+        m_Sw.transform.localScale = new Vector3(1, 1, gridSize + 2);
+        m_Se.transform.localPosition = new Vector3(gridSize, 0.0f, (gridSize - 1) / 2f);
+        m_Sw.transform.localPosition = new Vector3(-1, 0.0f, (gridSize - 1) / 2f);
+
+        m_AgentCam.orthographicSize = (gridSize) / 2f;
+        m_AgentCam.transform.localPosition = new Vector3((gridSize - 1) / 2f, gridSize + 1f, (gridSize - 1) / 2f);
+    }
+
+    public void AreaReset()
+    {
+        var gridSize = (int)m_ResetParams.GetWithDefault("gridSize", 5f);
+        foreach (var actor in actorObjs)
+        {
+            DestroyImmediate(actor);
+        }
+        SetEnvironment();
+
+        actorObjs.Clear();
+
+        var numbers = new HashSet<int>();
+        while (numbers.Count < players.Length + 1)
+        {
+            numbers.Add(Random.Range(0, gridSize * gridSize));
+        }
+        var numbersA = numbers.ToArray();
+
+        for (var i = 0; i < players.Length; i++)
+        {
+            var x = (numbersA[i]) / gridSize;
+            var y = (numbersA[i]) % gridSize;
+            var actorObj = Instantiate(m_Objects[players[i]], transform);
+            actorObj.transform.localPosition = new Vector3(x, -0.25f, y);
+            actorObjs.Add(actorObj);
+        }
+
+        var xA = (numbersA[players.Length]) / gridSize;
+        var yA = (numbersA[players.Length]) % gridSize;
+        trueAgent.transform.localPosition = new Vector3(xA, -0.25f, yA);
+    }
+}
