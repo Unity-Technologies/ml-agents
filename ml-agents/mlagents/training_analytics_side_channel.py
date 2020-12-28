a5bb1590d1b226a03cf0e8da778d90df1faa1d9c
Chris Elion <chris.elion@unity3d.com>
2020-12-28 15:17:58 -0800

    gather info in sidechannel

--- ml-agents/mlagents/training_analytics_side_channel.py
+++ ml-agents/mlagents/training_analytics_side_channel.py
@@ -1,4 +1,12 @@
+# flake8: noqa
+
+# TODO re-enable flake8
+import sys
 import uuid
+import mlagents_envs
+import mlagents.trainers
+from mlagents import torch_utils
+from mlagents.trainers.settings import RewardSignalType
 from mlagents_envs.exception import UnityCommunicationException
 from mlagents_envs.side_channel import SideChannel, IncomingMessage, OutgoingMessage
 
@@ -22,11 +30,36 @@ class TrainingAnalyticsSideChannel(SideChannel):
         )
 
     def environment_initialized(self) -> None:
+        # Tuple of (major, minor, patch)
+        vi = sys.version_info
+        python_version = f"{vi[0]}.{vi[1]}.{vi[2]}"
+        mlagents_version = mlagents.trainers.__version__
+        mlagents_envs_version = mlagents_envs.__version__
+        torch_version = torch_utils.torch.__version__
+        torch_device_type = torch_utils.default_device().type
+
         env_init_msg = OutgoingMessage()
         env_init_msg.write_string("environment_initialized!")
         super().queue_message_to_send(env_init_msg)
 
     def training_started(self, behavior_name: str, config: TrainerSettings) -> None:
+        trainer_type = config.trainer_type
+
+        extrinsic_reward_enabled = RewardSignalType.EXTRINSIC in config.reward_signals
+        gail_reward_enabled = RewardSignalType.GAIL in config.reward_signals
+        curiosity_reward_enabled = RewardSignalType.CURIOSITY in config.reward_signals
+        rnd_reward_enabled = RewardSignalType.RND in config.reward_signals
+
+        behavioral_cloning_enabled = config.behavioral_cloning is not None
+
+        recurrent_enabled = config.network_settings.memory is not None
+        visual_encoder = config.network_settings.vis_encode_type.value
+        network_layers = config.network_settings.num_layers
+        network_hidden_units = config.network_settings.hidden_units
+
+        trainer_threaded = config.threaded
+        self_play = config.self_play is not None
+
         training_start_msg = OutgoingMessage()
         training_start_msg.write_string(
             f"training_started for {behavior_name} with config {str(config)}!"
