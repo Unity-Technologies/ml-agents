5565aed0c6fa81e01e3b6aa05cae8e92868e308f
Andrew Cohen <andrew.cohen@unity3d.com>
2021-01-15 21:46:50 -0500

    sequencer env

---
+++ Project/Assets/ML-Agents/Examples/Arena-Sequence/Scripts/SequenceTile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceTile : MonoBehaviour
+{
+    public int NumberValue;
+
+    // [HideInInspector]
+    public MeshRenderer rend;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rend = GetComponentInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
