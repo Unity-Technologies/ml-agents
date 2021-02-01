cf9b4aa764f9acfd4b995e98d8f0240cc913d196
Chris Elion <chris.elion@unity3d.com>
2021-02-01 14:00:47 -0800

    exclude from GPU tests

--- ml-agents-plugin-examples/mlagents_plugin_examples/tests/test_stats_writer_plugin.py
+++ ml-agents-plugin-examples/mlagents_plugin_examples/tests/test_stats_writer_plugin.py
@@ -1,9 +1,12 @@
+import pytest
+
 from mlagents.plugins.stats_writer import register_stats_writer_plugins
 from mlagents.trainers.settings import RunOptions
 
 from mlagents_plugin_examples.example_stats_writer import ExampleStatsWriter
 
 
+@pytest.mark.check_environment_trains
 def test_register_stats_writers():
     # Make sure that the ExampleStatsWriter gets returned from the list of all StatsWriters
     stats_writers = register_stats_writer_plugins(RunOptions())
