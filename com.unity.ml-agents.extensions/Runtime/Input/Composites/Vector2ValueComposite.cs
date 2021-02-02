6d1608f925644c85d9e09116afcfbc9d46622270
Christopher Goy <christopherg@unity3d.com>
2021-02-02 11:13:03 -0800

    Update .gitignore, remove files i wish not to track.

--- com.unity.ml-agents.extensions/Runtime/Input/Composites/Vector2ValueComposite.cs
+++ com.unity.ml-agents.extensions/Runtime/Input/Composites/Vector2ValueComposite.cs
@@ -3,9 +3,14 @@ using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.Utilities;
 using UnityEngine.Scripting;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 namespace Unity.MLAgents.Extensions.Runtime.Input.Composites
 {
+#if UNITY_EDITOR
+    [InitializeOnLoad]
+#endif
     [Preserve]
     [DisplayStringFormat("{Vector2}")]
     public class Vector2ValueComposite : InputBindingComposite<Vector2>
