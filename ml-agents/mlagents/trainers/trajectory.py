71cbd04d6063553693ed61ac4b5e45ecd55f6026
Andrew Cohen <andrew.cohen@unity3d.com>
2021-02-03 11:46:32 -0500

    move from average to sum of rewards

--- ml-agents/mlagents/trainers/trajectory.py
+++ ml-agents/mlagents/trainers/trajectory.py
@@ -207,8 +207,10 @@ class Trajectory(NamedTuple):
             agent_buffer_trajectory["team_rewards"].append(teammate_rewards)
             team_reward = teammate_rewards + [exp.reward]
             agent_buffer_trajectory["average_team_reward"].append(
-                sum(team_reward) / len(team_reward)
+                sum(team_reward)
             )
+            #    sum(team_reward) / len(team_reward)
+            #)
 
             # Next actions
             teammate_cont_next_actions = []
