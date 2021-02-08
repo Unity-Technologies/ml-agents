c5feda6fda8b579b74c4b2b8eec5b5a26713f458
Ervin Teng <ervin@unity3d.com>
2021-02-08 13:55:52 -0500

    Better laser graphic

--- Project/Assets/ML-Agents/Examples/CubeWars/Scripts/Laser.cs
+++ Project/Assets/ML-Agents/Examples/CubeWars/Scripts/Laser.cs
@@ -6,30 +6,31 @@ public class Laser : MonoBehaviour
 {
     public bool isFired;
     public float maxLength = 25f;
-    public float width = 0.5f;
+    public float width = 1f;
     public bool animate = false;
     LineRenderer laserRenderer;
     // Start is called before the first frame update
     void Start()
     {
         laserRenderer = GetComponentInChildren<LineRenderer>();
-        laserRenderer.SetWidth(width, width);
         laserRenderer.material.SetTextureScale("_MainTex", new Vector2(0.07f, 1.0f));
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        laserRenderer.SetWidth(width, width);
         if (isFired)
         {
             if (animate)
             {
-                laserRenderer.material.SetTextureOffset("_MainTex", new Vector2(-3 * Time.time, 0.0f));
+                laserRenderer.material.SetTextureOffset("_MainTex", new Vector2(-6 * Time.time, 0.0f));
             }
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, maxLength))
+            if (Physics.SphereCast(transform.position, width, transform.forward, out hit, maxLength))
             {
-                laserRenderer.SetPosition(1, new Vector3(0f, 0f, hit.distance / transform.lossyScale.z));
+                float hitLength = Vector3.Distance(hit.point, transform.position);
+                laserRenderer.SetPosition(1, new Vector3(0f, 0f, hitLength / transform.lossyScale.z));
             }
             else
             {
