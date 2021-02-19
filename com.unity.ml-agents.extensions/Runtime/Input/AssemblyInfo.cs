52a6f80d3b8614372109b45bcccc376cbc0a7a03
Chris Goy <christopherg@unity3d.com>
2021-02-18 19:45:48 -0800

    InputActuatorComponent to allow the generation of an action space from an InputActionAsset (#4881) (#4974)

---
+++ com.unity.ml-agents.extensions/Runtime/Input/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.ML-Agents.Extensions.Input.Tests.Runtime")]
--- com.unity.ml-agents/Runtime/Actuators/ActionSpec.cs
+++ com.unity.ml-agents/Runtime/Actuators/ActionSpec.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using Unity.MLAgents.Policies;
 using UnityEngine;
 
 namespace Unity.MLAgents.Actuators
@@ -75,7 +75,7 @@ namespace Unity.MLAgents.Actuators
         public ActionSpec(int numContinuousActions = 0, int[] discreteBranchSizes = null)
         {
             m_NumContinuousActions = numContinuousActions;
-            BranchSizes = discreteBranchSizes;
+            BranchSizes = discreteBranchSizes ?? Array.Empty<int>();
         }
 
         /// <summary>
@@ -92,6 +92,47 @@ namespace Unity.MLAgents.Actuators
                     "ActionSpecs must be all continuous or all discrete."
                 );
             }
+        }
+
+        /// <summary>
+        /// Combines a list of actions specs and allocates a new array of branch sizes if needed.
+        /// </summary>
+        /// <param name="specs">The list of action specs to combine.</param>
+        /// <returns>An ActionSpec which represents the aggregate of the ActionSpecs passed in.</returns>
+        public static ActionSpec Combine(params ActionSpec[] specs)
+        {
+            var numContinuous = 0;
+            var numDiscrete = 0;
+            for (var i = 0; i < specs.Length; i++)
+            {
+                var spec = specs[i];
+                numContinuous += spec.NumContinuousActions;
+                numDiscrete += spec.NumDiscreteActions;
+            }
+
+            if (numDiscrete <= 0)
+            {
+                return MakeContinuous(numContinuous);
+            }
+
+            var branchSizes = new int[numDiscrete];
+            var offset = 0;
+            for (var i = 0; i < specs.Length; i++)
+            {
+                var spec = specs[i];
+                if (spec.BranchSizes.Length == 0)
+                {
+                    continue;
+                }
+                var branchSizesLength = spec.BranchSizes.Length;
+                Array.Copy(spec.BranchSizes,
+                    0,
+                    branchSizes,
+                    offset,
+                    branchSizesLength);
+                offset += branchSizesLength;
+            }
+            return new ActionSpec(numContinuous, branchSizes);
         }
     }
 }
