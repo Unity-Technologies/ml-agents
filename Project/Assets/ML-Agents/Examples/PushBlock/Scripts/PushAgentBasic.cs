2e5fb7f84e0c1b6675d57243b53822b70de9a2f7
vincentpierre <vincentpierre@unity3d.com>
2021-02-24 10:13:30 -0800

    readding pushblock and walljump

---
+++ Project/Assets/ML-Agents/Examples/PushBlock/Scripts/PushAgentBasic.cs
@@ -0,0 +1,255 @@
+//Put this script on your blue cube.
+
+using System.Collections;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class PushAgentBasic : Agent
+{
+    /// <summary>
+    /// The ground. The bounds are used to spawn the elements.
+    /// </summary>
+    public GameObject ground;
+
+    public GameObject area;
+
+    /// <summary>
+    /// The area bounds.
+    /// </summary>
+    [HideInInspector]
+    public Bounds areaBounds;
+
+    PushBlockSettings m_PushBlockSettings;
+
+    /// <summary>
+    /// The goal to push the block to.
+    /// </summary>
+    public GameObject goal;
+
+    /// <summary>
+    /// The block to be pushed to the goal.
+    /// </summary>
+    public GameObject block;
+
+    /// <summary>
+    /// Detects when the block touches the goal.
+    /// </summary>
+    [HideInInspector]
+    public GoalDetect goalDetect;
+
+    public bool useVectorObs;
+
+    Rigidbody m_BlockRb;  //cached on initialization
+    Rigidbody m_AgentRb;  //cached on initialization
+    Material m_GroundMaterial; //cached on Awake()
+
+    /// <summary>
+    /// We will be changing the ground material based on success/failue
+    /// </summary>
+    Renderer m_GroundRenderer;
+
+    EnvironmentParameters m_ResetParams;
+
+    void Awake()
+    {
+        m_PushBlockSettings = FindObjectOfType<PushBlockSettings>();
+    }
+
+    public override void Initialize()
+    {
+        goalDetect = block.GetComponent<GoalDetect>();
+        goalDetect.agent = this;
+
+        // Cache the agent rigidbody
+        m_AgentRb = GetComponent<Rigidbody>();
+        // Cache the block rigidbody
+        m_BlockRb = block.GetComponent<Rigidbody>();
+        // Get the ground's bounds
+        areaBounds = ground.GetComponent<Collider>().bounds;
+        // Get the ground renderer so we can change the material when a goal is scored
+        m_GroundRenderer = ground.GetComponent<Renderer>();
+        // Starting material
+        m_GroundMaterial = m_GroundRenderer.material;
+
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+
+        SetResetParameters();
+    }
+
+    /// <summary>
+    /// Use the ground's bounds to pick a random spawn position.
+    /// </summary>
+    public Vector3 GetRandomSpawnPos()
+    {
+        var foundNewSpawnLocation = false;
+        var randomSpawnPos = Vector3.zero;
+        while (foundNewSpawnLocation == false)
+        {
+            var randomPosX = Random.Range(-areaBounds.extents.x * m_PushBlockSettings.spawnAreaMarginMultiplier,
+                areaBounds.extents.x * m_PushBlockSettings.spawnAreaMarginMultiplier);
+
+            var randomPosZ = Random.Range(-areaBounds.extents.z * m_PushBlockSettings.spawnAreaMarginMultiplier,
+                areaBounds.extents.z * m_PushBlockSettings.spawnAreaMarginMultiplier);
+            randomSpawnPos = ground.transform.position + new Vector3(randomPosX, 1f, randomPosZ);
+            if (Physics.CheckBox(randomSpawnPos, new Vector3(2.5f, 0.01f, 2.5f)) == false)
+            {
+                foundNewSpawnLocation = true;
+            }
+        }
+        return randomSpawnPos;
+    }
+
+    /// <summary>
+    /// Called when the agent moves the block into the goal.
+    /// </summary>
+    public void ScoredAGoal()
+    {
+        // We use a reward of 5.
+        AddReward(5f);
+
+        // By marking an agent as done AgentReset() will be called automatically.
+        EndEpisode();
+
+        // Swap ground material for a bit to indicate we scored.
+        StartCoroutine(GoalScoredSwapGroundMaterial(m_PushBlockSettings.goalScoredMaterial, 0.5f));
+    }
+
+    /// <summary>
+    /// Swap ground material, wait time seconds, then swap back to the regular material.
+    /// </summary>
+    IEnumerator GoalScoredSwapGroundMaterial(Material mat, float time)
+    {
+        m_GroundRenderer.material = mat;
+        yield return new WaitForSeconds(time); // Wait for 2 sec
+        m_GroundRenderer.material = m_GroundMaterial;
+    }
+
+    /// <summary>
+    /// Moves the agent according to the selected action.
+    /// </summary>
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = transform.right * -0.75f;
+                break;
+            case 6:
+                dirToGo = transform.right * 0.75f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * 200f);
+        m_AgentRb.AddForce(dirToGo * m_PushBlockSettings.agentRunSpeed,
+            ForceMode.VelocityChange);
+    }
+
+    /// <summary>
+    /// Called every step of the engine. Here the agent takes an action.
+    /// </summary>
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+
+    {
+        // Move the agent using the action.
+        MoveAgent(actionBuffers.DiscreteActions);
+
+        // Penalty given each step to encourage agent to finish task quickly.
+        AddReward(-1f / MaxStep);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+    }
+
+    /// <summary>
+    /// Resets the block position and velocities.
+    /// </summary>
+    void ResetBlock()
+    {
+        // Get a random position for the block.
+        block.transform.position = GetRandomSpawnPos();
+
+        // Reset block velocity back to zero.
+        m_BlockRb.velocity = Vector3.zero;
+
+        // Reset block angularVelocity back to zero.
+        m_BlockRb.angularVelocity = Vector3.zero;
+    }
+
+    /// <summary>
+    /// In the editor, if "Reset On Done" is checked then AgentReset() will be
+    /// called automatically anytime we mark done = true in an agent script.
+    /// </summary>
+    public override void OnEpisodeBegin()
+    {
+        var rotation = Random.Range(0, 4);
+        var rotationAngle = rotation * 90f;
+        area.transform.Rotate(new Vector3(0f, rotationAngle, 0f));
+
+        ResetBlock();
+        transform.position = GetRandomSpawnPos();
+        m_AgentRb.velocity = Vector3.zero;
+        m_AgentRb.angularVelocity = Vector3.zero;
+
+        SetResetParameters();
+    }
+
+    public void SetGroundMaterialFriction()
+    {
+        var groundCollider = ground.GetComponent<Collider>();
+
+        groundCollider.material.dynamicFriction = m_ResetParams.GetWithDefault("dynamic_friction", 0);
+        groundCollider.material.staticFriction = m_ResetParams.GetWithDefault("static_friction", 0);
+    }
+
+    public void SetBlockProperties()
+    {
+        var scale = m_ResetParams.GetWithDefault("block_scale", 2);
+        //Set the scale of the block
+        m_BlockRb.transform.localScale = new Vector3(scale, 0.75f, scale);
+
+        // Set the drag of the block
+        m_BlockRb.drag = m_ResetParams.GetWithDefault("block_drag", 0.5f);
+    }
+
+    void SetResetParameters()
+    {
+        SetGroundMaterialFriction();
+        SetBlockProperties();
+    }
+}
---
+++ Project/Assets/ML-Agents/Examples/WallJump/Scripts/WallJumpAgent.cs
@@ -0,0 +1,366 @@
+//Put this script on your blue cube.
+
+using System.Collections;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.Barracuda;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgentsExamples;
+
+public class WallJumpAgent : Agent
+{
+    // Depending on this value, the wall will have different height
+    int m_Configuration;
+    // Brain to use when no wall is present
+    public NNModel noWallBrain;
+    // Brain to use when a jumpable wall is present
+    public NNModel smallWallBrain;
+    // Brain to use when a wall requiring a block to jump over is present
+    public NNModel bigWallBrain;
+
+    public GameObject ground;
+    public GameObject spawnArea;
+    Bounds m_SpawnAreaBounds;
+
+
+    public GameObject goal;
+    public GameObject shortBlock;
+    public GameObject wall;
+    Rigidbody m_ShortBlockRb;
+    Rigidbody m_AgentRb;
+    Material m_GroundMaterial;
+    Renderer m_GroundRenderer;
+    WallJumpSettings m_WallJumpSettings;
+
+    public float jumpingTime;
+    public float jumpTime;
+    // This is a downward force applied when falling to make jumps look
+    // less floaty
+    public float fallingForce;
+    // Use to check the coliding objects
+    public Collider[] hitGroundColliders = new Collider[3];
+    Vector3 m_JumpTargetPos;
+    Vector3 m_JumpStartingPos;
+
+    string m_NoWallBehaviorName = "SmallWallJump";
+    string m_SmallWallBehaviorName = "SmallWallJump";
+    string m_BigWallBehaviorName = "BigWallJump";
+
+    EnvironmentParameters m_ResetParams;
+
+    public override void Initialize()
+    {
+        m_WallJumpSettings = FindObjectOfType<WallJumpSettings>();
+        m_Configuration = Random.Range(0, 5);
+
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_ShortBlockRb = shortBlock.GetComponent<Rigidbody>();
+        m_SpawnAreaBounds = spawnArea.GetComponent<Collider>().bounds;
+        m_GroundRenderer = ground.GetComponent<Renderer>();
+        m_GroundMaterial = m_GroundRenderer.material;
+
+        spawnArea.SetActive(false);
+
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+
+        // Update model references if we're overriding
+        var modelOverrider = GetComponent<ModelOverrider>();
+        if (modelOverrider.HasOverrides)
+        {
+            noWallBrain = modelOverrider.GetModelForBehaviorName(m_NoWallBehaviorName);
+            m_NoWallBehaviorName = ModelOverrider.GetOverrideBehaviorName(m_NoWallBehaviorName);
+
+            smallWallBrain = modelOverrider.GetModelForBehaviorName(m_SmallWallBehaviorName);
+            m_SmallWallBehaviorName = ModelOverrider.GetOverrideBehaviorName(m_SmallWallBehaviorName);
+
+            bigWallBrain = modelOverrider.GetModelForBehaviorName(m_BigWallBehaviorName);
+            m_BigWallBehaviorName = ModelOverrider.GetOverrideBehaviorName(m_BigWallBehaviorName);
+        }
+    }
+
+    // Begin the jump sequence
+    public void Jump()
+    {
+        jumpingTime = 0.2f;
+        m_JumpStartingPos = m_AgentRb.position;
+    }
+
+    /// <summary>
+    /// Does the ground check.
+    /// </summary>
+    /// <returns><c>true</c>, if the agent is on the ground,
+    /// <c>false</c> otherwise.</returns>
+    /// <param name="smallCheck"></param>
+    public bool DoGroundCheck(bool smallCheck)
+    {
+        if (!smallCheck)
+        {
+            hitGroundColliders = new Collider[3];
+            var o = gameObject;
+            Physics.OverlapBoxNonAlloc(
+                o.transform.position + new Vector3(0, -0.05f, 0),
+                new Vector3(0.95f / 2f, 0.5f, 0.95f / 2f),
+                hitGroundColliders,
+                o.transform.rotation);
+            var grounded = false;
+            foreach (var col in hitGroundColliders)
+            {
+                if (col != null && col.transform != transform &&
+                    (col.CompareTag("walkableSurface") ||
+                     col.CompareTag("block") ||
+                     col.CompareTag("wall")))
+                {
+                    grounded = true; //then we're grounded
+                    break;
+                }
+            }
+            return grounded;
+        }
+        else
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position + new Vector3(0, -0.05f, 0), -Vector3.up, out hit,
+                1f);
+
+            if (hit.collider != null &&
+                (hit.collider.CompareTag("walkableSurface") ||
+                 hit.collider.CompareTag("block") ||
+                 hit.collider.CompareTag("wall"))
+                && hit.normal.y > 0.95f)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Moves  a rigidbody towards a position smoothly.
+    /// </summary>
+    /// <param name="targetPos">Target position.</param>
+    /// <param name="rb">The rigidbody to be moved.</param>
+    /// <param name="targetVel">The velocity to target during the
+    ///  motion.</param>
+    /// <param name="maxVel">The maximum velocity posible.</param>
+    void MoveTowards(
+        Vector3 targetPos, Rigidbody rb, float targetVel, float maxVel)
+    {
+        var moveToPos = targetPos - rb.worldCenterOfMass;
+        var velocityTarget = Time.fixedDeltaTime * targetVel * moveToPos;
+        if (float.IsNaN(velocityTarget.x) == false)
+        {
+            rb.velocity = Vector3.MoveTowards(
+                rb.velocity, velocityTarget, maxVel);
+        }
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        var agentPos = m_AgentRb.position - ground.transform.position;
+
+        sensor.AddObservation(agentPos / 20f);
+        sensor.AddObservation(DoGroundCheck(true) ? 1 : 0);
+    }
+
+    /// <summary>
+    /// Gets a random spawn position in the spawningArea.
+    /// </summary>
+    /// <returns>The random spawn position.</returns>
+    public Vector3 GetRandomSpawnPos()
+    {
+        var randomPosX = Random.Range(-m_SpawnAreaBounds.extents.x,
+            m_SpawnAreaBounds.extents.x);
+        var randomPosZ = Random.Range(-m_SpawnAreaBounds.extents.z,
+            m_SpawnAreaBounds.extents.z);
+
+        var randomSpawnPos = spawnArea.transform.position +
+            new Vector3(randomPosX, 0.45f, randomPosZ);
+        return randomSpawnPos;
+    }
+
+    /// <summary>
+    /// Changes the color of the ground for a moment.
+    /// </summary>
+    /// <returns>The Enumerator to be used in a Coroutine.</returns>
+    /// <param name="mat">The material to be swapped.</param>
+    /// <param name="time">The time the material will remain.</param>
+    IEnumerator GoalScoredSwapGroundMaterial(Material mat, float time)
+    {
+        m_GroundRenderer.material = mat;
+        yield return new WaitForSeconds(time); //wait for 2 sec
+        m_GroundRenderer.material = m_GroundMaterial;
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        AddReward(-0.0005f);
+        var smallGrounded = DoGroundCheck(true);
+        var largeGrounded = DoGroundCheck(false);
+
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+        var dirToGoForwardAction = act[0];
+        var rotateDirAction = act[1];
+        var dirToGoSideAction = act[2];
+        var jumpAction = act[3];
+
+        if (dirToGoForwardAction == 1)
+            dirToGo = (largeGrounded ? 1f : 0.5f) * 1f * transform.forward;
+        else if (dirToGoForwardAction == 2)
+            dirToGo = (largeGrounded ? 1f : 0.5f) * -1f * transform.forward;
+        if (rotateDirAction == 1)
+            rotateDir = transform.up * -1f;
+        else if (rotateDirAction == 2)
+            rotateDir = transform.up * 1f;
+        if (dirToGoSideAction == 1)
+            dirToGo = (largeGrounded ? 1f : 0.5f) * -0.6f * transform.right;
+        else if (dirToGoSideAction == 2)
+            dirToGo = (largeGrounded ? 1f : 0.5f) * 0.6f * transform.right;
+        if (jumpAction == 1)
+            if ((jumpingTime <= 0f) && smallGrounded)
+            {
+                Jump();
+            }
+
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * 300f);
+        m_AgentRb.AddForce(dirToGo * m_WallJumpSettings.agentRunSpeed,
+            ForceMode.VelocityChange);
+
+        if (jumpingTime > 0f)
+        {
+            m_JumpTargetPos =
+                new Vector3(m_AgentRb.position.x,
+                    m_JumpStartingPos.y + m_WallJumpSettings.agentJumpHeight,
+                    m_AgentRb.position.z) + dirToGo;
+            MoveTowards(m_JumpTargetPos, m_AgentRb, m_WallJumpSettings.agentJumpVelocity,
+                m_WallJumpSettings.agentJumpVelocityMaxChange);
+        }
+
+        if (!(jumpingTime > 0f) && !largeGrounded)
+        {
+            m_AgentRb.AddForce(
+                Vector3.down * fallingForce, ForceMode.Acceleration);
+        }
+        jumpingTime -= Time.fixedDeltaTime;
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+
+    {
+        MoveAgent(actionBuffers.DiscreteActions);
+        if ((!Physics.Raycast(m_AgentRb.position, Vector3.down, 20))
+            || (!Physics.Raycast(m_ShortBlockRb.position, Vector3.down, 20)))
+        {
+            SetReward(-1f);
+            EndEpisode();
+            ResetBlock(m_ShortBlockRb);
+            StartCoroutine(
+                GoalScoredSwapGroundMaterial(m_WallJumpSettings.failMaterial, .5f));
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut.Clear();
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[1] = 2;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[1] = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        discreteActionsOut[3] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+    }
+
+    // Detect when the agent hits the goal
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("goal") && DoGroundCheck(true))
+        {
+            SetReward(1f);
+            EndEpisode();
+            StartCoroutine(
+                GoalScoredSwapGroundMaterial(m_WallJumpSettings.goalScoredMaterial, 2));
+        }
+    }
+
+    //Reset the orange block position
+    void ResetBlock(Rigidbody blockRb)
+    {
+        blockRb.transform.position = GetRandomSpawnPos();
+        blockRb.velocity = Vector3.zero;
+        blockRb.angularVelocity = Vector3.zero;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        ResetBlock(m_ShortBlockRb);
+        transform.localPosition = new Vector3(
+            18 * (Random.value - 0.5f), 1, -12);
+        m_Configuration = Random.Range(0, 5);
+        m_AgentRb.velocity = default(Vector3);
+    }
+
+    void FixedUpdate()
+    {
+        if (m_Configuration != -1)
+        {
+            ConfigureAgent(m_Configuration);
+            m_Configuration = -1;
+        }
+    }
+
+    /// <summary>
+    /// Configures the agent. Given an integer config, the wall will have
+    /// different height and a different brain will be assigned to the agent.
+    /// </summary>
+    /// <param name="config">Config.
+    /// If 0 : No wall and noWallBrain.
+    /// If 1:  Small wall and smallWallBrain.
+    /// Other : Tall wall and BigWallBrain.
+    /// </param>
+    void ConfigureAgent(int config)
+    {
+        var localScale = wall.transform.localScale;
+        if (config == 0)
+        {
+            localScale = new Vector3(
+                localScale.x,
+                m_ResetParams.GetWithDefault("no_wall_height", 0),
+                localScale.z);
+            wall.transform.localScale = localScale;
+            SetModel(m_NoWallBehaviorName, noWallBrain);
+        }
+        else if (config == 1)
+        {
+            localScale = new Vector3(
+                localScale.x,
+                m_ResetParams.GetWithDefault("small_wall_height", 4),
+                localScale.z);
+            wall.transform.localScale = localScale;
+            SetModel(m_SmallWallBehaviorName, smallWallBrain);
+        }
+        else
+        {
+            var height = m_ResetParams.GetWithDefault("big_wall_height", 8);
+            localScale = new Vector3(
+                localScale.x,
+                height,
+                localScale.z);
+            wall.transform.localScale = localScale;
+            SetModel(m_BigWallBehaviorName, bigWallBrain);
+        }
+    }
+}
