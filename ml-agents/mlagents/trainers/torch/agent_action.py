59531f3746c58d62cf52f58a88e27a3e428e8946
Ervin Teng <ervin@unity3d.com>
2021-03-05 13:14:50 -0500

    Make agent action run

--- ml-agents/mlagents/trainers/torch/agent_action.py
+++ ml-agents/mlagents/trainers/torch/agent_action.py
@@ -21,17 +21,14 @@ class AgentAction(NamedTuple):
     discrete_list: Optional[List[torch.Tensor]]
 
     def __getitem__(self, index):
-        if isinstance(index, slice):
-            _cont = None
-            _disc_list = []
-            if self.continuous_tensor is not None:
-                _cont = self.continuous_tensor.__getitem__(index)
-            if self.discrete_list is not None and len(self.discrete_list) > 0:
-                for _disc in self.discrete_list:
-                    _disc_list.append(_disc.__getitem__(index))
-            return AgentAction(_cont, _disc_list)
-        else:
-            return super().__getitem__(index)
+        _cont = None
+        _disc_list = []
+        if self.continuous_tensor is not None:
+            _cont = self.continuous_tensor.__getitem__(index)
+        if self.discrete_list is not None and len(self.discrete_list) > 0:
+            for _disc in self.discrete_list:
+                _disc_list.append(_disc.__getitem__(index))
+        return AgentAction(_cont, _disc_list)
 
     @property
     def discrete_tensor(self) -> torch.Tensor:
