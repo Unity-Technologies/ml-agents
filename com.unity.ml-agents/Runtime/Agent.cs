5555ea0a316d3961a69d3ec7ec50799a2f57f4a1
Christopher Goy <christopherg@unity3d.com>
2021-03-12 22:31:39 -0800

    Make IActuator implement IHeuristicProvider.

--- com.unity.ml-agents/Runtime/Agent.cs
+++ com.unity.ml-agents/Runtime/Agent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using UnityEngine;
 using Unity.Barracuda;
 using Unity.MLAgents.Actuators;
