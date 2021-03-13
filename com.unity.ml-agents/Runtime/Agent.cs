df9f1346f1080dce73f71b0f39f0d5ea56e44e39
Christopher Goy <christopherg@unity3d.com>
2021-03-12 22:38:41 -0800

    Remove import.

--- com.unity.ml-agents/Runtime/Agent.cs
+++ com.unity.ml-agents/Runtime/Agent.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using UnityEngine;
 using Unity.Barracuda;
 using Unity.MLAgents.Actuators;
