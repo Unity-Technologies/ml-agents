4d64f86f3f7b6eb2cac8f18b0a1507046b1a2c43
Christopher Goy <christopherg@unity3d.com>
2021-03-19 01:16:10 -0700

    Move physics/sensor tests that fail in edit mode depending on what's in the scene to playmode tests.

--- com.unity.ml-agents/Tests/Runtime/Sensor/CameraSensorTest.cs
+++ com.unity.ml-agents/Tests/Runtime/Sensor/CameraSensorTest.cs
@@ -2,6 +2,7 @@ using System;
 using NUnit.Framework;
 using UnityEngine;
 using Unity.MLAgents.Sensors;
+using Object = System.Object;
 
 namespace Unity.MLAgents.Tests
 {
@@ -19,6 +20,11 @@ namespace Unity.MLAgents.Tests
                     var width = 24;
                     var height = 16;
                     var camera = Camera.main;
+                    var c = new GameObject();
+                    if (ReferenceEquals(null, camera))
+                    {
+                        camera = c.AddComponent<Camera>();
+                    }
                     var sensor = new CameraSensor(camera, width, height, grayscale, "TestCameraSensor", compression);
 
                     var obsWriter = new ObservationWriter();
@@ -27,6 +33,7 @@ namespace Unity.MLAgents.Tests
                     Assert.AreEqual((int)compression, (int)obs.CompressionType);
                     var expectedShape = new[] { height, width, grayscale ? 1 : 3 };
                     Assert.AreEqual(expectedShape, obs.Shape);
+                    UnityEngine.Object.DestroyImmediate(c);
                 }
             }
         }
