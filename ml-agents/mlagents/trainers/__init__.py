b514fb87e2a5db200803e47f43884708c62c8a23
Ervin Teng <ervin@unity3d.com>
2021-04-08 17:09:48 -0400

    Update Python and release versions

--- ml-agents/mlagents/trainers/__init__.py
+++ ml-agents/mlagents/trainers/__init__.py
@@ -1,5 +1,5 @@
 # Version of the library that will be used to upload to pypi
-__version__ = "0.25.0"
+__version__ = "0.25.1"
 
 # Git tag that will be checked to determine whether to trigger upload to pypi
-__release_tag__ = "release_15"
+__release_tag__ = "release_16"
