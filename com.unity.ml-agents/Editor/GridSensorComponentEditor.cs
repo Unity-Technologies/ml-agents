132feb02ecd20849d260a2e42f63bfc1399d23df
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2021-04-14 13:29:45 -0700

    Update com.unity.ml-agents/Editor/GridSensorComponentEditor.cs
    Co-authored-by: Chris Goy <christopherg@unity3d.com>

--- com.unity.ml-agents/Editor/GridSensorComponentEditor.cs
+++ com.unity.ml-agents/Editor/GridSensorComponentEditor.cs
@@ -12,7 +12,7 @@ namespace Unity.MLAgents.Editor
         {
 #if !MLA_UNITY_PHYSICS_MODULE
             EditorGUILayout.HelpBox("The Physics Module is not currently present.  " +
-            "Please add it to your project in order to use the Ray Perception APIs in the " +
+            "Please add it to your project in order to use the GridSensor APIs in the " +
             $"{nameof(GridSensorComponent)}", MessageType.Warning);
 #endif
 
