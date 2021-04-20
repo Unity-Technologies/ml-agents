cacfc8cadf68847a6e75bb5449291ffb453a3442
Maryam Honari <honari.m94@gmail.com>
2021-04-20 09:36:06 -0700

    collecting latest step as a stat (#5264) (#5295)
    * collecting latest step as a stat
    
    * adding a list of hidden_keys to TB summarywriter to hide unnecessary stats from user
    
    * fixing precommit
    
    * formating
    
    * defined the property types
    
    * moving custom defaults to get_default_stats_writers
    
    * new test for TensorboardWriter.hidden_keys
    
    * improved testing
    
    * explicit None evaluation
    
    Co-authored-by: Ervin T. <ervin@unity3d.com>
    
    * make hidden_keys optional
    
    Co-authored-by: Ervin T. <ervin@unity3d.com>
    
    * adding optional argument
    
    * lowering the training threshold to 0.8 on test_var_len_obs_and_goal_poca
    
    * Update pytest.yml
    
    * Do not merge! droping pytest 3.9 job
    
    * -add back pytest
    -format imports and comments
    
    * back to default threshold for test_var_len_obs_and_goal_poca
    
    Co-authored-by: mahon94 <maryam.honari@unity3d.com>
    Co-authored-by: Ervin T. <ervin@unity3d.com>
    
    Co-authored-by: mahon94 <maryam.honari@unity3d.com>
    Co-authored-by: Ervin T. <ervin@unity3d.com>

--- ml-agents/mlagents/trainers/stats.py
+++ ml-agents/mlagents/trainers/stats.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from enum import Enum
-from typing import List, Dict, NamedTuple, Any
+from typing import List, Dict, NamedTuple, Any, Optional
 import numpy as np
 import abc
 import os
@@ -13,7 +13,6 @@ from mlagents_envs.logging_util import get_logger
 from mlagents_envs.timers import set_gauge
 from torch.utils.tensorboard import SummaryWriter
 from mlagents.torch_utils.globals import get_rank
-
 
 logger = get_logger(__name__)
 
@@ -212,7 +211,12 @@ class ConsoleWriter(StatsWriter):
 
 
 class TensorboardWriter(StatsWriter):
-    def __init__(self, base_dir: str, clear_past_data: bool = False):
+    def __init__(
+        self,
+        base_dir: str,
+        clear_past_data: bool = False,
+        hidden_keys: Optional[List[str]] = None,
+    ):
         """
         A StatsWriter that writes to a Tensorboard summary.
 
@@ -220,16 +224,21 @@ class TensorboardWriter(StatsWriter):
         {base_dir}/{category} directory.
         :param clear_past_data: Whether or not to clean up existing Tensorboard files associated with the base_dir and
         category.
+        :param hidden_keys: If provided, Tensorboard Writer won't write statistics identified with these Keys in
+        Tensorboard summary.
         """
         self.summary_writers: Dict[str, SummaryWriter] = {}
         self.base_dir: str = base_dir
         self._clear_past_data = clear_past_data
+        self.hidden_keys: List[str] = hidden_keys if hidden_keys is not None else []
 
     def write_stats(
         self, category: str, values: Dict[str, StatsSummary], step: int
     ) -> None:
         self._maybe_create_summary_writer(category)
         for key, value in values.items():
+            if key in self.hidden_keys:
+                continue
             self.summary_writers[category].add_scalar(
                 f"{key}", value.aggregated_value, step
             )
