4feb64deff15f5f6a606b3f2f28cb92f2e158b2a
Christopher Goy <christopherg@unity3d.com>
2021-04-28 17:03:17 -0700

    Update to pre2

--- com.unity.ml-agents/Runtime/Academy.cs
+++ com.unity.ml-agents/Runtime/Academy.cs
@@ -107,7 +107,7 @@ namespace Unity.MLAgents
         /// Unity package version of com.unity.ml-agents.
         /// This must match the version string in package.json and is checked in a unit test.
         /// </summary>
-        internal const string k_PackageVersion = "2.0.0-pre.1";
+        internal const string k_PackageVersion = "2.0.0-pre.2";
 
         const int k_EditorTrainingPort = 5004;
 
@@ -301,7 +301,7 @@ namespace Unity.MLAgents
                 // This try-catch is because DontDestroyOnLoad cannot be used in Editor Tests
                 GameObject.DontDestroyOnLoad(m_StepperObject);
             }
-            catch { }
+            catch {}
         }
 
         /// <summary>
@@ -482,13 +482,13 @@ namespace Unity.MLAgents
 
         void ResetActions()
         {
-            DecideAction = () => { };
-            DestroyAction = () => { };
-            AgentPreStep = i => { };
-            AgentSendState = () => { };
-            AgentAct = () => { };
-            AgentForceReset = () => { };
-            OnEnvironmentReset = () => { };
+            DecideAction = () => {};
+            DestroyAction = () => {};
+            AgentPreStep = i => {};
+            AgentSendState = () => {};
+            AgentAct = () => {};
+            AgentForceReset = () => {};
+            OnEnvironmentReset = () => {};
         }
 
         static void OnQuitCommandReceived()
