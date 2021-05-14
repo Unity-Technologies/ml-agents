5d97acb78326bf8aa6f9dcecb26744496b260d9e
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2021-05-14 09:43:51 -0700

    exclude test_visual_encoder_trains from GPU test

--- ml-agents/mlagents/trainers/tests/torch/test_encoders.py
+++ ml-agents/mlagents/trainers/tests/torch/test_encoders.py
@@ -103,6 +103,7 @@ def test_visual_encoder(vis_class, image_size):
         (FullyConnectedVisualEncoder, 36),
     ],
 )
+@pytest.mark.check_environment_trains
 def test_visual_encoder_trains(vis_class, size):
     torch.manual_seed(0)
     image_size = (size, size, 1)
