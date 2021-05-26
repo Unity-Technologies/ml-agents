12eda6412c9e308a8bb9c044fcb27cbfdfcb6b5f
Chris Elion <chris.elion@unity3d.com>
2021-05-26 13:12:02 -0700

    copy yamato util from main

--- ml-agents/tests/yamato/yamato_utils.py
+++ ml-agents/tests/yamato/yamato_utils.py
@@ -3,19 +3,17 @@ import shutil
 import subprocess
 import yaml
 from typing import List, Optional
+from sys import platform
 
 
 def get_unity_executable_path():
-    UNITY_VERSION = os.environ["UNITY_VERSION"]
-    BOKKEN_UNITY = f"/Users/bokken/{UNITY_VERSION}/Unity.app/Contents/MacOS/Unity"
-    HUB_UNITY = (
-        f"/Applications/Unity/Hub/Editor/{UNITY_VERSION}/Unity.app/Contents/MacOS/Unity"
-    )
-    if os.path.exists(BOKKEN_UNITY):
-        return BOKKEN_UNITY
-    if os.path.exists(HUB_UNITY):
-        return HUB_UNITY
-    raise FileNotFoundError("Can't find bokken or hub executables")
+    if platform == "darwin":
+        downloader_install_path = "./.Editor/Unity.app/Contents/MacOS/Unity"
+    else:  # if platform == "linux":
+        downloader_install_path = "./.Editor/Unity"
+    if os.path.exists(downloader_install_path):
+        return downloader_install_path
+    raise FileNotFoundError("Can't find executable from unity-downloader-cli")
 
 
 def get_base_path():
