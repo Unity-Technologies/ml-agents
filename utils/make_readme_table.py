b57b8fa7edc4c965727ccbbe9442a4a17ed1eab0
Chris Elion <chris.elion@unity3d.com>
2021-06-08 11:53:28 -0700

    Update table with new release (#5425)

--- utils/make_readme_table.py
+++ utils/make_readme_table.py
@@ -135,6 +135,7 @@ versions = [
     ReleaseInfo("release_15", "1.9.0", "0.25.0", "March 17, 2021"),
     ReleaseInfo("release_16", "1.9.1", "0.25.1", "April 13, 2021"),
     ReleaseInfo("release_17", "2.0.0", "0.26.0", "April 22, 2021"),
+    ReleaseInfo("release_18", "2.1.0", "0.27.0", "June 9, 2021"),
     # Verified releases
     ReleaseInfo("", "1.0.8", "0.16.1", "May 26, 2021", is_verified=True),
     ReleaseInfo("", "1.0.7", "0.16.1", "March 8, 2021", is_verified=True),
