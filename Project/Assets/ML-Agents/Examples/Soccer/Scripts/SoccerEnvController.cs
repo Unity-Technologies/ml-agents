d6d94b51e3baacfbe7e261806b8563ce6d715951
Andrew Cohen <andrew.cohen@unity3d.com>
2021-06-08 09:01:47 -0700

    cast time penalty to float

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerEnvController.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerEnvController.cs
@@ -100,12 +100,12 @@ public class SoccerEnvController : MonoBehaviour
     {
         if (scoredTeam == Team.Blue)
         {
-            m_BlueAgentGroup.AddGroupReward(1 - m_ResetTimer / MaxEnvironmentSteps);
+            m_BlueAgentGroup.AddGroupReward(1 - (float)m_ResetTimer / MaxEnvironmentSteps);
             m_PurpleAgentGroup.AddGroupReward(-1);
         }
         else
         {
-            m_PurpleAgentGroup.AddGroupReward(1 - m_ResetTimer / MaxEnvironmentSteps);
+            m_PurpleAgentGroup.AddGroupReward(1 - (float)m_ResetTimer / MaxEnvironmentSteps);
             m_BlueAgentGroup.AddGroupReward(-1);
         }
         m_PurpleAgentGroup.EndGroupEpisode();
