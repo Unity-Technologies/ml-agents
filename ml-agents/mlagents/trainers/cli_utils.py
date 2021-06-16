cc1028f9747ad87bddcc2c30610db11f80f26e32
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2021-06-16 16:22:54 -0700

    fix argparser key for torch device

--- ml-agents/mlagents/trainers/cli_utils.py
+++ ml-agents/mlagents/trainers/cli_utils.py
@@ -257,7 +257,7 @@ def _create_parser() -> argparse.ArgumentParser:
     torch_conf.add_argument(
         "--torch-device",
         default=None,
-        dest="device",
+        dest="torch_device",
         action=DetectDefault,
         help='Settings for the default torch.device used in training, for example, "cpu", "cuda", or "cuda:0"',
     )
