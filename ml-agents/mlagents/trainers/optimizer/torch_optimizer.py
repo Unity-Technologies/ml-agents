32253f54d6c5dbe5aaf1406dddc694f0076b03d7
Ervin Teng <ervin@unity3d.com>
2021-06-17 17:32:18 -0700

    Remove extraneous probability weighting

--- ml-agents/mlagents/trainers/optimizer/torch_optimizer.py
+++ ml-agents/mlagents/trainers/optimizer/torch_optimizer.py
@@ -88,11 +88,7 @@ class TorchOptimizer(Optimizer):
 
         # Return KL
         return torch.mean(
-            torch.sum(
-                torch.exp(flattened_policy_log_probs)
-                * (flattened_policy_log_probs - mixture_log_probs),
-                dim=-1,
-            )
+            torch.sum(flattened_policy_log_probs - mixture_log_probs, dim=-1)
         )
 
     def _evaluate_by_sequence(
