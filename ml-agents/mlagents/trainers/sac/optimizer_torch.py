cf0c8c4f24a12c746881cf5947127108ccf4cd9c
Kolby Nottingham <kolbytn@gmail.com>
2021-07-06 17:18:55 -0700

    MEDE no noise logging fix

--- ml-agents/mlagents/trainers/sac/optimizer_torch.py
+++ ml-agents/mlagents/trainers/sac/optimizer_torch.py
@@ -231,7 +231,7 @@ class DiverseNetworkVariational(torch.nn.Module):
         )
 
         if self._z_sigma is None:
-            return base_loss, base_loss, 0, 0, 0
+            return base_loss, base_loss, None, None, None
         else:
             _, mu = self.predict(obs_input, action_input, detach_action, var_noise)
             kl_loss = ModelUtils.masked_mean(
@@ -959,9 +959,9 @@ class TorchSACOptimizer(TorchOptimizer):
             update_stats.update({
                 "Policy/MEDE Loss": mede_loss.item(),
                 "Policy/MEDE Base": base_loss.item(),
-                "Policy/MEDE Variational": vail_loss.item(),
-                "Policy/MEDE KL": kl_loss.item(),
-                "Policy/MEDE beta": beta.item(),
+                "Policy/MEDE Variational": vail_loss.item() if vail_loss is not None else 0,
+                "Policy/MEDE KL": kl_loss.item() if kl_loss is not None else 0,
+                "Policy/MEDE beta": beta.item() if beta is not None else 0,
             })
 
         return update_stats
