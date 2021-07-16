5a636be2889addc971c479ab99ab5a2041e51d78
HH <brandonh@unity3d.com>
2021-07-15 19:52:47 -0700

    add more models

---
+++ Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/PolyDrawExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class PolyDrawExample : MonoBehaviour
+{
+    public int numberOfSides;
+    public float polygonRadius;
+    // public Vector2 polygonCenter;
+
+    void Update()
+    {
+        // DebugDrawPolygon(polygonCenter, polygonRadius, numberOfSides);
+        DebugDrawPolygon(transform.position, polygonRadius, numberOfSides);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        foreach (var pos in rayPosList)
+        {
+            Gizmos.DrawSphere(pos,.1f);
+        }
+    }
+
+    public List<Vector3> rayPosList = new List<Vector3>();
+    // Draw a polygon in the XY plane with a specfied position, number of sides
+    // and radius.
+    void DebugDrawPolygon(Vector3 center, float radius, int numSides)
+    {
+        rayPosList.Clear();
+        // The corner that is used to start the polygon (parallel to the X axis).
+        // Vector3 startCorner = new Vector3(radius, 0) + center;
+        // Vector3 startCorner = new Vector3(radius, 0, radius);
+        Vector3 startCorner = transform.TransformPoint(new Vector3(radius, 0, radius));
+        startCorner.y = transform.position.y;
+
+        // The "previous" corner point, initialised to the starting corner.
+        Vector3 previousCorner = startCorner;
+
+        // For each corner after the starting corner...
+        for (int i = 0; i < numSides; i++)
+        {
+            // Calculate the angle of the corner in radians.
+            float cornerAngle = 2f * Mathf.PI / (float)numSides * i;
+
+            // Get the X and Y coordinates of the corner point.
+            // Vector2 currentCorner = new Vector2(Mathf.Cos(cornerAngle) * radius, Mathf.Sin(cornerAngle) * radius) + center;
+            // Vector2 currentCorner = new Vector2(Mathf.Sin(cornerAngle) * radius, Mathf.Cos(cornerAngle) * radius) + center;
+            Vector3 currentCorner = new Vector3(Mathf.Cos(cornerAngle) * radius, 0, Mathf.Sin(cornerAngle) * radius);
+            currentCorner = transform.TransformPoint(currentCorner);
+            currentCorner.y = transform.position.y;
+            rayPosList.Add(currentCorner);
+            // Debug.DrawRay(currentCorner);
+            // Draw a side of the polygon by connecting the current corner to the previous one.
+            Debug.DrawLine(currentCorner, previousCorner);
+
+            // Having used the current corner, it now becomes the previous corner.
+            previousCorner = currentCorner;
+        }
+
+        // Draw the final side by connecting the last corner to the starting corner.
+        Debug.DrawLine(startCorner, previousCorner);
+    }
+}
---
+++ Project/Assets/ML-Agents/Examples/SharedAssets/Scripts/ShowTriangleIndex.cs
@@ -0,0 +1,39 @@
+﻿// This script draws a debug line around mesh triangles
+// as you move the mouse over them.
+using UnityEngine;
+using System.Collections;
+
+public class ShowTriangleIndex : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+        if (meshCollider == null || meshCollider.sharedMesh == null)
+            return;
+
+        Mesh mesh = meshCollider.sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
+        Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
+        Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
+        Transform hitTransform = hit.collider.transform;
+        p0 = hitTransform.TransformPoint(p0);
+        p1 = hitTransform.TransformPoint(p1);
+        p2 = hitTransform.TransformPoint(p2);
+        Debug.DrawLine(p0, p1);
+        Debug.DrawLine(p1, p2);
+        Debug.DrawLine(p2, p0);
+    }
+}
