d65438375aecfcc95da14ca830318f993e826043
vincentpierre <vincentpierre@unity3d.com>
2021-07-23 13:39:13 -0700

    improvements

--- gym-unity/gym_unity/envs/__init__.py
+++ gym-unity/gym_unity/envs/__init__.py
@@ -1,12 +1,12 @@
 from gym.envs.registration import register
 from mlagents_envs.registry import default_registry
-from gym_unity.envs.ma_gym import MultiAgentGymWrapper
+from gym_unity.envs.gym_env import UnityToGymWrapper
 
 
 def gym_entry_point(env_name, **kwargs):
     def create(**kwagrs):
         _e = default_registry[env_name].make(**kwargs)
-        return MultiAgentGymWrapper(_e)
+        return UnityToGymWrapper(_e)
 
     return create
 
--- gym-unity/gym_unity/envs/gym_env.py
+++ gym-unity/gym_unity/envs/gym_env.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Any, Dict, List, Optional, Tuple, Union
 
+import itertools
 import gym
 from gym import error, spaces
 from urllib.parse import urlparse, parse_qs
@@ -46,20 +47,27 @@ class AgentStatus:
         self.needs_action = needs_action
 
 
-class MultiAgentGymWrapper(gym.Env):
-    def __init__(self, env: BaseEnv):
+class UnityToGymWrapper(gym.Env):
+    def __init__(self, env: BaseEnv, action_space_seed: Optional[int] = None):
         self._env = env
         self._agent_index = -1
         self._current_batch: Optional[Tuple[DecisionSteps, TerminalSteps]] = None
         self._behavior_index = 0
         self._current_action: Optional[ActionTuple] = None
         self._last: Optional[AgentStatus] = None
+        self._action_spaces: Dict[str, spaces.Space] = {}
+        self._obs_spaces: Dict[str, spaces.Space] = {}
+        self._action_sampling_seed = action_space_seed
 
     def _current_behavior_name(self) -> str:
-        return list(self._env.behavior_specs.keys())[self._behavior_index]
+        names = list(self._env.behavior_specs.keys())
+        if len(names) > self._behavior_index:
+            return names[self._behavior_index]
+        return None
+
 
     @property
-    def last(self) -> Optional[AgentStatus]:
+    def active(self) -> Optional[AgentStatus]:
         """
         This is not part of the original gym api. It is needed to know which behaviors
         and agent ID the observation from a reset call belongs to.
@@ -68,8 +76,12 @@ class MultiAgentGymWrapper(gym.Env):
         return self._last
 
     @property
-    def observation_space(self) -> spaces.Space:
+    def observation_space(self) -> Optional[spaces.Space]:
         current_behavior = self._current_behavior_name()
+        if current_behavior is None:
+            return None
+        if current_behavior in self._obs_spaces:
+            return self._obs_spaces[current_behavior]
         obs_spec = self._env.behavior_specs[current_behavior].observation_specs
         obs_spaces = tuple(
             [
@@ -77,27 +89,46 @@ class MultiAgentGymWrapper(gym.Env):
                 for spec in obs_spec
             ]
         )
-        return spaces.Tuple(obs_spaces)
+        if len(obs_spaces) == 1:
+            self._obs_spaces[current_behavior] = obs_spaces[0]
+        else:
+            self._obs_spaces[current_behavior] = spaces.Tuple(obs_spaces)
+        return self._obs_spaces[current_behavior]
 
     @property
-    def action_space(self) -> spaces.Space:
+    def action_space(self) -> Optional[spaces.Space]:
         current_behavior = self._current_behavior_name()
+        if current_behavior is None:
+            return None
+        if current_behavior in self._action_spaces:
+            return self._action_spaces[current_behavior]
         act_spec = self._env.behavior_specs[current_behavior].action_spec
         if act_spec.continuous_size == 0 and len(act_spec.discrete_branches) == 0:
             raise Exception("No actions found")
         if act_spec.discrete_size == 1:
             d_space = spaces.Discrete(act_spec.discrete_branches[0])
+            if self._action_sampling_seed is not None:
+                d_space.seed(self._action_sampling_seed)
             if act_spec.continuous_size == 0:
-                return d_space
+                self._action_spaces[current_behavior] = d_space
+                return self._action_spaces[current_behavior]
         if act_spec.discrete_size > 0:
             d_space = spaces.MultiDiscrete(act_spec.discrete_branches)
+            if self._action_sampling_seed is not None:
+                d_space.seed(self._action_sampling_seed)
             if act_spec.continuous_size == 0:
-                return d_space
+                self._action_spaces[current_behavior] = d_space
+                return self._action_spaces[current_behavior]
         if act_spec.continuous_size > 0:
             c_space = spaces.Box(-1, 1, (act_spec.continuous_size,), dtype=np.int32)
+            if self._action_sampling_seed is not None:
+                c_space.seed(self._action_sampling_seed)
             if len(act_spec.discrete_branches) == 0:
-                return c_space
-        return spaces.Tuple(tuple(c_space, d_space))
+                self._action_spaces[current_behavior] = c_space
+                return self._action_spaces[current_behavior]
+        self._action_spaces[current_behavior] = spaces.Tuple(tuple(c_space, d_space))
+        return self._action_spaces[current_behavior]
+
 
     def step(
         self, action: Any = None
@@ -158,7 +189,8 @@ class MultiAgentGymWrapper(gym.Env):
         if self._agent_index < len(termination_batch):
             # The index is within the terminal steps
             index = self._agent_index
-            obs = tuple([batch_obs[index] for batch_obs in termination_batch.obs])
+            obs = [batch_obs[index] for batch_obs in termination_batch.obs]
+            obs = obs if len(obs) > 1 else obs[0]
             reward = termination_batch.reward[index]
             done = True
             self._last = AgentStatus(
@@ -178,7 +210,8 @@ class MultiAgentGymWrapper(gym.Env):
         if self._agent_index < len(decision_batch) + len(termination_batch):
             index = self._agent_index - len(termination_batch)
             # the index is within the decsion steps
-            obs = tuple([batch_obs[index] for batch_obs in decision_batch.obs])
+            obs = [batch_obs[index] for batch_obs in decision_batch.obs]
+            obs = obs if len(obs) > 1 else obs[0]
             reward = decision_batch.reward[index]
             done = False
 
