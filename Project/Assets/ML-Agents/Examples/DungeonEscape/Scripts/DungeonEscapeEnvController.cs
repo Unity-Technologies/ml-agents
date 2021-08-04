00cad8e211ee1128f6e299e6d8074ee772a09c6c
Andrew Cohen <andrew.cohen@unity3d.com>
2021-08-03 18:17:44 -0700

    update dungeon to more agents and dragons types

--- Project/Assets/ML-Agents/Examples/DungeonEscape/Scripts/DungeonEscapeEnvController.cs
+++ Project/Assets/ML-Agents/Examples/DungeonEscape/Scripts/DungeonEscapeEnvController.cs
@@ -71,7 +71,7 @@ public class DungeonEscapeEnvController : MonoBehaviour
 
     private int m_NumberOfRemainingPlayers;
     public GameObject Key;
-    public GameObject Tombstone;
+    //public GameObject Tombstone;
     private SimpleMultiAgentGroup m_AgentGroup;
     void Start()
     {
@@ -158,30 +158,57 @@ public class DungeonEscapeEnvController : MonoBehaviour
         ResetScene();
     }
 
+    public Vector3 GetNearestAgent(Vector3 baddiePosition)
+    {
+        Vector3 nearest = Vector3.zero;
+        float min_dist = 50000;
+        foreach (var item in AgentsList)
+        {
+            if (item.Agent.gameObject.activeSelf)
+            {
+                var dist = Vector3.Distance(baddiePosition, item.Agent.transform.position); 
+                if (dist < min_dist)
+                {
+                    nearest = item.Agent.transform.position;
+                    min_dist = dist;
+                }
+             }
+         }
+         return nearest;
+    }
+
     public void KilledByBaddie(PushAgentEscape agent, Collision baddieCol)
     {
-        baddieCol.gameObject.SetActive(false);
-        m_NumberOfRemainingPlayers--;
-        if (m_Absorbing == 0.0f)
+        var baddie = baddieCol.gameObject.GetComponent<SimpleNPC>();
+        if (baddie.KeyCarrier)
         {
-            agent.gameObject.SetActive(false);
+            baddieCol.gameObject.SetActive(false);
+            Key.transform.SetPositionAndRotation(baddieCol.collider.transform.position, baddieCol.collider.transform.rotation);
+            Key.SetActive(true);
+        }
+
+        m_NumberOfRemainingPlayers--;
+        if (m_NumberOfRemainingPlayers == 0 || agent.IHaveAKey)
+        {
+            m_AgentGroup.EndGroupEpisode();
+            ResetScene();
         }
         else
         {
-            Vector3 pos = new Vector3(Random.Range(200f, 2000f), Random.Range(-1000f, 1000f), Random.Range(-1000f, 1000f));
-            var rot = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
-            agent.transform.SetPositionAndRotation(pos, rot);
+            if (m_Absorbing == 0.0f)
+            {
+                agent.gameObject.SetActive(false);
+            }
+            else
+            {
+                Vector3 pos = new Vector3(Random.Range(200f, 2000f), Random.Range(-1000f, 1000f), Random.Range(-1000f, 1000f));
+                var rot = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
+                agent.transform.SetPositionAndRotation(pos, rot);
+            }
         }
 
         print($"{baddieCol.gameObject.name} ate {agent.transform.name}");
 
-        //Spawn Tombstone
-        Tombstone.transform.SetPositionAndRotation(agent.transform.position, agent.transform.rotation);
-        Tombstone.SetActive(true);
-
-        //Spawn the Key Pickup
-        Key.transform.SetPositionAndRotation(baddieCol.collider.transform.position, baddieCol.collider.transform.rotation);
-        Key.SetActive(true);
     }
 
     /// <summary>
@@ -222,7 +249,7 @@ public class DungeonEscapeEnvController : MonoBehaviour
         m_AgentGroup.EndGroupEpisode();
 
         // Swap ground material for a bit to indicate we scored.
-        StartCoroutine(GoalScoredSwapGroundMaterial(m_PushBlockSettings.failMaterial, 0.5f));
+        //StartCoroutine(GoalScoredSwapGroundMaterial(m_PushBlockSettings.failMaterial, 0.5f));
         ResetScene();
     }
 
@@ -249,6 +276,7 @@ public class DungeonEscapeEnvController : MonoBehaviour
         //Reset Agents
         foreach (var item in AgentsList)
         {
+            Debug.Log(item.Agent);
             var pos = UseRandomAgentPosition ? GetRandomSpawnPos() : item.StartingPos;
             var rot = UseRandomAgentRotation ? GetRandomRot() : item.StartingRot;
 
@@ -262,21 +290,24 @@ public class DungeonEscapeEnvController : MonoBehaviour
         }
 
         //Reset Key
-        Key.SetActive(false);
+       // Key.SetActive(false);
 
         //Reset Tombstone
-        Tombstone.SetActive(false);
+        //Tombstone.SetActive(false);
 
         //End Episode
         foreach (var item in DragonsList)
         {
-            if (!item.Agent)
-            {
-                return;
-            }
-            item.Agent.transform.SetPositionAndRotation(item.StartingPos, item.StartingRot);
-            item.Agent.SetRandomWalkSpeed();
+            //var pos = item.StartingPos;
+            //var pos = UseRandomAgentPosition ? GetRandomSpawnPos() : item.StartingPos;
+           // var rot = UseRandomAgentRotation ? GetRandomRot() : item.StartingRot;
+
+            //item.Agent.transform.SetPositionAndRotation(pos, rot);
+
             item.Agent.gameObject.SetActive(true);
+            item.T.SetPositionAndRotation(item.StartingPos, item.StartingRot);
+            item.Agent.Initialize();
         }
+        Debug.Log("DONEINIT");
     }
 }
