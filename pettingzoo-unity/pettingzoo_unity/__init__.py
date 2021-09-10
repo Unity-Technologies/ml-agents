58f537fa0f79f05bd177abc075201cbe6392ecac
Ruo-Ping Dong <ruoping.dong@unity3d.com>
2021-09-10 14:21:55 -0700

    add setup

---
+++ pettingzoo-unity/pettingzoo_unity/__init__.py
@@ -0,0 +1,7 @@
+# Version of the library that will be used to upload to pypi
+__version__ = "0.28.0.dev0"
+
+# Git tag that will be checked to determine whether to trigger upload to pypi
+__release_tag__ = None
+
+from .pettingzoo_wrapper import UnityToPettingZooWrapper  # noqa
