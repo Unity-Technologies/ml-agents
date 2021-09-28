7d3a8bbf8cf2f984d7441fe1966365353b7b9436
Miguel Alonso Jr <miguelalonsojr>
2021-09-28 14:22:51 -0400

    Added setup for tests for the training area replicator.

--- com.unity.ml-agents/Tests/Editor/Areas/TrainingAreaReplicatorTests.cs
+++ com.unity.ml-agents/Tests/Editor/Areas/TrainingAreaReplicatorTests.cs
@@ -9,28 +9,32 @@ namespace Unity.MLAgents.Tests.Areas
     [TestFixture]
     public class TrainingAreaReplicatorTests
     {
+        private TrainingAreaReplicator m_Replicator;
+
+        [SetUp]
+        public void Setup()
+        {
+            var gameObject = new GameObject();
+            var trainingArea = new GameObject();
+            trainingArea.name = "MyTrainingArea";
+            m_Replicator = gameObject.AddComponent<TrainingAreaReplicator>();
+            m_Replicator.numAreas = 10;
+            m_Replicator.baseArea = trainingArea;
+            m_Replicator.Awake();
+            m_Replicator.OnEnable();
+        }
+
         [Test]
         public void TestComputeGridSize()
         {
-            var gameObject = new GameObject();
-            var trainingAreaReplicator = gameObject.AddComponent<TrainingAreaReplicator>();
-            trainingAreaReplicator.numAreas = 10;
-            trainingAreaReplicator.Awake();
-            Assert.AreEqual(new int3(3, 3, 1), trainingAreaReplicator.GridSize);
+            Assert.GreaterOrEqual(m_Replicator.GridSize.x * m_Replicator.GridSize.y * m_Replicator.GridSize.z, m_Replicator.numAreas);
+            Assert.AreEqual(new int3(2, 2, 6), m_Replicator.GridSize);
         }
 
         [Test]
         public void TestAddEnvironments()
         {
-            var gameObject = new GameObject();
-            var trainingArea = new GameObject();
-            trainingArea.name = "MyTrainingArea";
-            var trainingAreaReplicator = gameObject.AddComponent<TrainingAreaReplicator>();
-            trainingAreaReplicator.numAreas = 10;
-            trainingAreaReplicator.baseArea = trainingArea;
-            trainingAreaReplicator.Awake();
-            trainingAreaReplicator.OnEnable();
-            var trainingAreas = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == trainingAreaReplicator.TrainingAreaName);
+            var trainingAreas = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == m_Replicator.TrainingAreaName);
             Assert.AreEqual(10, trainingAreas.Count());
 
         }
