377d622cf4375431f5a4222de2512c6b0e1596a7
Andrew Cohen <andrew.cohen@unity3d.com>
2022-01-17 15:13:25 -0800

    remove unused envs

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerSettings.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SoccerSettings : MonoBehaviour
-{
-    public Material purpleMaterial;
-    public Material blueMaterial;
-    public bool randomizePlayersTeamForTraining = true;
-    public float agentRunSpeed;
-}
