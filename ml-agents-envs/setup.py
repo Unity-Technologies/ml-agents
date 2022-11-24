b9382ca8ddc1a03b7e1c845591dca15678e6259e
mahon94 <maryam.honari@unity3d.com>
2022-11-24 08:31:29 -0800

    remove duplicate numpy

--- ml-agents-envs/setup.py
+++ ml-agents-envs/setup.py
@@ -51,7 +51,6 @@ setup(
     install_requires=[
         "cloudpickle",
         "grpcio>=1.11.0",
-        "numpy>=1.14.1",
         "Pillow>=4.2.1",
         "protobuf>=3.6",
         "pyyaml>=3.1.0",
