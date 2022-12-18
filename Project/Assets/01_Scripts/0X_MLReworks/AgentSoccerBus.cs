37a7ccf9e6ba156574f2811af18171c8f890256b
BHPAV <120694498+BHPAV@users.noreply.github.com>
2022-12-18 19:24:23 +1100

    Bus Setup - Done
    Agent Prefas for team testing.
    
    Agent is based around the model. Couldn't figure it out any other way.
    
    Sis_ABus must be centrally based around the RB and Model.

---
+++ Project/Assets/01_Scripts/0X_MLReworks/AgentSoccerBus.cs
@@ -0,0 +1,267 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
+public class AgentSoccerBus : Agent
+{
+    // Note that that the detectable tags are different for the blue and purple teams. The order is
+    // * ball
+    // * own goal
+    // * opposing goal
+    // * wall
+    // * own teammate
+    // * opposing player
+
+    public enum Position
+    {
+        Striker,
+        Goalie,
+        Generic
+    }
+
+    [HideInInspector]
+    public Team team;
+    float m_KickPower;
+    // The coefficient for the reward for colliding with a ball. Set using curriculum.
+    float m_BallTouch;
+    public Position position;
+
+    const float k_Power = 2000f;
+    float m_Existential;
+    float m_LateralSpeed;
+    float m_ForwardSpeed;
+
+
+    [HideInInspector]
+    public Rigidbody agentRb;
+    SoccerSettings m_SoccerSettings;
+    BehaviorParameters m_BehaviorParameters;
+    public Vector3 initialPos;
+    public float rotSign;
+
+    EnvironmentParameters m_ResetParams;
+
+
+    public CarDriver carDriver;
+
+    public override void Initialize()
+    {
+        SoccerBusEnvController envController = GetComponentInParent<SoccerBusEnvController>();
+        if (envController != null)
+        {
+            m_Existential = 1f / envController.MaxEnvironmentSteps;
+        }
+        else
+        {
+            m_Existential = 1f / MaxStep;
+        }
+
+        m_BehaviorParameters = gameObject.GetComponent<BehaviorParameters>();
+        if (m_BehaviorParameters.TeamId == (int)Team.Blue)
+        {
+            team = Team.Blue;
+            initialPos = new Vector3(transform.position.x - 5f, .5f, transform.position.z);
+            rotSign = 1f;
+        }
+        else
+        {
+            team = Team.Purple;
+            initialPos = new Vector3(transform.position.x + 5f, .5f, transform.position.z);
+            rotSign = -1f;
+        }
+        if (position == Position.Goalie)
+        {
+            m_LateralSpeed = 1.0f;
+            m_ForwardSpeed = 1.0f;
+        }
+        else if (position == Position.Striker)
+        {
+            m_LateralSpeed = 0.3f;
+            m_ForwardSpeed = 1.3f;
+        }
+        else
+        {
+            m_LateralSpeed = 0.3f;
+            m_ForwardSpeed = 1.0f;
+        }
+        m_SoccerSettings = FindObjectOfType<SoccerSettings>();
+        agentRb = GetComponent<Rigidbody>();
+        agentRb.maxAngularVelocity = 500;
+
+        m_ResetParams = Academy.Instance.EnvironmentParameters;
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        //var dirToGo = Vector3.zero;
+        //var rotateDir = Vector3.zero;
+
+        m_KickPower = 0f;
+        
+
+        //var forwardAxis = act[0];
+        //var rightAxis = act[1];
+        //var rotateAxis = act[2];
+        /*
+        switch (forwardAxis)
+        {
+            case 1:
+                dirToGo = transform.forward * m_ForwardSpeed;
+                m_KickPower = 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -m_ForwardSpeed;
+                break;
+        }
+
+        switch (rightAxis)
+        {
+            case 1:
+                dirToGo = transform.right * m_LateralSpeed;
+                break;
+            case 2:
+                dirToGo = transform.right * -m_LateralSpeed;
+                break;
+        }
+
+        switch (rotateAxis)
+        {
+            case 1:
+                rotateDir = transform.up * -1f;
+                break;
+            case 2:
+                rotateDir = transform.up * 1f;
+                break;
+        }
+
+        transform.Rotate(rotateDir, Time.deltaTime * 100f);
+        agentRb.AddForce(dirToGo * m_SoccerSettings.agentRunSpeed,
+            ForceMode.VelocityChange);
+        */
+
+        //// CAR WORKINGS ---------------
+        float forwardAmount = 0f;
+        float turnAmount = 0f;
+        float breakAction = 0f;
+        float boostAction = 0f;
+        float otherAction = 0f;
+
+
+        switch (act[0])
+        {
+            case 0: forwardAmount = 0f; break;
+            case 1: forwardAmount = +1f; break;
+            case 2: forwardAmount = -1f; break;
+        }
+
+        switch (act[1])
+        {
+            case 0: turnAmount = 0f; break;
+            case 1: turnAmount = +1f; break;
+            case 2: turnAmount = -1f; break;
+        }
+
+        switch (act[2])
+        {
+            case 0: breakAction = 0f; break;
+            case 1: breakAction = +1f; break;
+        }
+
+        switch (act[3])
+        {
+            case 0: boostAction = 0f; break;
+            case 1: boostAction = +1f; break;
+        }
+
+        switch (act[4])
+        {
+            case 0: otherAction = 0f; break;
+            case 1: otherAction = +1f; break;
+        }
+
+        carDriver.SetInputs5(forwardAmount, turnAmount, breakAction, boostAction, otherAction);
+        ///// END CAR WORKINGS --------------    
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+
+    {
+
+        if (position == Position.Goalie)
+        {
+            // Existential bonus for Goalies.
+            AddReward(m_Existential);
+        }
+        else if (position == Position.Striker)
+        {
+            // Existential penalty for Strikers
+            AddReward(-m_Existential);
+        }
+        MoveAgent(actionBuffers.DiscreteActions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        int forwardAction = 0;
+        if (Input.GetKey(KeyCode.UpArrow)) forwardAction = 1;
+        if (Input.GetKey(KeyCode.DownArrow)) forwardAction = 2;
+
+        int turnAction = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) turnAction = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) turnAction = 2;
+
+        int breakAction = 0;
+        if (Input.GetKey(KeyCode.Space)) breakAction = 1;
+
+        int boostAction = 0;
+        if (Input.GetKey(KeyCode.LeftShift)) boostAction = 1;
+
+        int generalAction = 0;
+        if (Input.GetKey(KeyCode.F)) generalAction = 1;
+
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = forwardAction;
+        discreteActions[1] = turnAction;
+        discreteActions[2] = breakAction;
+        discreteActions[3] = boostAction;
+        discreteActions[4] = generalAction;
+    }
+    /// <summary>
+    /// Used to provide a "kick" to the ball.
+    /// </summary>
+    void OnCollisionEnter(Collision c)
+    {
+        var force = k_Power * m_KickPower;
+        if (position == Position.Goalie)
+        {
+            force = k_Power;
+        }
+        if (c.gameObject.CompareTag("ball"))
+        {
+            AddReward(.2f * m_BallTouch);
+            var dir = c.contacts[0].point - transform.position;
+            dir = dir.normalized;
+            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        m_BallTouch = m_ResetParams.GetWithDefault("ball_touch", 0);
+    }
+
+    public void HideThisError()
+    {
+        if(m_ForwardSpeed <= 0.0f)
+        {
+            m_ForwardSpeed = 1f;
+        }
+
+        if(m_LateralSpeed <= 0.0f)
+        {
+            m_LateralSpeed = 1f;
+        }
+    }
+
+}
---
+++ Project/Assets/01_Scripts/0X_MLReworks/SoccerBusBallController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SoccerBusBallController : MonoBehaviour
+{
+    public GameObject area;
+    [HideInInspector]
+    public SoccerBusEnvController envController;
+    public string purpleGoalTag; //will be used to check if collided with purple goal
+    public string blueGoalTag; //will be used to check if collided with blue goal
+
+    void Start()
+    {
+        envController = area.GetComponent<SoccerBusEnvController>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
+        {
+            envController.GoalTouched(Team.Blue);
+        }
+        if (col.gameObject.CompareTag(blueGoalTag)) //ball touched blue goal
+        {
+            envController.GoalTouched(Team.Purple);
+        }
+    }
+}
