f4b8e361fe5acc69c775fa85dc973e40389ccaf8
Miguel Alonso Jr <miguel.alonso@unity3d.com>
2022-12-30 23:59:23 -0500

    Added latent sampling to policy.

--- ml-agents/mlagents/trainers/policy/ase_policy.py
+++ ml-agents/mlagents/trainers/policy/ase_policy.py
@@ -1,13 +1,12 @@
 from typing import Dict, Any, List
 
 import numpy as np
-
 from mlagents_envs.base_env import DecisionSteps
 from mlagents_envs.base_env import BehaviorSpec
 from mlagents.trainers.policy import Policy
 from mlagents.trainers.settings import NetworkSettings
 from mlagents.trainers.torch_entities.networks import GlobalSteps
-from mlagents.torch_utils import default_device
+from mlagents.torch_utils import default_device, nn
 from mlagents.trainers.action_info import ActionInfo
 from mlagents.trainers.behavior_id_utils import get_global_agent_id
 from mlagents_envs.timers import timed
@@ -27,6 +26,7 @@ class ASEPolicy(Policy):
         actor_kwargs: Dict[str, Any],
         disc_enc_cls: type,
         disc_enc_kwargs: Dict[str, Any],
+        additional_kwargs: Dict[str, Any]
     ):
         super().__init__(seed, behavior_spec, network_settings)
         self.global_step = GlobalSteps()
@@ -56,8 +56,16 @@ class ASEPolicy(Policy):
         # m_size needed for training is determined by network, not trainer settings
         self.m_size = self.actor.memory_size
 
+        # Save the embedding size
+        self._embedding_size = disc_enc_kwargs["embedding_size"]
+        self._embedding_idx = additional_kwargs["embedding_idx"]
+        self._latent_steps_min = additional_kwargs["latent_steps_min"]
+        self._latent_steps_max = additional_kwargs["latent_steps_max"]
+        self._latent_step_count = {}
+
         self.actor.to(default_device())
         self.discriminator_encoder.to(default_device())
+        self._latents = {}
 
     def _extract_masks(self, decision_requests: DecisionSteps) -> np.ndarray:
         mask = None
@@ -111,6 +119,8 @@ class ASEPolicy(Policy):
             for agent_id in decision_requests.agent_id
         ]  # For 1-D array, the iterator order is correct.
 
+        self._update_latents(decision_requests, global_agent_ids)
+
         run_out = self.evaluate(decision_requests, global_agent_ids)
         self.save_memories(global_agent_ids, run_out.get("memory_out"))
         self.check_nan_action(run_out.get("action"))
@@ -121,11 +131,68 @@ class ASEPolicy(Policy):
             agent_ids=list(decision_requests.agent_id),
         )
 
+    def _update_latents(self, decision_requests: DecisionSteps, global_agent_ids: List[str]) -> None:
+        if self._latents_need_updates(global_agent_ids):
+            self._reset_latents(global_agent_ids)
+            self._reset_latent_step_count(global_agent_ids)
+        else:
+            self._decrease_latent_step_count()
+
+        decision_requests.obs[self._embedding_idx] = np.array([self._latents[k] for k in global_agent_ids])
+
+    def _decrease_latent_step_count(self):
+        for k in self._latent_step_count:
+            self._latent_step_count[k] -= 1
+
+    def _latents_need_updates(self, global_agent_ids: List[str]) -> bool:
+        for agent_id in global_agent_ids:
+            try:
+                if self._latent_step_count[agent_id] <= 0:
+                    return True
+            except KeyError:
+                return True
+        return False
+
+    def _get_latents_need_updates(self, global_agent_ids: List[str]) -> List[str]:
+        needs_update_list = []
+        for agent_id in global_agent_ids:
+            try:
+                if self._latent_step_count[agent_id] <= 0:
+                    needs_update_list.append(agent_id)
+            except KeyError:
+                return global_agent_ids
+        return needs_update_list
+
+    def _reset_latents(self, global_agent_ids: List[str]) -> None:
+        update_list = self._get_latents_need_updates(global_agent_ids)
+        latents = self.sample_latents(len(update_list))
+        self._latents.update(zip(update_list, latents))
+
+    def _reset_latent_step_count(self, global_agent_ids: List[str]) -> None:
+        self._latent_step_count.update(zip(global_agent_ids,
+                                           [np.random.randint(self._latent_steps_min, self._latent_steps_max) for _ in
+                                            range(len(global_agent_ids))]))
+
+    def sample_latents(self, n):
+        z = np.random.normal(np.zeros([n, self._embedding_size])).astype(np.float32)
+        z = self._normalize(z)
+        return z
+
+    @staticmethod
+    def _normalize(z: np.ndarray) -> np.ndarray:
+        denom = np.linalg.norm(z, axis=1, keepdims=True)
+        return z / denom
+
     def increment_step(self, n_steps):
-        pass
+        self.global_step.increment(n_steps)
+        return self.get_current_step()
 
     def get_current_step(self):
         return self.global_step.current_step
+
+    def set_step(self, step: int) -> int:
+        self.global_step.current_step = step
+        return step
 
     def load_weights(self, values: List[np.ndarray]) -> None:
         pass
