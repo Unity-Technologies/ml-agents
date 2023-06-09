e2e0768fd7462b17a2054d3dddf9fe13f1463575
Thomas Simonini <simonini.thomas.pro@gmail.com>
2023-06-09 19:45:43 +0200

    Update ML-Agents to Python 3.10.12 (Colab) (#5929)
    * Update setup.py
    
    *python_requires from <= 3.10.11 to 3.10.12
    
    * Update setup.py
    
    *python_requires from <= 3.10.11 to 3.10.12

--- ml-agents-envs/setup.py
+++ ml-agents-envs/setup.py
@@ -59,7 +59,7 @@ setup(
         "numpy==1.21.2",
         "filelock>=3.4.0",
     ],
-    python_requires=">=3.8.13,<=3.10.11",
+    python_requires=">=3.8.13,<=3.10.12",
     # TODO: Remove this once mypy stops having spurious setuptools issues.
     cmdclass={"verify": VerifyVersionCommand},  # type: ignore
 )
