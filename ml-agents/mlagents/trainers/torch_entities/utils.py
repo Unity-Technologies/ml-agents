Fixed Mixed CPU, GPU Computation Error

ml-agents/mlagents/trainers/torch_entities/utils.py
@@ -325,6 +325,8 @@ class ModelUtils:
                 (torch.ones_like(tensor) * masks).float().sum(), min=1.0
             )
         else:
+            masks = masks.to(tensor.device)
+            # print("Mask Device:",masks.device)
             return (
                 tensor.permute(*torch.arange(tensor.ndim - 1, -1, -1)) * masks
             ).sum() / torch.clamp(
@@ -416,9 +418,11 @@ class ModelUtils:
         value_losses = []
         for name, head in values.items():
             old_val_tensor = old_values[name]
+            old_val_tensor = old_val_tensor.to(head.device)
             returns_tensor = returns[name]
+            returns_tensor = returns_tensor.to(head.device)
             clipped_value_estimate = old_val_tensor + torch.clamp(
-                head - old_val_tensor, -1 * epsilon, epsilon
+                head - old_val_tensor, -epsilon, epsilon
             )
             v_opt_a = (returns_tensor - head) ** 2
             v_opt_b = (returns_tensor - clipped_value_estimate) ** 2
@@ -443,6 +447,8 @@ class ModelUtils:
         :param loss_masks: Mask for losses. Used with LSTM to ignore 0'ed out experiences.
         """
         advantage = advantages.unsqueeze(-1)
+        advantage = advantage.to(log_probs.device)
+        old_log_probs = old_log_probs.to(log_probs.device)
         r_theta = torch.exp(log_probs - old_log_probs)
         p_opt_a = r_theta * advantage
         p_opt_b = torch.clamp(r_theta, 1.0 - epsilon, 1.0 + epsilon) * advantage