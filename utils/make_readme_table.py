f4c60562f6b992012ebf76612167d4e80733ee8b
maryam-zia <maryam.zia@unity3d.com>
2025-08-15 15:55:37 -0400

    Undo release 23 table

--- utils/make_readme_table.py
+++ utils/make_readme_table.py
@@ -146,7 +146,6 @@ versions = [
     ReleaseInfo("release_20", "2.3.0", "0.30.0", "November 21, 2022"),
     ReleaseInfo("release_21", "3.0.0-exp.1", "1.0.0", "October 9, 2023"),
     ReleaseInfo("release_22", "3.0.0", "1.1.0", "October 5, 2024"),
-    ReleaseInfo("release_23", "4.0.0", "1.1.0", "July 11, 2025"),
     # Verified releases
     # ReleaseInfo("", "1.0.8", "0.16.1", "May 26, 2021", is_verified=True),
     # ReleaseInfo("", "1.0.7", "0.16.1", "March 8, 2021", is_verified=True),
