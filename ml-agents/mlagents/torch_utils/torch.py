fec00c2f839671f373677ca238a44e02c9c7c95a
Viviane Rochon Montplaisir <v.rochonmontplaisir@unity3d.com>
2026-06-17 14:37:33 -0400

    Add "xpu" and "mps" as target devices

--- ml-agents/mlagents/torch_utils/torch.py
+++ ml-agents/mlagents/torch_utils/torch.py
@@ -49,11 +49,9 @@ def set_torch_config(torch_settings: TorchSettings) -> None:
 
     _device = torch.device(device_str)
 
-    if _device.type == "cuda":
-        torch.set_default_device(_device.type)
-        torch.set_default_dtype(torch.float32)
-    else:
-        torch.set_default_dtype(torch.float32)
+    if _device.type == "cuda" or _device.type == "xpu" or _device.type == "mps":
+        torch.set_default_device(_device)
+    torch.set_default_dtype(torch.float32)
     logger.debug(f"default Torch device: {_device}")
 
 
